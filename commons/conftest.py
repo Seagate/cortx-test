cceab1bbc82f8d858b0a3c50ef2ff4ff37a1fc7d
seagate-sarang-sawant <70256535+seagate-sarang-sawant@users.noreply.github.com>
2020-12-29 20:09:39 +0530

    EOS-15913 seperate logging for each test case

--- commons/conftest.py
+++ commons/conftest.py
@@ -21,8 +21,10 @@
 import pytest
 import pathlib
 import json
+import logging
 from testfixtures import LogCapture
 from commons.utils import yaml_utils
+from commons import Globals
 
 @pytest.fixture(autouse=True)
 def _read_project_config(request):
@@ -42,11 +44,51 @@ def read_project_config(request):
     with config.open() as fp:
         return json.load(fp)
 
+def init_loghandler(log): #TODO needs to be removed after framework Logger is developed
+    log.setLevel(logging.DEBUG)
+    fh = logging.FileHandler('pytestfeatures.log', mode='a')
+    fh.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    log.addHandler(fh)
+    log.addHandler(ch)
+
 
 @pytest.fixture(autouse=True)
 def capture():
     with LogCapture() as logs:
         yield logs
+
+
+@pytest.fixture(scope='session')
+def formatter():
+    format_log_message = '%(asctime)s\t%(levelname)s\t%(filename)s\t%(funcName)s\t%(processName)s\t%(message)s'
+    formatter = logging.Formatter(fmt=format_log_message, datefmt='%Y-%m-%d %H:%M:%S')
+    return formatter
+
+@pytest.fixture(scope='session')
+def logger():
+    logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    init_loghandler(logger)  #TODO reference
+    return logger
+
+
+@pytest.fixture(scope='function')
+def log_cutter(request, formatter):
+    print("setup")
+    name = request.function.__name__
+    records = dict()
+    yield records
+    records = Globals.records.get(name)
+    print("teardown")
+    with open(name, 'w') as f:
+        for rec in records:
+            f.write(formatter.format(rec) + '\n')
 
 
 @pytest.fixture
