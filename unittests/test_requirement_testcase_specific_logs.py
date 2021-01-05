3635fd807586981c5fcfa759ccda15fc147cccbb
seagate-sarang-sawant <70256535+seagate-sarang-sawant@users.noreply.github.com>
2021-01-05 17:03:40 +0530

    EOS-15980 and EOS-15913 documentation and code review  (#3)
    * Create Framework-Requirements.md
    
    Test Automation Framework Requirements
    
    * Update Framework-Requirements.md
    
    Framework requirement for new test automation project for R2.
    
    * Create Architectural-Overview.md
    
    * Update Architectural-Overview.md
    
    Architectural decision and Repo structure
    
    * Update Architectural-Overview.md
    
    Formatting changes
    
    * Update Architectural-Overview.md
    
    * Create Test-Execution-Deployment-View.md
    
    Deployment View Diagram for full setup mode
    
    * Add files via upload
    
    * Update Test-Execution-Deployment-View.md
    
    * Add deployment view in development setup
    
    * dev execution single VM deployment view
    
    * Replace old file
    
    * Update Architectural-Overview.md
    
    * Create Design-Document-Test-Framework.md
    
    * Update Design-Document-Test-Framework.md
    
    * Update Design-Document-Test-Framework.md
    
    * Update Design-Document-Test-Framework.md
    
    * Update Design-Document-Test-Framework.md
    
    * Add files via upload
    
    Test Framework layering
    
    * Update Design-Document-Test-Framework.md
    
    * Update Design-Document-Test-Framework.md
    
    * Update Design-Document-Test-Framework.md
    
    Config management
    
    * Update Design-Document-Test-Framework.md
    
    * Update Design-Document-Test-Framework.md
    
    * Add files via upload
    
    Design Docs from Swapnil and Ketan
    
    * EOS-15980 Added Reporting and Test runner design doc and moved test framework images to media folder
    
    * Update Architectural-Overview.md
    
    * Update Design-Document-Test-Framework.md
    
    * Update Design-Document-Test-Framework.md
    
    * Update Design-Document-Test-Framework.md
    
    * Update Design-Document-Test-Framework.md
    
    * Add files via upload
    
    * Update Design-Document-Test-Framework.md
    
    * Update Test-Execution-Deployment-View.md
    
    * Update Test-Execution-Deployment-View.md
    
    * Update Design-Document-Test-Framework.md
    
    * Update Design-Document-Test-Framework.md
    
    * EOS-15913 seperate logging for each test case
    
    * Update DB_REST_Reporting.md
    
    * EOS-15913 removed unwanted comments
    
    Signed-off-by: Subham Pattanaik <shubham.patnaik@seagate.com>

---
+++ unittests/test_requirement_testcase_specific_logs.py
@@ -0,0 +1,105 @@
+import pytest
+import logging
+from commons import Globals
+# import _pytest.logging.LogCaptureFixture
+from testfixtures import LogCapture
+
+def init_loghandler(log):
+    log.setLevel(logging.DEBUG)
+    fh = logging.FileHandler('pytestfeatures.log', mode='a')
+    fh.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    log.addHandler(fh)
+    log.addHandler(ch)
+
+@pytest.fixture(scope='session')
+def formatter():
+    format_log_message = '%(asctime)s\t%(levelname)s\t%(filename)s\t%(funcName)s\t%(processName)s\t%(message)s'
+    formatter = logging.Formatter(fmt=format_log_message, datefmt='%Y-%m-%d %H:%M:%S')
+    return formatter
+
+@pytest.fixture(scope='session')
+def logger():
+    logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    init_loghandler(logger)
+    return logger
+
+
+def setup_module(module):
+    logging.getLogger(__name__).info('Entered teardown module')
+
+
+def teardown_module(module):
+    logging.getLogger(__name__).info('Exited teardown module')
+
+
+def setup_function(function):
+    """ setup any state tied to the execution of the given function.
+    Invoked for every test function in the module.
+    """
+
+
+def teardown_function(function):
+    """ teardown any state that was previously setup with a setup_function
+    call.
+    """
+    # with open(function.__name__, 'w') as f:
+    #     for rec in capture.records:
+    #         f.write(formatter.format(rec) + '\n')
+
+@pytest.fixture(autouse=True)
+def capture():
+    with LogCapture() as logs:
+        yield logs
+
+
+def max(values):
+    _max = values[0]
+
+    for val in values:
+        if val > _max:
+            _max = val
+    logging.getLogger(__name__).info("inner function max is %s" % _max)
+    return _max
+
+
+def min(values):
+    _min = values[0]
+
+    for val in values:
+        if val < _min:
+            _min = val
+    logging.getLogger(__name__).info("inner function min is %s" % _min)
+    return _min
+
+
+@pytest.fixture(scope='function')
+def log_cutter(request, formatter):
+    print("setup")
+    name = request.function.__name__
+    records = dict()
+    yield records
+    records = Globals.records.get(name)
+    print("teardown")
+    with open(name, 'w') as f:
+        for rec in records:
+            f.write(formatter.format(rec) + '\n')
+
+@pytest.mark.usefixtures("log_cutter")
+def test_min(request, capture, logger):
+    values = (2, 3, 1, 4, 6)
+    val = min(values)
+    logger.debug("min is %s" % val)
+    logger.warning("min is %s" % val)
+    logger.info("min is %s" % val)
+    logger.error("min is %s" % val)
+    assert val == 1
+    records = capture.records
+    test_name = request.node.name
+    Globals.records.update({test_name: records})
