f717eeff68792180cb1ebd42f1ce20173486fda1
Divya Kachhwaha <divya.kachhwaha@seagate.com>
2021-01-11 16:58:14 +0530

    host file review changed done

--- commons/helpers/host.py
+++ commons/helpers/host.py
@@ -17,67 +17,47 @@
 # For any questions about this software or licensing,
 # please email opensource@seagate.com or cortx-questions@seagate.com.
 #
-
-################################################################################
-# Standard libraries
-################################################################################
 import logging
 import time
 import socket
+from typing import Union, Tuple, List
 import pysftp
 import paramiko
 
-################################################################################
-# Local libraries
-################################################################################
-# None
-
-################################################################################
-# Constants
-################################################################################
 log = logging.getLogger(__name__)
 
-################################################################################
-# Interface Classes
-################################################################################
 
 class Host():
     """Interface class for establishing connections
-    """    
-    def __init__(self, hostname, username, password):
+    """
+
+    def __init__(self, hostname: str, username: str, password: str) -> bool:
         self.hostname = hostname
         self.username = username
         self.password = password
         self.host_obj = None
+        self.shell_obj = None
 
-    ############################################################################
-    # remote connection options
-    ############################################################################
-    def _connect(self, shell=True, port=22 , timeout_sec=400, **kwargs):
+    def connect(self, shell=False, timeout_sec=400, **kwargs) -> bool:
         """
-        Connect to remote host.
-        :param host: host ip address
-        :type host: str
-        :param username: host username
-        :type username: str
-        :param password: host password
-        :type password: str
+        Connect to remote host using hostname, username and password attribute
         :param shell: In case required shell invocation
+        :param timeout_sec: timeout in seconds
+        :param kwargs: Optional keyword arguments for SSHClient.connect func call
         :return: Boolean, Whether ssh connection establish or not
         """
         try:
             self.host_obj = paramiko.SSHClient()
             self.host_obj.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             log.debug(f"Connecting to host: {self.hostname}")
-            self.host_obj.connect(hostname=self.hostname, 
-                                  username=self.username, 
+            self.host_obj.connect(hostname=self.hostname,
+                                  username=self.username,
                                   password=self.password,
                                   timeout=timeout_sec,
-                                  port=port,
                                   **kwargs)
             if shell:
-                shell = self.host_obj.invoke_shell()
-   
+                self.shell_obj = self.host_obj.invoke_shell()
+
         except paramiko.AuthenticationException:
             log.error("Server authentication failed")
             result = False
@@ -86,8 +66,9 @@ class Host():
             result = False
         except socket.timeout as timeout_exception:
             log.error("Could not establish connection because of timeout: %s",
-                timeout_exception)
-            result = False
+                      timeout_exception)
+            result = self.reconnect(
+                1, shell=shell, timeout_sec=timeout_sec, **kwargs)
         except Exception as error:
             log.error("Exception while connecting to server")
             log.error(f"Error message: {error}")
@@ -100,12 +81,9 @@ class Host():
             result = True
         return result
 
-    def connect_pysftp(self, private_key=None, private_key_pass=None):
+    def connect_pysftp(self, private_key=None, private_key_pass: str = None) -> bool:
         """
         Connect to remote host using pysftp
-        :param str host: The Hostname or IP of the remote machine
-        :param str username: Your username at the remote machine
-        :param str pwd: Your password at the remote machine
         :param str private_key: path to private key file(str) or paramiko.AgentKey
         :param str private_key_pass:  password to use, if private_key is encrypted
         :return: connection object based on the success
@@ -115,12 +93,12 @@ class Host():
             log.debug(f"Connecting to host: {self.hostname}")
             cnopts = pysftp.CnOpts()
             cnopts.hostkeys = None
-            result = pysftp.Connection( host=self.hostname,
-                                        username=self.username,
-                                        password=self.password,
-                                        private_key=private_key,
-                                        private_key_pass=private_key_pass,
-                                        cnopts=cnopts)
+            result = pysftp.Connection(host=self.hostname,
+                                       username=self.username,
+                                       password=self.password,
+                                       private_key=private_key,
+                                       private_key_pass=private_key_pass,
+                                       cnopts=cnopts)
             self.host_obj = result
         except socket.timeout as timeout_exception:
             log.error(
@@ -134,49 +112,48 @@ class Host():
         return result
 
     def disconnect(self):
+        if self.shell_obj is not None:
+            self.shell_obj.close()
         self.host_obj.close()
+        self.host_obj = None
+        self.shell_obj = None
 
-    def reconnect(self, retry_count):
+    def reconnect(self, retry_count: int, **kwargs) -> bool:
         """
         This method re-connect to host machine
-        :param host: host machine ip
-        :param username: host machine username
-        :param password: host machine password
         :param retry_count: host retry count
-        :return: string
+        :return: bool
         """
         while retry_count:
-            retval = self._connect()
-            if retval is False:
+            retval = self.connect(**kwargs)
+            if retval:
+                return retval
+            else:
                 retry_count -= 1
                 time.sleep(1)
-                continue
-            break
-            
+        return False
     ############################################################################
     # execute command
     ############################################################################
-    def execute_cmd(self, cmd, shell=False, inputs=None, read_lines=False, read_nbytes=-1 ,timeout_sec=400, **kwargs):
+
+    def execute_cmd(self, cmd: str, inputs: str = None, read_lines: bool = False, 
+    read_nbytes: int = -1, timeout_sec=400, **kwargs) -> Tuple[bool, Union[List[str], str, bytes]]:
         """
+        If connection is not established,  it will establish the connection and 
         Execute any command on remote machine/VM
-        :param host: Host IP
-        :param user: Host user name
-        :param password: Host password
         :param cmd: command user wants to execute on host
         :param read_lines: Response will be return using readlines() else using read()
         :param inputs: used to pass yes argument to commands.
-        :type inputs: str
         :param nbytes: nbytes returns string buffer.
-        :type nbytes: bool
-        :param read_sls: use only if we want to read sls file data
-        :type read_sls: bool
-        :return: response
+        :return: bool, stdout / strerr
         """
         try:
             result = False
-            result = self._connect(shell=shell,**kwargs)
+            if self.host_obj.get_transport() is None:
+                result = self.connect(timeout=timeout_sec, **kwargs)
             if result:
-                stdin, stdout, stderr = self.host_obj.exec_command(cmd, timeout=timeout_sec)
+                stdin, stdout, stderr = self.host_obj.exec_command(
+                    cmd, timeout=timeout_sec)
                 exit_status = stdout.channel.recv_exit_status()
                 log.debug(exit_status)
                 if exit_status != 0:
@@ -200,4 +177,3 @@ class Host():
         except BaseException as error:
             log.error(error)
             return False, str(error)
-
