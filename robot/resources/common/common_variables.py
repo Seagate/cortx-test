ee0fa6a7304fb915ad9a7c5c34e21fc008e33508
ashish-seagate <67002513+ashish-seagate@users.noreply.github.com>
2021-01-12 18:55:17 +0530

    robot framework folder structure and Common keywords (#14)
    * robot framework folder structure and Common keywords
    
    * Addressed the review comments
    
    Co-authored-by: ‘ashish-seagate’ <‘ashish.dhavalikar@seagate.com’>
    Signed-off-by: Subham Pattanaik <shubham.patnaik@seagate.com>

---
+++ robot/resources/common/common_variables.py
@@ -0,0 +1,17 @@
+"""
+# File for maintaining common variables
+"""
+
+# Common variables required for GUI access
+# URL's required to connect to the CSM GUI
+CSM_URL = "https://10.230.246.58:28100/#/"
+
+#Login page msg.
+LOGIN_FAILED_MSG = 'Login failed !'
+
+# Add Infra variables
+BROWSER = "chrome"
+
+#Expected value
+S3_ACCOUNT = 'tests3account'
+CSM_USER = 'monitoruser'
