e54697209ccdb8b37846c3ffce3f15534f11cd7b
VikasKumarC <vikas.kumar@seagate.com>
2021-01-14 20:10:51 +0530

    incorporating review comments

--- commons/gevent_thread.py
+++ commons/gevent_thread.py
@@ -23,10 +23,7 @@ import sys
 import logging
 import gevent
 from gevent import Greenlet
-from gevent import Timeout
 from gevent.queue import Queue
-from gevent.pool import Pool
-# from queue import Queue
 from typing import List, Tuple, Any, Optional
 
 logger = logging.getLogger(__name__)
@@ -44,13 +41,28 @@ class Multithreading(Greenlet):
 
     def __init__(
             self,
-            *args,
-            run=None,
-            thread_id=None,
-            thread_q=None,
-            **kwargs):
+            *args : Optional,
+            run: Any = None,
+            thread_id: int = None,
+            thread_q: object = None,
+            **kwargs : Optional) -> None:
         """
-        Constructor
+        if run is None then _run will be executed
+        *args and *kwargs will be passed to super constructor to execute thread method
+        :param args: collection of function parameter.
+        :type tuple:  packed in tuple.
+        :param run: name of the function passed in thread for execution.
+        :type object:  The callable object to run. If not given, this object's
+            `_run` method will be invoked (typically defined by subclasses)..
+        :param thread_id: thread number associated with thread in execution.
+        :type integer:  assigned as per sequence of execution.
+        :param thread_q: collection of thread data in fifo .
+        :type Queue:  Gevent queue.
+        :param kwargs: collection of keyword function parameter.
+        :type dict:  packed in dictionary.
+        :return: None.
+        :rtype: None.
+
         """
         super().__init__(run, *args, **kwargs)
         if thread_q is not None:
@@ -58,15 +70,17 @@ class Multithreading(Greenlet):
         else:
             self.queue = Queue()
         self.thread_id = thread_id
-        self._action()
+        self._q_put()
 
-    def _run(self):
+    def _run(self) -> Any:
         """Build some CPU-intensive tasks to run via multiprocessing here.
-        and Return some information back through multiprocessing.Queue
+        and Return some information back
+        Subclasses may override this method to take any number of
+        arguments and keyword arguments.
         """
-        logger.info("Running GThraeds")
+        logger.info("Running GThrads")
 
-    def _action(self) -> None:
+    def _q_put(self) -> None:
         """
          self.thread_id is id of thread passed from calling function.
          self.name is an attribute of multiprocessing.Process
@@ -76,14 +90,16 @@ class Multithreading(Greenlet):
                 self.thread_id, self.name))
 
     def terminate(self) -> None:
-        """ wait until queue is empty and terminate threads """
+        """ wait until queue is empty and terminate threads
+            Method to overide and extend for child classes
+         """
         pass
 
 
 class GThread(Multithreading):
     queue = Queue()
 
-    def __init__(self, *args, run=None, thread_id=None, **kwargs):
+    def __init__(self, *args, run=None, thread_id=None, **kwargs) -> None:
         super(
             GThread,
             self).__init__(
@@ -95,15 +111,18 @@ class GThread(Multithreading):
         logger.debug("Creating GThread Object")
         self.responses = dict()  # Collecting Thread name and Thread Result/Return response
 
-    def _run(self, message=None) -> None:
+    def _run(self, message=None) -> Any:
         logger.debug(message)
         super()._run()
 
     @staticmethod
     def join() -> None:
         """
+        Operating on list/collection of executing thread objects
         Waiting for all threads to complete.
         Collecting list of finished threads and their results.
+        :return: None.
+        :rtype: None.
         """
         logger.debug("Waiting for all threads to complete\n")
         logger.debug(threads)
@@ -112,13 +131,11 @@ class GThread(Multithreading):
         GThread.responses = {thread.name: thread.value for thread in threads}
 
     @staticmethod
-    def terminate() -> Tuple:
+    def terminate() -> Tuple[bool, List]:
         """ wait until queue is empty and terminate threads """
         GThread.join()
-        logger.debug(threads)
         logger.debug(
             "Terminating all processes once they finished with task\n")
-        results = list()
         while not GThread.queue.empty():
             # get results from the queue...
             logger.info("RESULT: {0}".format(GThread.queue.get()))
