f4ea55e48340873fdf0411cfcc1aa7b64c8aa1b2
Rajesh Pratap Singh Chouhan <rajesh.chouhan7@gmail.com>
2021-02-19 19:03:54 +0530

    EOS-17453 added new CFT tests (#81)
    * automated CFT TEST-1213 , updated TEST-1219
    fixed few other minor things
    Signed-off-by: rajesh.chouhan <rajesh.chouhan@seagate.com>
    
    * updated comment
    
    Signed-off-by: rajesh.chouhan <rajesh.chouhan@seagate.com>
    
    * addressed review comments
    
    Signed-off-by: rajesh.chouhan <rajesh.chouhan@seagate.com>
    
    Co-authored-by: seagate-sarang-sawant <70256535+seagate-sarang-sawant@users.noreply.github.com>

--- robot/resources/common/element_locators.py
+++ robot/resources/common/element_locators.py
@@ -21,6 +21,17 @@ DASHBOARD_MENU_ID = "Dashboard"
 MAINTENANCE_MENU_ID = 'Maintenance'
 AUDIT_LOG_TAB_ID = "goToAuditLog"
 
+# Alerts
+ALERT_IMAGE_1_ID='alert-zoom'  # from Dashbard
+ALERT_IMAGE_2_ID='alert-dotwhite'  # from All pages
+ALERT_COMMENT_SAVE_BUTTON_ID = 'alert-save-commnetbtn'
+ALERT_COMMENT_CLOSE_BUTTON_ID = 'alert-close-comment-dialogbtn'
+ALERT_COMMENT_CLOSE_IMAGE_ID = 'alert-closeadd-comment-dialog'
+ALERT_COMMENT_TEXT_ID= 'alert-comment-textarea'
+ALERT_DETAILS_PAGE_ICON_XPATH = '//*[@id="tblAlertLarge"]/div/table/tbody/tr[1]/td[5]/img'
+ALERT_COMMENT_ICON_XPATH = '//*[@id="tblAlertLarge"]/div/table/tbody/tr[1]/td[5]/div/div'
+ALERT_ACKNOWLEDGE_ICON_XPATH = '//*[@id="tblAlertLarge"]/div/table/tbody/tr[1]/td[5]/div/span[2]/div'
+
 #  S3 Config
 S3_ACCOUNTS_TABLE_XPATH = '//*[@id="s3-datatable"]//table/tbody/tr/td'
 S3_ACCOUNT_NUMBER_OF_ROWS = '//*[@id="s3-datatable"]//table/tbody/tr'
@@ -65,7 +76,7 @@ EDIT_S3_ACCOUNT_OPTIONS_XPATH = '//*[@id="app"]//div[@class ="py-0 col-5 col"]//
 # Preboarding 
 WELCOME_START_BUTTON_ID = 'welcome-startbtn'
 ELUA_BUTTON_ID = 'show-license-agreement-dialogbtn'
-LICENSE_BUTTON_ID = 'license-acceptagreement'
+LICENSE_ACCEPT_BUTTON_ID = 'license-acceptagreement'
 LICENSE_CANCLE_BUTTON_ID = 'license-cancelagreementbtn'
 LICENSE_CANCLE_IMAGE_ID = 'license-cancelagreementicon'
 LICENSE_TITLE_ID = 'agreement-title'
