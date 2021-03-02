53b4111b4374076757f6f5ecd172138f2f17435f
Divya Kachhwaha <divya.kachhwaha@seagate.com>
2021-03-02 16:49:36 +0530

    commit

--- config/__init__.py
+++ config/__init__.py
@@ -12,14 +12,19 @@ RAS_VAL = config_utils.read_yaml(RAS_CONFIG_PATH)[1]
 RAS_TEST_CFG = config_utils.read_yaml(SSPL_TEST_CONFIG_PATH)[1]
 CMN_DESTRUCTIVE_CFG = config_utils.read_yaml(COMMON_DESTRUCTIVE_CONFIG_PATH)[1]
 CSM_CFG = configmanager.get_config_wrapper(fpath=CSM_CONFIG)
-import pdb
-pdb.set_trace()
-if os.getenv('TARGET') is not None:
+
+args = sys.argv
+args = dict(zip(args[::2],args[1::2]))
+
+if '--local' in args and args['--local']:
+    target = args['--target']
+elif os.getenv('TARGET') is not None:
     target = os.environ["TARGET"]
-    if os.path.exists(SETUPS_FPATH):
-        SETUP_DETAILS = config_utils.read_content_json(SETUPS_FPATH)[target]
-    else:
-        setup_query = {"setupname": target}
-        SETUP_DETAILS = configmanager.get_config_wrapper(setup_query = setup_query)[target]
 else:
-    SETUP_DETAILS = None
+    target = "automation"
+
+if os.path.exists(SETUPS_FPATH):
+    SETUP_DETAILS = config_utils.read_content_json(SETUPS_FPATH)[target]
+else:
+    setup_query = {"setupname": target}
+    SETUP_DETAILS = configmanager.get_config_wrapper(setup_query = setup_query)[target]
