43f6103ea4ca5eb0af3aea3edbf290fcb4693243
priyankaborawake <priyanka.borawake@seagate.com>
2021-03-09 14:48:09 +0530

    URI changes for mongodb
    Signed-off-by: priyankaborawake <priyanka.borawake@seagate.com>

--- tools/dash_server/mongodbAPIs.py
+++ tools/dash_server/mongodbAPIs.py
@@ -1,138 +1,19 @@
 from pymongo import MongoClient
 from pymongo.errors import PyMongoError
 import configparser
+from urllib.parse import quote_plus
 
 # A MongoDb instance can have multiple databases in it.
 # Each database can have multiple collections(tables) in it.
 # Each collection can have multiple documents(row) in it.
 config = configparser.ConfigParser()
 config.read('config.ini')
-writeHostURI = config["MONGODB_URI"]["writeHostURI"]
-readHostURI = config["MONGODB_URI"]["readHostURI"]
-
-
-def add_one_to_database(data):
-    """
-    Add single document to mongodb database
-
-        Parameters:
-            data (dict): The document to be inserted
-
-        Returns:
-            result (bool): insert operation successfully completed or not
-    """
-    with MongoClient(writeHostURI) as client:
-        db = client['cft_test_results']  # Connect to cft_test_results database
-        tests = db.results  # Connect to results collection in cft_test_results database
-        try:
-            tests.insert_one(data)
-        except PyMongoError as mongo_error:
-            print("Unable to insert documents into database. Observed following exception:")
-            print(mongo_error)
-            return False
-        else:
-            print("Document added into database")
-        return True
-
-
-def add_multiple_to_database(data):
-    """
-    Add single document to mongodb database
-
-        Parameters:
-            data (list of dict): List of documents to be inserted
-
-        Returns:
-            result (bool): insert operation successfully completed or not
-    """
-    with MongoClient(writeHostURI) as client:
-        db = client['cft_test_results']  # Connect to cft_test_results database
-        tests = db.results  # Connect to results collection in cft_test_results database
-        try:
-            tests.insert_many(data)
-        except PyMongoError as mongo_error:
-            print("Unable to insert documents into database. Observed following exception:")
-            print(mongo_error)
-            return False
-        else:
-            print("Documents added into database")
-            return True
-
-
-def find_and_update_one(db_filter, update):
-    """
-    Finds a single document and updates it
-
-        Parameters:
-            db_filter (dict): A query that matches the document to update
-                e.g. {'testid': 'Test1'} will query database for a document with testid as Test1
-            update (dict): The update operations to apply
-                e.g. {'$set': {'result': 'Pass'}} will set Pass in result field
-        Returns:
-            result (bool): update operation successfully completed or not
-    """
-    with MongoClient(writeHostURI) as client:
-        db = client['cft_test_results']
-        tests = db.results
-        try:
-            tests.find_one_and_update(db_filter, {"$set": update})
-        except PyMongoError as mongo_error:
-            print("Unable to find and update document into database. Observed following exception:")
-            print(mongo_error)
-            return False
-        else:
-            print("Document updated in database")
-            return True
-
-
-def delete_one(db_filter):
-    """
-    Delete a single document matching the db_filter
-
-        Parameters:
-            db_filter (dict): A query that matches the document to delete
-                e.g. {'testid': 'Test1'} will query database for a document with testid as Test1
-        Returns:
-            result (bool): delete operation successfully completed or not
-    """
-    with MongoClient(writeHostURI) as client:
-        db = client['cft_test_results']
-        tests = db.results
-        try:
-            tests.delete_one(db_filter)
-        except PyMongoError as mongo_error:
-            print("Unable to delete a document from database. Observed following exception:")
-            print(mongo_error)
-            return False
-        else:
-            print("Document deleted from database")
-            return True
-
-
-def delete_many(db_filter):
-    """
-    Delete multiple documents matching the db_filter
-
-        Parameters:
-            db_filter (dict): A query that matches the document to delete
-                e.g. {'build': 'demo'} will query database for documents with build as demo
-        Returns:
-            result (bool): delete operation successfully completed or not
-    """
-    with MongoClient(writeHostURI) as client:
-        db = client['cft_test_results']
-        tests = db.results
-        try:
-            tests.delete_many(db_filter)
-        except PyMongoError as mongo_error:
-            print("Unable to delete documents from database. Observed following exception:")
-            print(mongo_error)
-            return False
-        else:
-            print("Documents deleted from database")
-            return True
-
-
+hostname = config["MONGODB_URI"]["hostname"]
+db_username = config["MONGODB_URI"]["db_username"]
+db_password = config["MONGODB_URI"]["db_password"]
+hostURI = "mongodb://{0}:{1}@{2}".format(quote_plus(db_username),
+                                         quote_plus(db_password),
+                                         hostname)
 def find(db_filter):
     """
     Find multiple documents matching the db_filter
@@ -143,7 +24,7 @@ def find(db_filter):
         Returns:
             result (bool): find operation successfully completed or not
     """
-    with MongoClient(readHostURI) as client:
+    with MongoClient(hostURI) as client:
         db = client['cft_test_results']
         tests = db.results
         result = None
@@ -168,7 +49,7 @@ def count_documents(db_filter):
         Returns:
             result (int): number of documents matching the db_filter
     """
-    with MongoClient(readHostURI) as client:
+    with MongoClient(hostURI) as client:
         db = client['cft_test_results']
         tests = db.results
         result = None
@@ -194,7 +75,7 @@ def find_distinct(entry, db_filter):
        Returns:
            result (int): number of documents matching the db_filter
        """
-    with MongoClient(readHostURI) as client:
+    with MongoClient(hostURI) as client:
         db = client['cft_test_results']
         tests = db.results
         result = None
--- tools/dash_server/timingAPIs.py
+++ tools/dash_server/timingAPIs.py
@@ -1,139 +1,20 @@
 from pymongo import MongoClient
+from pymongo.errors import PyMongoError
+import configparser
+from urllib.parse import quote_plus
 
 # A MongoDb instance can have multiple databases in it.
 # Each database can have multiple collections(tables) in it.
 # Each collection can have multiple documents(row) in it.
 
-readHostURI = "mongodb://dataread:seagate%40123@cftic1.pun.seagate.com:27017," \
-              "cftic2.pun.seagate.com:27017,apollojenkins.pun.seagate.com:27017/" \
-              "?authSource=cft_test_results&replicaSet=rs0"
-
-writeHostURI = "mongodb://datawrite:seagate%40123@cftic1.pun.seagate.com:27017," \
-               "cftic2.pun.seagate.com:27017,apollojenkins.pun.seagate.com:27017" \
-               "/?authSource=cft_test_results&replicaSet=rs0"
-
-
-def add_one_to_database(data):
-    '''
-    Add single document to mongodb database
-
-        Parameters:
-            data (dict): The document to be inserted
-
-        Returns:
-            result (bool): insert operation successfully completed or not
-    '''
-    with MongoClient(writeHostURI) as client:
-        db = client['cft_test_results']  # Connect to cft_test_results database
-        tests = db.timings  # Connect to timings collection in cft_test_results database
-        try:
-            tests.insert_one(data)
-        except Exception as e:
-            print("Unable to insert a document into database. Observed following exception:")
-            print(e)
-            return False
-        else:
-            print("Document added into database")
-            return True
-
-
-def add_multiple_to_database(data):
-    '''
-    Add single document to mongodb database
-
-        Parameters:
-            data (list of dict): List of documents to be inserted
-
-        Returns:
-            result (bool): insert operation successfully completed or not
-    '''
-    with MongoClient(writeHostURI) as client:
-        db = client['cft_test_results']  # Connect to cft_test_results database
-        tests = db.timings  # Connect to timings collection in cft_test_results database
-        try:
-            tests.insert_many(data)
-        except Exception as e:
-            print("Unable to insert documents into database. Observed following exception:")
-            print(e)
-            return False
-        else:
-            print("Documents added into database")
-            return True
-
-
-def find_and_update_one(db_filter, update):
-    '''
-    Finds a single document and updates it
-
-        Parameters:
-            db_filter (dict): A query that matches the document to update
-                e.g. {'testid': 'Test1'} will query database for a document with testid as Test1
-            update (dict): The update operations to apply
-                e.g. {'$set': {'result': 'Pass'}} will set Pass in result field
-        Returns:
-            result (bool): update operation successfully completed or not
-    '''
-    with MongoClient(writeHostURI) as client:
-        db = client['cft_test_results']
-        tests = db.timings
-        try:
-            tests.find_one_and_update(db_filter, {"$set": update})
-        except Exception as e:
-            print("Unable to find and update document into database. Observed following exception:")
-            print(e)
-            return False
-        else:
-            print("Document updated in database")
-            return True
-
-
-def delete_one(db_filter):
-    '''
-    Delete a single document matching the db_filter
-
-        Parameters:
-            db_filter (dict): A query that matches the document to delete
-                e.g. {'testid': 'Test1'} will query database for a document with testid as Test1
-        Returns:
-            result (bool): delete operation successfully completed or not
-    '''
-    with MongoClient(writeHostURI) as client:
-        db = client['cft_test_results']
-        tests = db.timings
-        try:
-            tests.delete_one(db_filter)
-        except Exception as e:
-            print("Unable to delete a document from database. Observed following exception:")
-            print(e)
-            return False
-        else:
-            print("Document deleted from database")
-            return True
-
-
-def delete_many(db_filter):
-    '''
-    Delete multiple documents matching the db_filter
-
-        Parameters:
-            db_filter (dict): A query that matches the document to delete
-                e.g. {'build': 'demo'} will query database for documents with build as demo
-        Returns:
-            result (bool): delete operation successfully completed or not
-    '''
-    with MongoClient(writeHostURI) as client:
-        db = client['cft_test_results']
-        tests = db.timings
-        try:
-            tests.delete_many(db_filter)
-        except Exception as e:
-            print("Unable to delete documents from database. Observed following exception:")
-            print(e)
-            return False
-        else:
-            print("Documents deleted from database")
-            return True
-
+config = configparser.ConfigParser()
+config.read('config.ini')
+hostname = config["MONGODB_URI"]["hostname"]
+db_username = config["MONGODB_URI"]["db_username"]
+db_password = config["MONGODB_URI"]["db_password"]
+hostURI = "mongodb://{0}:{1}@{2}".format(quote_plus(db_username),
+                                         quote_plus(db_password),
+                                         hostname)
 
 def find(db_filter):
     '''
@@ -145,15 +26,15 @@ def find(db_filter):
         Returns:
             result (bool): find operation successfully completed or not
     '''
-    with MongoClient(readHostURI) as client:
+    with MongoClient(hostURI) as client:
         db = client['cft_test_results']
         tests = db.timings
         result = None
         try:
             result = tests.find(db_filter)
-        except Exception as e:
+        except PyMongoError as mongo_error:
             print("Unable to search documents from database. Observed following exception:")
-            print(e)
+            print(mongo_error)
             return False
         else:
             print("Documents search complete")
@@ -170,15 +51,15 @@ def count_documents(db_filter):
         Returns:
             result (int): number of documents matching the db_filter
     '''
-    with MongoClient(readHostURI) as client:
+    with MongoClient(hostURI) as client:
         db = client['cft_test_results']
         tests = db.timings
         result = None
         try:
             result = tests.count_documents(db_filter)
-        except Exception as e:
+        except PyMongoError as mongo_error:
             print("Unable to search documents from database. Observed following exception:")
-            print(e)
+            print(mongo_error)
             return False
         else:
             print("Documents count complete")
@@ -186,15 +67,15 @@ def count_documents(db_filter):
 
 
 def find_distinct(entry, db_filter):
-    with MongoClient(readHostURI) as client:
+    with MongoClient(hostURI) as client:
         db = client['cft_test_results']
         tests = db.timings
         result = None
         try:
             result = tests.distinct(entry, db_filter)
-        except Exception as e:
+        except PyMongoError as mongo_error:
             print("Unable to search documents from database. Observed following exception:")
-            print(e)
+            print(mongo_error)
             return False
         else:
             print("Distinct documents retrieved")
