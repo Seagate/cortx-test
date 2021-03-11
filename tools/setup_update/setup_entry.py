50ca82e7fb725fbf178676f556c02d5e0a5b55ce
Divya Kachhwaha <divya.kachhwaha@seagate.com>
2021-03-11 08:54:00 +0530

    commit

--- tools/setup_update/setup_entry.py
+++ tools/setup_update/setup_entry.py
@@ -19,36 +19,50 @@
 # please email opensource@seagate.com or cortx-questions@seagate.com.
 #
 """
-Script can be used to insert new setup in the DB
+Script can be used to insert / update setup in the DB
 """
 import os
 import logging
 from urllib.parse import quote_plus
-import yaml
 import json
-
 from pymongo import MongoClient
-from commons import pswdmanager
-from commons.params import DB_HOSTNAME, DB_NAME, SYS_INFO_COLLECTION
 
-LOG = logging.getLogger(__name__)
-fpath = "setup_entry.json"
-setupname="unique"
-setup_query ={"setupname":setupname}
-with open(fpath, 'rb') as json_file:
-    data = json.loads(json_file.read())
-LOG.debug("Database hostname: %s", DB_HOSTNAME)
-LOG.debug("Database name: %s", DB_NAME)
-LOG.debug("Collection name: %s", SYS_INFO_COLLECTION)
-db_creds = pswdmanager.get_secrets(secret_ids=['DBUSER', 'DBPSWD'])
-mongodburi = "mongodb://{0}:{1}@{2}"
-uri = mongodburi.format(quote_plus(db_creds['DBUSER']), quote_plus(db_creds['DBPSWD']), DB_HOSTNAME)
-LOG.debug("URI : %s", uri)
-client = MongoClient(uri)
-setup_db = client[DB_NAME]
-collection_obj = setup_db[SYS_INFO_COLLECTION]
-LOG.debug("Collection obj for DB interaction %s", collection_obj)
-LOG.debug("Setup query : %s", setup_query)
-LOG.debug("Data to be updated : %s", data)
-rdata = collection_obj.insert_one(setup_query, data)
-LOG.debug("Data is updated successfully")
+FPATH = os.path.join(os.path.dirname(__file__), "setup_entry.json")
+DB_HOSTNAME = """cftic1.pun.seagate.com:27017,
+cftic2.pun.seagate.com:27017,
+apollojenkins.pun.seagate.com:27017/
+?authSource=cft_test_results&replicaSet=rs0"""
+DB_NAME = "cft_test_results"
+SYS_INFO_COLLECTION = "r2_systems"
+DBUSER=os.environ['DBUSER']
+DBPSWD=os.environ['DBPSWD']
+
+def insert_new_setup():
+    LOG = logging.getLogger(__name__)
+    with open(FPATH, 'rb') as json_file:
+        data = json.loads(json_file.read())
+    setupname = data['setupname']
+    setup_query = {"setupname": setupname}
+    LOG.debug("Database hostname: %s", DB_HOSTNAME)
+    LOG.debug("Database name: %s", DB_NAME)
+    LOG.debug("Collection name: %s", SYS_INFO_COLLECTION)
+    mongodburi = "mongodb://{0}:{1}@{2}"
+    uri = mongodburi.format(quote_plus(DBUSER), quote_plus(DBPSWD), DB_HOSTNAME)
+    LOG.debug("URI : %s", uri)
+    client = MongoClient(uri)
+    setup_db = client[DB_NAME]
+    collection_obj = setup_db[SYS_INFO_COLLECTION]
+    LOG.debug("Collection obj for DB interaction %s", collection_obj)
+    LOG.debug("Setup query : %s", setup_query)
+    LOG.debug("Data to be updated : %s", data)
+    import pdb
+    pdb.set_trace()
+    if not collection_obj.find(setup_query).count():
+        rdata = collection_obj.update(setup_query, {'$set': data})
+    if collection_obj.find(setup_query).count():
+        LOG.error("Setup entry failed.")
+    else:
+        LOG.debug("Data is updated successfully")
+
+if __name__ == '__main__':
+    insert_new_setup()
