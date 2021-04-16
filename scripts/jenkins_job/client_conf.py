4bf3a2c3c13029f718753f87480a49d950c5fb3a
Sonal Kalbende <sonal.kalbende@seagate.com>
2021-04-16 16:33:37 +0530

    som emore
    Signed-off-by: Sonal Kalbende <sonal.kalbende@seagate.com>

--- scripts/jenkins_job/client_conf.py
+++ scripts/jenkins_job/client_conf.py
@@ -25,11 +25,28 @@ Setup file for client configuration for executing the R2 regression.
 import os
 import logging
 import json
+import subprocess
 from commons.helpers.node_helper import Node
 
 
 # Global Constants
 LOGGER = logging.getLogger(__name__)
+
+def run_cmd(cmd):
+    """
+    Execute bash commands on the host
+    :param str cmd: command to be executed
+    :return: command output
+    :rtype:
+    """
+    print("Executing command: {}".format(cmd))
+    proc = subprocess.Popen(cmd, shell=True,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+
+    result = str(proc.communicate())
+    print("output = {}".format(result))
+    return result
 
 def create_db_entry(hostname, username, password, ip_addr):
     """
@@ -85,7 +102,6 @@ def main():
     host_passwd = os.getenv("HOST_PASS")
     client_passwd = "seagate"
     nd_obj_host = Node(hostname=host, username=uname, password=host_passwd)
-    nd_obj_client = Node(hostname=client, username=uname, password=client_passwd)
     # Get the cluster and mgnt IPs
     cmd = "cat /etc/hosts"
     output = nd_obj_host.execute_cmd(cmd, read_lines=True)
@@ -96,18 +112,16 @@ def main():
             clstr_ip = line.split( )[0]
     os.environ["CLUSTR_IP"] = clstr_ip
     os.environ["CSM_MGMT_IP"] = mgmnt_ip
-    cmd = "mkdir -p /etc/ssl/stx-s3-clients/s3/"
-    nd_obj_client.execute_cmd(cmd, read_lines=True)
+    run_cmd("mkdir -p /etc/ssl/stx-s3-clients/s3/")
     remote_path= "/opt/seagate/cortx/provisioner/srv/components/s3clients/files/ca.crt"
     local_path= "/etc/ssl/stx-s3-clients/s3/ca.crt"
     if os.path.exists(local_path):
-        cmd = "rm -f {}".format(local_path)
-        nd_obj_client.execute_cmd(cmd, read_lines=True)
+        run_cmd("rm -f {}".format(local_path))
     nd_obj_host.copy_file_to_local(remote_path=remote_path, local_path=local_path)
     set_s3_endpoints(clstr_ip)
     create_db_entry(host, uname, host_passwd, mgmnt_ip)
-    cmd = "python3.7 /root/workspace/cortx-test/tools/setup_update/setup_entry.py --dbuser datawrite --dbpassword seagate@123"
-    nd_obj_client.execute_cmd(cmd, read_lines=True)
+    run_cmd("python3.7 /root/workspace/cortx-test/tools/setup_update/setup_entry.py "
+            "--dbuser datawrite --dbpassword seagate@123")
 
 if __name__ == "__main__":
     main()
