3e438f40eb496c98e4ffa7c65f05caa97964080d
seagate-sarang-sawant <70256535+seagate-sarang-sawant@users.noreply.github.com>
2021-04-23 13:22:54 +0530

    EOS-19299 Uppercase logger.

--- libs/di/di_test_framework.py
+++ libs/di/di_test_framework.py
@@ -41,7 +41,7 @@ from commons import params
 if sys.platform != 'win32':
     import fcntl
 
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 uploadObjects = []
 
 
@@ -64,7 +64,7 @@ class Uploader(object):
             s3 = boto3.resource('s3', aws_access_key_id=access_key, aws_secret_access_key=secret_key,
                                 endpoint_url="https://s3.seagate.com")
         except Exception as e:
-            logger.info(
+            LOGGER.info(
                 f'could not create s3 object for user {user_name} with '
                 f'access key {access_key} secret key {secret_key} exception:{e}')
             return
@@ -74,17 +74,17 @@ class Uploader(object):
                 file1 = open(di_params.DATASET_FILES, "r")
                 obj_file_paths = file1.readlines()
             except Exception as e:
-                logger.info(f'could not access file {di_params.DATASET_FILES} exception:{e}')
+                LOGGER.info(f'could not access file {di_params.DATASET_FILES} exception:{e}')
                 return
             else:
-                logger.info(f'able to access file {di_params.DATASET_FILES}')
+                LOGGER.info(f'able to access file {di_params.DATASET_FILES}')
 
             try:
                 s3.create_bucket(Bucket=bucket)
             except Exception as e:
-                logger.info(f'could not create create bucket {bucket} exception:{e}')
+                LOGGER.info(f'could not create create bucket {bucket} exception:{e}')
             else:
-                logger.info(f'create bucket {bucket} Done')
+                LOGGER.info(f'create bucket {bucket} Done')
 
             for each_line in obj_file_paths:
                 reg = '\(\'(.+)\''
@@ -97,9 +97,9 @@ class Uploader(object):
                                                    os.path.basename(each_file_path))
 
                     except Exception as e:
-                        logger.info(f'{each_file_path} in bucket {bucket} Upload caught exception: {e}')
+                        LOGGER.info(f'{each_file_path} in bucket {bucket} Upload caught exception: {e}')
                     else:
-                        logger.info(f'{each_file_path} in bucket {bucket} Upload Done')
+                        LOGGER.info(f'{each_file_path} in bucket {bucket} Upload Done')
                         md5sum = hashlib.md5(open(each_file_path, 'rb').read()).hexdigest()
 
                         obj_name = os.path.basename(each_file_path)
@@ -119,15 +119,15 @@ class Uploader(object):
 
     @staticmethod
     def start(users):
-        logger.info('Starting uploads')
+        LOGGER.info('Starting uploads')
         try:
             os.remove(di_params.uploadFinishedFileName)
         except Exception as e:
-            logger.info(f'file not able to remove: {e}')
+            LOGGER.info(f'file not able to remove: {e}')
         try:
             os.remove(di_params.UPLOADED_FILES)
         except Exception as e:
-            logger.info(f'file not able to remove: {e}')
+            LOGGER.info(f'file not able to remove: {e}')
 
         users_path = os.path.join(params.LOG_DIR, params.USER_JSON)
         config_utils.create_content_json(users_path, users)
@@ -140,7 +140,7 @@ class Uploader(object):
             p.start()
         for p in jobs:
             p.join()
-        logger.info('Upload Done for all users')
+        LOGGER.info('Upload Done for all users')
         with open(di_params.uploadFinishedFileName, 'w') as f:
             pass
 
@@ -161,7 +161,7 @@ class DIChecker(object):
                                     aws_secret_access_key=secret_key,
                                     endpoint_url=di_params.S3_ENDPOINT)
             except Exception as e:
-                logger.error(
+                LOGGER.error(
                     f'could not create s3 object for user {user_name} with access '
                     f'key {access_key} secret key {secret_key} exception:{e}')
 
@@ -194,21 +194,21 @@ class DIChecker(object):
                 if not os.path.exists(basepath):
                     DIChecker.mkdirs(basepath)
             except Exception as e:
-                logger.error(f"Error while creating directory for user {user}")
+                LOGGER.error(f"Error while creating directory for user {user}")
 
             try:
                 s3 = DIChecker.s3ObjectList[user]
             except Exception as fault:
                 print(fault)
-                logger.error(f'No S3 Connection for user {kwargs} in S3 sessions list {fault}')
-                logger.error(f"Won't be able to download object {kwargs} without connection")
+                LOGGER.error(f'No S3 Connection for user {kwargs} in S3 sessions list {fault}')
+                LOGGER.error(f"Won't be able to download object {kwargs} without connection")
                 return
             try:
                 s3.meta.client.download_file(bucket, objectpath, objpth)
-                logger.info(f'downloaded object : {kwargs}')
+                LOGGER.info(f'downloaded object : {kwargs}')
             except Exception as e:
                 print(e)
-                logger.error(f'Final object download failed for {kwargs} with exception {e}')
+                LOGGER.error(f'Final object download failed for {kwargs} with exception {e}')
                 DIChecker.failedFilesServerError.append(kwargs)
             else:
                 print("Downloaded file '{}' from '{}'".format(objectpath, bucket))
@@ -233,17 +233,17 @@ class DIChecker(object):
                         os.system(rmLocalObject)
 
                 if objcsum == csum.strip():
-                    logger.info(
+                    LOGGER.info(
                         "download object checksum {} matches provided checksum {} for"
                         " file {}".format(csum, objcsum, objectpath))
                 else:
-                    logger.error(
+                    LOGGER.error(
                         "download object checksum {} does not matches provided "
                         "checksum {} for file {}".format(csum, objcsum, objectpath))
                     DIChecker.failedFiles.append(kwargs)
         except Exception as fault:
-            logger.exception(fault)
-            logger.error(f'Exception occurred for item {kwargs} with exception {fault}')
+            LOGGER.exception(fault)
+            LOGGER.error(f'Exception occurred for item {kwargs} with exception {fault}')
 
     @classmethod
     def verify_data_integrity(cls, users):
@@ -267,7 +267,7 @@ class DIChecker(object):
 
         if len(uploadedFiles) == 0:
             print("uploaded data not found, exiting script")
-            logger.info("uploaded data not found, exiting script")
+            LOGGER.info("uploaded data not found, exiting script")
             exit(1)
 
         if os.path.exists(di_params.deleteOpFileName):
@@ -280,7 +280,7 @@ class DIChecker(object):
             if len(f) == 4:
                 deletedDict[(f[0], f[1], f[2])] = f[3]
             else:
-                logger.error("Skipped considering deleted file {}".format(f))
+                LOGGER.error("Skipped considering deleted file {}".format(f))
 
         for i in range(1, ulen + 1):
             try:
@@ -289,8 +289,8 @@ class DIChecker(object):
                     DIChecker.mkdirs(os.path.join(di_params.DOWNLOAD_HOME,
                                                   ManagementOPs.user_prefix + str(i)))
             except (OSError, Exception) as err:
-                logger.error(str(err))
-                logger.error(f"Error while creating directory for user {i}")
+                LOGGER.error(str(err))
+                LOGGER.error(f"Error while creating directory for user {i}")
 
         for ix, ent in enumerate(uploadedFiles, 1):
             if (ent[0], ent[1], ent[2]) in deletedDict:
@@ -306,11 +306,11 @@ class DIChecker(object):
             kwargs['secret'] = users.get(ent[0])[1]
             workQ.put(kwargs)
             workers.wenque(workQ)
-            logger.info(f"Enqueued item {ix} for download and checksum compare")
+            LOGGER.info(f"Enqueued item {ix} for download and checksum compare")
             # if workQ is not None:
             #    workQ.join()
             # workQ = None
-        logger.info(f"processed items {ix} for data integrity check")
+        LOGGER.info(f"processed items {ix} for data integrity check")
 
         summary['failed_files'] = len(cls.failedFiles) + len(cls.failedFilesServerError)
         summary['uploaded_files'] = ix
@@ -323,10 +323,10 @@ class DIChecker(object):
                 wr.writerows(cls.failedFiles)
 
         for item in cls.failedFiles:
-            logger.error(f'checksum mismatch for {item}')
+            LOGGER.error(f'checksum mismatch for {item}')
 
         for item in cls.failedFilesServerError:
-            logger.error(f'Server Error for {item}')
+            LOGGER.error(f'Server Error for {item}')
 
         if len(cls.failedFilesServerError) > 0:
             keys = cls.failedFilesServerError[0].keys()
@@ -335,11 +335,11 @@ class DIChecker(object):
                 wr.writerows(cls.failedFilesServerError)
 
         workers.end_workers()
-        logger.info('Workers shutdown completed successfully')
-        logger.info("Test run summary Uploaded files {}  "
+        LOGGER.info('Workers shutdown completed successfully')
+        LOGGER.info("Test run summary Uploaded files {}  "
                     "Deleted Files {} ".format(summary['uploaded_files'],
                                                summary['deleted_files']))
-        logger.info("Failed files were {} and Checksum verified "
+        LOGGER.info("Failed files were {} and Checksum verified "
                     "for Files {} ".format(summary['failed_files'],
                                            summary['checksum_verified']))
         assert len(cls.failedFiles) < 1
