f6b14777d35bec40cc1286e298298273bc8bd91b
VikasKumarC <vikas.kumar@seagate.com>
2021-04-24 18:30:50 +0530

    cortxcli incorporated

--- tests/s3/test_bucket_workflow_operations.py
+++ tests/s3/test_bucket_workflow_operations.py
@@ -32,11 +32,11 @@ from commons.errorcodes import error_handler
 from commons.exceptions import CTException
 from commons.utils import assert_utils
 from commons.utils import system_utils
+from config import S3_CFG
 from libs.s3 import s3_test_lib
 from libs.s3 import iam_test_lib
 from libs.s3 import s3_acl_test_lib
-from libs.s3 import LDAP_USERNAME, LDAP_PASSWD
-
+from libs.s3.cortxcli_test_lib import CortxCliTestLib
 
 S3_TEST_OBJ = s3_test_lib.S3TestLib()
 IAM_OBJ = iam_test_lib.IamTestLib()
@@ -45,6 +45,10 @@ ACL_OBJ = s3_acl_test_lib.S3AclTestLib()
 
 class TestBucketWorkflowOperations:
     """Bucket Workflow Operations Test suite."""
+
+    filename = None
+    file_path = None
+    cortx_obj = None
 
     @classmethod
     def setup_class(cls):
@@ -56,6 +60,7 @@ class TestBucketWorkflowOperations:
         cls.log = logging.getLogger(__name__)
         cls.log.info("STARTED: Setup suite operations")
         cls.account_name = "bktwrkflowaccnt"
+        cls.s3acc_password = S3_CFG["CliConfig"]["s3_account"]["password"]
         cls.folder_path = os.path.join(
             TEST_DATA_FOLDER, "TestBucketWorkflowOperations")
         if not system_utils.path_exists(cls.folder_path):
@@ -84,6 +89,7 @@ class TestBucketWorkflowOperations:
         self.log.info("STARTED: Setup operations")
         self.filename = "bkt_workflow{}.txt".format(str(time.time()))
         self.file_path = os.path.join(self.folder_path, self.filename)
+        self.cortx_obj = CortxCliTestLib()
         self.log.info("ENDED: Setup operations")
 
     def teardown_method(self):
@@ -98,23 +104,20 @@ class TestBucketWorkflowOperations:
             system_utils.remove_file(self.file_path)
         self.log.info("ENDED: Teardown operations")
 
-    def delete_iam_account(self, account_name):
+    def delete_iam_account(self, account_name, password=None):
         """
         Delete iam account.
 
-        :param account: account name.
+        :param account_name: name of account to be deleted.
+        :param password: password for account to be deleted
         """
-        self.log.info("Deleting IAM accounts")
-        acc_list = IAM_OBJ.list_accounts_s3iamcli(
-            LDAP_USERNAME, LDAP_PASSWD)[1]
-        self.log.info(acc_list)
-        all_acc = [acc["AccountName"]
-                   for acc in acc_list if account_name == acc["AccountName"]]
-        self.log.info(all_acc)
-        for acc_name in all_acc:
-            resp = IAM_OBJ.reset_access_key_and_delete_account_s3iamcli(acc_name)
-            assert_utils.assert_true(resp[0], resp[1])
-        self.log.info("Deleted IAM accounts successfully")
+        self.log.info("Deleting: IAM accounts")
+        if password is None:
+            password = self.s3acc_password
+        resp = self.cortx_obj.delete_account_cortxcli(
+            account_name=account_name, password=password)
+        assert_utils.assert_true(resp[0], resp[1])
+        self.log.info("Deleted: IAM accounts successfully")
 
     def delete_s3_buckets(self, bucket):
         """
@@ -243,9 +246,10 @@ class TestBucketWorkflowOperations:
             "STARTED: Bucket name with less than 3 characters and more than 63 characters")
         self.log.info(
             "Creating buckets with name less than 3 and more than 63 character length")
-        bkt_name2_64 = ["a2",
-        "bktworkflow-seagateeosbucket-8537-bktworkflow-seagateeosbucketsbktworkflow"
-        "-seagateeosbucket"]
+        bkt_name2_64 = [
+            "a2",
+            "bktworkflow-seagateeosbucket-8537-bktworkflow-seagateeosbucketsbktworkflow"
+            "-seagateeosbucket"]
         for each_bucket in bkt_name2_64:
             try:
                 resp = S3_TEST_OBJ.create_bucket(each_bucket)
@@ -458,7 +462,8 @@ class TestBucketWorkflowOperations:
         resp = S3_TEST_OBJ.bucket_count()
         self.log.info(resp)
         assert resp[0], resp[1]
-        assert_utils.assert_equal(len(bkt_list), 100, "failed to create 100 buckets")
+        assert_utils.assert_equal(
+            len(bkt_list), 100, "failed to create 100 buckets")
         resp = S3_TEST_OBJ.delete_multiple_buckets(bkt_list)
         self.log.info(resp)
         assert_utils.assert_true(resp[0], resp1[1])
@@ -584,8 +589,10 @@ class TestBucketWorkflowOperations:
             assert resp[0], resp[1]
             assert resp[1] == bucket_name, resp[1]
             bucket_list.append(bucket_name)
-        assert_utils.assert_equal(len(bucket_list), 10, "Failed to create 10 bucket: {}".format(
-            bucket_list))
+        assert_utils.assert_equal(
+            len(bucket_list),
+            10,
+            "Failed to create 10 bucket: {}".format(bucket_list))
         self.log.info("Multiple buckets are created")
         self.log.info("Deleting multiple buckets")
         resp = S3_TEST_OBJ.delete_multiple_buckets(bucket_list)
@@ -791,8 +798,7 @@ class TestBucketWorkflowOperations:
         resp = S3_TEST_OBJ.bucket_location(
             bkt_name)
         assert resp[0], resp[1]
-        assert resp[1]["LocationConstraint"] == \
-            "us-west-2", resp[1]
+        assert resp[1]["LocationConstraint"] == "us-west-2", resp[1]
         self.delete_s3_buckets(bkt_name)
         self.log.info("ENDED: Verification of bucket location")
 
@@ -888,20 +894,17 @@ class TestBucketWorkflowOperations:
             "STARTED: create bucket and upload objects from account1 and dont give"
             " any permissions to account2 and delete multiple objects from account2")
         bktname = "bktworkflow-434-{}".format(str(time.time()))
-        acc_name_2 = "bktwrkflowaccnt434_{}".format(str(time.time()))
+        acc_name_2 = "{}_{}".format(self.account_name, str(time.time()))
         emailid_2 = "acltestaccnt434_{}@seagate.com".format(str(time.time()))
         self.log.info(
             "Step : Creating account with name %s and email_id %s",
             acc_name_2, emailid_2)
-        create_account = IAM_OBJ.create_account_s3iamcli(
-            acc_name_2,
-            emailid_2,
-            LDAP_USERNAME,
-            LDAP_PASSWD)
+        create_account = self.cortx_obj.create_account_cortxcli(
+            account_name=acc_name_2, account_email=emailid_2, password=self.s3acc_password)
         assert create_account[0], create_account[1]
         access_key = create_account[1]["access_key"]
         secret_key = create_account[1]["secret_key"]
-        self.log.info("Step Successfully created the s3iamcli account")
+        self.log.info("Step Successfully created the cortxcli account")
         s3_obj_2 = s3_test_lib.S3TestLib(
             access_key=access_key, secret_key=secret_key)
         self.log.info(
@@ -957,20 +960,17 @@ class TestBucketWorkflowOperations:
             "STARTED: create bucket and upload objects from account1 and dont give"
             " any permissions to account2 and delete multiple objects from account2")
         bktname = "bktworkflow-435-{}".format(str(time.time()))
-        acc_name_2 = "bktwrkflowaccnt435_{}".format(str(time.time()))
+        acc_name_2 = "{}_{}".format(self.account_name, str(time.time()))
         emailid_2 = "acltestaccnt435_{}@seagate.com".format(str(time.time()))
         self.log.info(
             "Step : Creating account with name %s and email_id %s",
             acc_name_2, emailid_2)
-        create_account = IAM_OBJ.create_account_s3iamcli(
-            acc_name_2,
-            emailid_2,
-            LDAP_USERNAME,
-            LDAP_PASSWD)
+        create_account = self.cortx_obj.create_account_cortxcli(
+            account_name=acc_name_2, account_email=emailid_2, password=self.s3acc_password)
         assert create_account[0], create_account[1]
         access_key = create_account[1]["access_key"]
         secret_key = create_account[1]["secret_key"]
-        self.log.info("Step Successfully created the s3iamcli account")
+        self.log.info("Step Successfully created the cortxcli account")
         s3_obj_2 = s3_test_lib.S3TestLib(
             access_key=access_key, secret_key=secret_key)
         self.log.info(
