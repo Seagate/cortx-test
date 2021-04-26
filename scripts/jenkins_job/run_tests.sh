63613aaacaeac61a1fba396ba255a1afcfecb19d
Sonal Kalbende <sonal.kalbende@seagate.com>
2021-04-26 16:31:56 +0530

    passing tests
    Signed-off-by: Sonal Kalbende <sonal.kalbende@seagate.com>

--- scripts/jenkins_job/run_tests.sh
+++ scripts/jenkins_job/run_tests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 TGT_NAME=`echo "$HOSTNAME" | sed  's/\..*//'`
 echo "Target node entry name: $TGT_NAME"
-pytest tests/csm/cli/test_cli_csm_user.py::TestCliCSMUser --local True --target $TGT_NAME
+pytest tests/blackbox/test_cortxcli.py::TestCortxcli::test_2394 --local True --target $TGT_NAME
+pytest tests/blackbox/test_cortxcli.py::TestCortxcli::test_2396 --local True --target $TGT_NAME
+
