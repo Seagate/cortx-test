f6275cf63f326053ea3fb222268466f43384c029
Dhananjay Dandapat <66424932+dhananjayd1@users.noreply.github.com>
2021-05-05 20:28:50 +0530

    Refatore s3 tests suite: Ready for review (#283)
    * Refatore s3 tests suite
    - Moved few helper libs from tests suite to helper packages
    - Removed few config
    - Minimised few lines of code
    - Removed unused codes
    
    Signed-off-by: Dhananjay Dandapat <dhananjay.dandapat@seagate.com>
    
    * Review changes incorporated
    
    Signed-off-by: Dhananjay Dandapat <dhananjay.dandapat@seagate.com>
    
    * Incorporated Review changes
    
    Signed-off-by: Dhananjay Dandapat <dhananjay.dandapat@seagate.com>
    
    * fix codacy
    
    Signed-off-by: Dhananjay Dandapat <dhananjay.dandapat@seagate.com>
    
    * Resolved key error.
    Signed-off-by: Dhananjay Dandapat <dhananjay.dandapat@seagate.com>
    
    * Added audit fix after run
    Signed-off-by: Dhananjay Dandapat <dhananjay.dandapat@seagate.com>
    
    Signed-off-by: Dhananjay Dandapat <dhananjay.dandapat@seagate.com>

--- config/__init__.py
+++ config/__init__.py
@@ -35,6 +35,7 @@ from commons.params import PROV_TEST_CONFIG_PATH
 from commons.params import DI_CONFIG_PATH
 from commons.params import DATA_PATH_CONFIG_PATH
 from commons.params import S3_BKT_TEST_CONFIG
+from commons.params import S3_LDAP_TEST_CONFIG
 from commons.params import S3_USER_ACC_MGMT_CONFIG_PATH
 
 
@@ -80,6 +81,7 @@ S3_CFG = configmanager.get_config_wrapper(fpath=S3_CONFIG, target=target, target
 S3_OBJ_TST = configmanager.get_config_wrapper(fpath=S3_OBJ_TEST_CONFIG, target=target, target_key="s3")
 S3_BKT_TST = configmanager.get_config_wrapper(fpath=S3_BKT_TEST_CONFIG, target=target,
                                             target_key="s3")
+S3_LDAP_TST_CFG = configmanager.get_config_wrapper(fpath=S3_LDAP_TEST_CONFIG, target=target)
 RAS_VAL = configmanager.get_config_wrapper(fpath=RAS_CONFIG_PATH,
                                            target=target, target_key="csm")
 CMN_DESTRUCTIVE_CFG = configmanager.get_config_wrapper(fpath=COMMON_DESTRUCTIVE_CONFIG_PATH)
