2848a895cc6c8b590be6bb6224f3adf01070c86f
Vishal Dhobale <69301843+DhobaleVishal@users.noreply.github.com>
2021-05-18 15:49:02 +0530

    Eos 19971 ta put object s3 fault tolerance (#329)
    * Method: Create file using tool fallocate.
    
    Signed-off-by: Vishal Dhobale <vishal.dhobale@seagate.com>
    
    * Method: Inject(enable/disable) fault tolerance in s3server.
    
    Signed-off-by: Vishal Dhobale <vishal.dhobale@seagate.com>
    
    * Methods to get file, put file to remote path.
    
    Signed-off-by: Vishal Dhobale <vishal.dhobale@seagate.com>
    
    * Method: update s3config.yaml as per section, parameter.
    
    Signed-off-by: Vishal Dhobale <vishal.dhobale@seagate.com>
    
    * Automated: All putobject works fine with faultinjection disabled/enable.
    
    Signed-off-by: Vishal Dhobale <vishal.dhobale@seagate.com>
    
    * Addressed & incorporated review comments from PR 329 also fixed pylint issue.
    
    Signed-off-by: Vishal Dhobale <vishal.dhobale@seagate.com>
    
    * Added getobject api scenarios
    
    * Updated
    
    * Added missing test skip reasons.
    
    Signed-off-by: Vishal Dhobale <vishal.dhobale@seagate.com>
    
    * minor changes.
    
    Signed-off-by: Vishal Dhobale <vishal.dhobale@seagate.com>
    
    Co-authored-by: VikasKumarC <vikas.kumar@seagate.com>

--- commons/constants.py
+++ commons/constants.py
@@ -83,6 +83,7 @@ NODE_PREFIX = "eosnode-"
 
 """ S3 constants """
 const.S3_CONFIG = "/opt/seagate/cortx/s3/conf/s3config.yaml"
+const.LOCAL_S3_CONFIG = "/tmp/s3config.yaml"
 const.CA_CERT_PATH = "/opt/seagate/cortx/provisioner/srv/components/s3clients/files/ca.crt"
 const.REMOTE_DEFAULT_DIR = "/var/motr"
 const.CFG_FILES = ["/etc/haproxy/haproxy.cfg",
@@ -96,6 +97,9 @@ const.CRASH_COMMANDS = ["ls -l /var/crash", "ls -lR /var/motr | grep core"],
 const.AUTHSERVER_LOG_PATH = "/var/log/seagate/auth/server/app.log"
 const.S3CMD = "s3cmd"
 const.S3FS = "s3fs-fuse"
+const.SLAPD = "slapd"
+const.HAPROXY = "haproxy"
+const.S3AUTHSERVER = "s3authserver"
 const.HAPROXY_LOG_PATH = "/var/log/haproxy.log"
 const.S3_LOG_PATH = "/var/log/seagate/s3"
 const.SUPPORT_BUNDLE_SUCCESS_MSG = "S3 support bundle generated successfully"
