bd87b66b5af3bbfc64c579ab27ebf6b25099bde1
Sonal Kalbende <sonal.kalbende@seagate.com>
2021-05-21 14:31:12 +0530

    min
    Signed-off-by: Sonal Kalbende <sonal.kalbende@seagate.com>

--- scripts/jenkins_job/run_tests.sh
+++ scripts/jenkins_job/run_tests.sh
@@ -2,5 +2,4 @@
 TGT_NAME=`echo "$HOSTNAME" | sed  's/\..*//'`
 echo "Target node entry name: $TGT_NAME"
 
-pytest scripts/jenkins_job/aws_configure.py --local True --target ${TGT_NAME}
 pytest -m "release_regression" --local True --target ${TGT_NAME} --junitxml "log/latest/results.xml" --html "log/latest/results.html"
