edc6a19a84fb7eec7e5ffca5d13756cbc9534e98
kulrk <rohan.1.kulkarni@seagate.com>
2021-05-24 11:18:37 +0530

    added run.sh script

---
+++ ci_tools/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+python3.7 -u /cortx-test/tools/clone_test_plan/clone_test_plan.py -tp=$TEST_PLAN_NUMBER -b=$BUILD -bt=$BUILD_TYPE -s=$SETUP_TYPE -c='TEST-19657'
+
+INPUT=cloned_tp_info.csv
+OLDIFS=$IFS
+IFS=','
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read tp_id te_id
+do
+	  echo "tp_id : $tp_id"
+	  echo "te_id : $te_id"
+    test_exe=$(echo $te_id | sed -e 's/\r//g')
+    python3.7 -u /cortx-test/testrunner.py -te=$test_exe -tp=$tp_id -tg=$Target_Node -b=$BUILD -t=$BUILD_TYPE -d=${DB_Update} --force_serial_run ${Sequential_Execution}
+done < $INPUT
+IFS=$OLDIFS
