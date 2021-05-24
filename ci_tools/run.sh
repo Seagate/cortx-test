2e33869292a45d7ac47e26a810bcab4cdab7d279
kulrk <rohan.1.kulkarni@seagate.com>
2021-05-24 06:55:16 +0530

    modified run.sh

--- ci_tools/run.sh
+++ ci_tools/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python3 -u tools/clone_test_plan/clone_test_plan.py -tp=TEST-20074 -b=175 -bt=stable -s=regular -c='TEST-19657'
+python3 -u tools/clone_test_plan/clone_test_plan.py -tp=$TEST_PLAN_NUMBER -b=$BUILD -bt=$BUILD_TYPE -s=$SETUP_TYPE -c='TEST-19657'
 
 INPUT=cloned_tp_info.csv
 OLDIFS=$IFS
@@ -11,6 +11,6 @@ do
 	echo "tp_id : $tp_id"
 	echo "te_id : $te_id"
     test_exe=$(echo $te_id | sed -e 's/\r//g')
-    python3 -u testrunner.py -te=$test_exe -tp=$tp_id -tg=${Target_Node} -b=${Build} -t=${Build_Type} -d=${DB_Update} --force_serial_run ${Sequential_Execution}
+    python3 -u testrunner.py -te=$test_exe -tp=$tp_id -tg=$Target_Node -b=$BUILD -t=$BUILD_TYPE -d=${DB_Update} --force_serial_run ${Sequential_Execution}
 done < $INPUT
 IFS=$OLDIFS
