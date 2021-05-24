d31c351e596ed1fe85e9de8abaddaab01f4c939d
kulrk <rohan.1.kulkarni@seagate.com>
2021-05-24 07:05:49 +0530

    corrected python version

--- ci_tools/run.sh
+++ ci_tools/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python3 -u tools/clone_test_plan/clone_test_plan.py -tp=$TEST_PLAN_NUMBER -b=$BUILD -bt=$BUILD_TYPE -s=$SETUP_TYPE -c='TEST-19657'
+python3.7 -u /cortx-test/tools/clone_test_plan/clone_test_plan.py -tp=$TEST_PLAN_NUMBER -b=$BUILD -bt=$BUILD_TYPE -s=$SETUP_TYPE -c='TEST-19657'
 
 INPUT=cloned_tp_info.csv
 OLDIFS=$IFS
@@ -8,9 +8,9 @@ IFS=','
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
 while read tp_id te_id
 do
-	echo "tp_id : $tp_id"
-	echo "te_id : $te_id"
+	  echo "tp_id : $tp_id"
+	  echo "te_id : $te_id"
     test_exe=$(echo $te_id | sed -e 's/\r//g')
-    python3 -u testrunner.py -te=$test_exe -tp=$tp_id -tg=$Target_Node -b=$BUILD -t=$BUILD_TYPE -d=${DB_Update} --force_serial_run ${Sequential_Execution}
+    python3.7 -u /cortx-test/testrunner.py -te=$test_exe -tp=$tp_id -tg=$Target_Node -b=$BUILD -t=$BUILD_TYPE -d=${DB_Update} --force_serial_run ${Sequential_Execution}
 done < $INPUT
 IFS=$OLDIFS
