88126f45dd972c1d42eeca43a669beb953d35ebe
kulrk <rohan.1.kulkarni@seagate.com>
2021-05-25 17:16:25 +0530

    modified run.sh and Dockerfile

--- ci_tools/run.sh
+++ ci_tools/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python3.7 -u /cortx-test/tools/clone_test_plan/clone_test_plan.py -tp=$TEST_PLAN_NUMBER -b=$BUILD -bt=$BUILD_TYPE -s=$SETUP_TYPE -c='TEST-19657'
+python3.7 -u /cortx-test/tools/clone_test_plan/clone_test_plan.py -tp=$TEST_PLAN_NUMBER -b=$BUILD -bt=$BUILD_TYPE -s=$SETUP_TYPE -c=$COMMENT_JIRA
 
 INPUT=cloned_tp_info.csv
 OLDIFS=$IFS
@@ -11,6 +11,6 @@ do
 	  echo "tp_id : $tp_id"
 	  echo "te_id : $te_id"
     test_exe=$(echo $te_id | sed -e 's/\r//g')
-    python3.7 -u /cortx-test/testrunner.py -te=$test_exe -tp=$tp_id -tg=$Target_Node -b=$BUILD -t=$BUILD_TYPE -d=${DB_Update} --force_serial_run ${Sequential_Execution}
+    python3.7 -u /cortx-test/testrunner.py -te=$test_exe -tp=$tp_id -tg=$Target_Node -b=$BUILD -t=$BUILD_TYPE -d=$DB_UPDATE --force_serial_run=$Sequential_Execution
 done < $INPUT
 IFS=$OLDIFS
