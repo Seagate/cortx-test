650a1fbe81cbcf1c1a52879fe922f1fdaba1ac7e
Dhananjay Dandapat <66424932+dhananjayd1@users.noreply.github.com>
2021-06-15 22:19:05 +0530

    Added libs to invoke IO in async and sync mode along with tests (#339)
    * Added libs to invoke IO in asyn and sync mode along with tests
    did some quick fixes in scripts
    
    Signed-off-by: Dhananjay Dandapat <dhananjay.dandapat@seagate.com>
    
    * Incorporated review changes
    
    Signed-off-by: Dhananjay Dandapat <dhananjay.dandapat@seagate.com>
    
    * Added suggested changes as per user perfs within test.
    Signed-off-by: Dhananjay Dandapat <dhananjay.dandapat@seagate.com>
    
    * Added stop/termintae logic
    Fix Conftest API
    
    Signed-off-by: Dhananjay Dandapat <dhananjay.dandapat@seagate.com>
    
    * Added stop logic using event, testrunner api and sample test file
    Signed-off-by: Dhananjay Dandapat <dhananjay.dandapat@seagate.com>
    
    * Added stop solution in case of test failure
    Added API for test runner
    Added fixes in upload and download files
    Added feature to add all objects data per bucket in user_meta data file
    Added unittest for all Sceanrio
    
    Signed-off-by: Dhananjay Dandapat <dhananjay.dandapat@seagate.com>
    
    * fix download failure due to exit()
    
    Signed-off-by: Dhananjay Dandapat <dhananjay.dandapat@seagate.com>
    
    * fix test runner parallel process
    
    Signed-off-by: Dhananjay Dandapat <dhananjay.dandapat@seagate.com>
    
    * Incoprtorated suggested changes
    
    Signed-off-by: Dhananjay Dandapat <dhananjay.dandapat@seagate.com>
    
    * Added new changes
    
    Signed-off-by: Dhananjay Dandapat <dhananjay.dandapat@seagate.com>
    
    Co-authored-by: sonalk0209 <53035906+sonalk0209@users.noreply.github.com>

---
+++ unittests/test_di_sample.py
@@ -0,0 +1,115 @@
+import time
+import pytest
+import logging
+from conftest import run_io_async
+from libs.di.di_run_man import RunDataCheckManager
+from libs.di.di_mgmt_ops import ManagementOPs
+import threading
+
+
+class TestSample:
+
+    log = logging.getLogger(__name__)
+
+    @classmethod
+    def setup_class(cls):
+        cls.log.info("I am in setup class")
+
+    def setup_method(self):
+        self.log.info("I am in setup method")
+
+    def teardown_method(self):
+        self.log.info("I am in teardown method")
+
+    @classmethod
+    def teardown_class(cls):
+        cls.log.info("I am in Teardown class")
+
+    def test_01(self):
+        mgm_ops = ManagementOPs()
+        users = mgm_ops.create_account_users(nusers=2, use_cortx_cli=False)
+        users = mgm_ops.create_buckets(nbuckets=2, users=users)
+        pref_dir = {"prefix_dir": 'test_01'}
+        run_man_obj = RunDataCheckManager(users=users)
+        run_man_obj.run_io_sequentially(users=users, prefs=pref_dir)
+        time.sleep(320)
+        assert True, "msg"
+
+    def test_02(self, run_io_async):
+        time.sleep(8)
+        assert False, "msg"
+
+    @pytest.mark.usefixtures("run_io_async")
+    def test_03(self):
+        assert True, "msg"
+
+    def test_04(self):
+        mgm_ops = ManagementOPs()
+        users = mgm_ops.create_account_users(nusers=2, use_cortx_cli=False)
+        data = mgm_ops.create_buckets(nbuckets=2, users=users)
+        run_data_chk_obj = RunDataCheckManager(users=data)
+        pref_dir = {"prefix_dir": 'test_04'}
+        run_data_chk_obj.start_io(
+            users=data, buckets=None, files_count=8, prefs=pref_dir)
+        time.sleep(660)
+        run_data_chk_obj.stop_io(users=data, di_check=True)
+        assert True, "msg"
+
+    def test_05(self):
+        mgm_ops = ManagementOPs()
+        users = mgm_ops.create_account_users(nusers=5, use_cortx_cli=False)
+        data = mgm_ops.create_buckets(nbuckets=4, users=users)
+        run_data_chk_obj = RunDataCheckManager(users=data)
+        pref_dir = {"prefix_dir": 'test_05'}
+        event = threading.Event()
+        run_data_chk_obj.start_io_async(
+            users=data, buckets=None, files_count=65, prefs=pref_dir,
+            event=event)
+        event.set()
+        time.sleep(4)
+        print("test", event.is_set())
+        run_data_chk_obj.stop_io_async(users=data, di_check=True)
+        assert True, "msg"
+
+    def test_07(self):
+        mgm_ops = ManagementOPs()
+        users = mgm_ops.create_account_users(nusers=5, use_cortx_cli=False)
+        data = mgm_ops.create_buckets(nbuckets=4, users=users)
+        run_data_chk_obj = RunDataCheckManager(users=data)
+        pref_dir = {"prefix_dir": 'test_07'}
+        run_data_chk_obj.start_io_async(
+            users=data, buckets=None, files_count=65, prefs=pref_dir)
+        time.sleep(4)
+        run_data_chk_obj.stop_io_async(users=data, di_check=True,
+                                       eventual_stop=True)
+        assert True, "msg"
+
+    def test_08(self):
+        mgm_ops = ManagementOPs()
+        users = mgm_ops.create_account_users(nusers=5, use_cortx_cli=False)
+        data = mgm_ops.create_buckets(nbuckets=4, users=users)
+        run_data_chk_obj = RunDataCheckManager(users=data)
+        pref_dir = {"prefix_dir": 'test_08'}
+        run_data_chk_obj.start_io_async(
+            users=data, buckets=None, files_count=35, prefs=pref_dir)
+        run_data_chk_obj.event.set()
+        time.sleep(4)
+        print("test", run_data_chk_obj.event.is_set())
+        run_data_chk_obj.stop_io_async(users=data, di_check=True)
+        assert True, "msg"
+
+    def test_09(self):
+        mgm_ops = ManagementOPs()
+        users = mgm_ops.create_account_users(nusers=2, use_cortx_cli=False)
+        data = mgm_ops.create_buckets(nbuckets=2, users=users,
+                                      use_cortxcli=True)
+        run_data_chk_obj = RunDataCheckManager(users=data)
+        pref_dir = {"prefix_dir": 'test_09'}
+        run_data_chk_obj.start_io(
+            users=data, buckets=None, files_count=8, prefs=pref_dir)
+        time.sleep(8)
+        run_data_chk_obj.stop_io(users=data, di_check=True, eventual_stop=True)
+        assert True, "msg"
+
+    def test_10(self):
+        assert True, "msg"
