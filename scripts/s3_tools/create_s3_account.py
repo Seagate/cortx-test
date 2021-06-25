186a3be307232114e424ae5de4fc6fab38a5a15d
Divya Kachhwaha <divya.kachhwaha@seagate.com>
2021-06-25 12:29:58 +0530

    tested
    Signed-off-by: Divya Kachhwaha <divya.kachhwaha@seagate.com>

--- scripts/s3_tools/create_s3_account.py
+++ scripts/s3_tools/create_s3_account.py
@@ -19,12 +19,20 @@
 # please email opensource@seagate.com or cortx-questions@seagate.com.
 #
 """ Script for creating S3 account. """
-
-import requests
 import argparse
+import requests
 
 
 def login(mgmt_vip, username, password, port=28100, set_secure=True):
+    """[summary]
+
+    :param mgmt_vip: Management IP address
+    :param username: CSM login Username
+    :param password: CSM login password
+    :param port: CSM connection port
+    :param set_secure: True than https will be used else http
+    :return [tuple]: base_url, headers
+    """
     if set_secure:
         start = "https://"
     else:
@@ -45,6 +53,17 @@ def login(mgmt_vip, username, password, port=28100, set_secure=True):
 
 def create_s3_account(account_name, account_email, account_password,
                       mgmt_vip, username, password, **kwargs):
+    """Create S3 account
+
+    :param account_name: S3 aaccount name
+    :param account_email:  S3 account email
+    :param account_password: S3 account password
+    :param mgmt_vip: Management IP address
+    :param username: CSM login Username
+    :param password: CSM login password
+    :return [Dict]:{'account_name': '', 'account_email': '', 'account_id': '', 'canonical_id': '', 
+    'access_key': '', 'secret_key': ''}
+    """
     base_url, headers = login(mgmt_vip, username, password, **kwargs)
     create_s3_url = base_url + "/api/v2/s3_accounts"
     data = {"account_name": account_name,
@@ -53,47 +72,32 @@ def create_s3_account(account_name, account_email, account_password,
     print("URL :", create_s3_url)
     print("Credentials : ", data)
     print("Headers : ", headers)
-    response = requests.post(url=create_s3_url, data=data, verify=False, headers=headers)
+    response = requests.post(url=create_s3_url, data=data, verify=False, headers=headers, json=None)
     print("Response: ", response.status_code)
     print("Response: ", response.json())
-    return response
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
 
 
 if __name__ == '__main__':
     # Setup details
     parser = argparse.ArgumentParser(description='Create s3 account')
-    parser.add_argument('--mgmt_vip', dest='mgmt_vip', help='Management IP',
-                        type=str, default="ssc-vm-5455.colo.seagate.com")
-    parser.add_argument(
-        '--username',
-        dest='username',
-        help='CSM admin username',
-        type=str,
-        default="admin")
-    parser.add_argument(
-        '--password',
-        dest='password',
-        help='CSM admin password',
-        type=str,
-        default="Seagate@1")
-    parser.add_argument(
-        '--account_name',
-        dest='account_name',
-        help='New S3 account name',
-        type=str,
-        default="dk_s3")
+    parser.add_argument('--mgmt_vip', dest='mgmt_vip', help='Management IP', type=str)
+    parser.add_argument('--username', dest='username', help='CSM admin username', type=str)
+    parser.add_argument('--password', dest='password', help='CSM admin password', type=str)
+    parser.add_argument('--account_name', dest='account_name', help='New S3 account name', type=str)
     parser.add_argument(
         '--account_email',
         dest='account_email',
         help='New S3 account email',
-        type=str,
-        default="s3@seagate.com")
+        type=str)
     parser.add_argument(
         '--account_password',
         dest='account_password',
         help='New S3 account password',
-        type=str,
-        default="Seagate@1")
+        type=str)
     args = parser.parse_args()
     create_s3_account(
         args.account_name,
