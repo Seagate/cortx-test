3b5e998d7b3b4f0ae6070c401164dcf8e06661bc
kulrk <rohan.1.kulkarni@seagate.com>
2021-06-25 17:58:59 +0530

    codacy fixes
    Signed-off-by: kulrk <rohan.1.kulkarni@seagate.com>

--- run.sh
+++ run.sh
@@ -7,13 +7,13 @@ export LIB=/usr/lib/python3.7
 export SITE_PKGS=/usr/lib/python3.7/site-packages
 export PYTHONPATH=$LIB:$SITE_PKGS:$CORTX_TEST_ROOT
 
-$PYTHON_VAR ci_tools/aws_configure.py --access_key=$AWS_ACCESS_KEY_ID --secret_key=$AWS_SECRET_ACCESS_KEY
+"$PYTHON_VAR" ci_tools/aws_configure.py --access_key="$AWS_ACCESS_KEY_ID" --secret_key="$AWS_SECRET_ACCESS_KEY"
 
 if [ "$SKIP_TE" != "None" ];
 then
-  $PYTHON_VAR -u tools/clone_test_plan/clone_test_plan.py -tp=$TEST_PLAN_NUMBER -b=$BUILD -s=$SETUP_TYPE -c=$COMMENT_JIRA -br=$BRANCH -n=$NODES -sr=$SERVER_TYPE -e=$ENCLOSURE_TYPE -st $SKIP_TE
+  "$PYTHON_VAR" -u tools/clone_test_plan/clone_test_plan.py -tp="$TEST_PLAN_NUMBER" -b="$BUILD" -s="$SETUP_TYPE" -c="$COMMENT_JIRA" -br="$BRANCH" -n="$NODES" -sr="$SERVER_TYPE" -e="$ENCLOSURE_TYPE" -st "$SKIP_TE"
 else
-  $PYTHON_VAR -u tools/clone_test_plan/clone_test_plan.py -tp=$TEST_PLAN_NUMBER -b=$BUILD -s=$SETUP_TYPE -c=$COMMENT_JIRA -br=$BRANCH -n=$NODES -sr=$SERVER_TYPE -e=$ENCLOSURE_TYPE
+  "$PYTHON_VAR" -u tools/clone_test_plan/clone_test_plan.py -tp="$TEST_PLAN_NUMBER" -b="$BUILD" -s="$SETUP_TYPE" -c="$COMMENT_JIRA" -br="$BRANCH" -n="$NODES" -sr="$SERVER_TYPE" -e="$ENCLOSURE_TYPE"
 fi
 
 INPUT=cloned_tp_info.csv
@@ -24,8 +24,9 @@ while read tp_id te_id old_te
 do
     echo "tp_id : $tp_id"
     echo "te_id : $te_id"
+    echo "old_te : $old_te"
     test_exe=$(echo $te_id | sed -e 's/\r//g')
-    $PYTHON_VAR -u testrunner.py -te=$test_exe -tp=$tp_id -tg=$TARGET_NODE -b=$BUILD -t=$BUILD_TYPE -d=$DB_UPDATE --force_serial_run=$SEQUENTIAL_EXECUTION
+    "$PYTHON_VAR" -u testrunner.py -te="$test_exe" -tp="$tp_id" -tg="$TARGET_NODE" -b="$BUILD" -t="$BUILD_TYPE" -d="$DB_UPDATE" --force_serial_run="$SEQUENTIAL_EXECUTION"
 done < $INPUT
 IFS=$OLDIFS
 
