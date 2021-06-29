a4246a6e9375b9828ac47ec56dbf4cbaf1a8cccf
Rajesh Pratap Singh Chouhan <rajesh.chouhan@seagate.com>
2021-06-29 13:17:51 +0530

    Updated existing tests based on new feature F-45G and UI changes (#452)
    Signed-off-by: rajesh.chouhan <rajesh.chouhan@seagate.com>
    
    Co-authored-by: swapnil-khandare <66417964+swapnil-khandare@users.noreply.github.com>

--- robot/resources/common/element_locators.py
+++ robot/resources/common/element_locators.py
@@ -9,7 +9,8 @@ CSM_LOGIN_FAIL_MSG_ID = 'login-failsmsg'
 
 # CSM Dashboard Locators
 LOGGED_IN_USER_NAME_ID = 'header-username'
-LOG_OUT_ID = 'logout-icon'
+USER_DROPDOWN_ID = 'logout-icon'
+LOG_OUT_ID = 'logout-link'
 CAPACITY_TOTAL_LABEL_ID = 'capacity-total-text'
 CAPACITY_AVAILABLE_LABEL_ID = 'capacity-available-text'
 CAPACITY_USED_LABEL_ID = 'capacity-used-text'
@@ -22,11 +23,13 @@ TOTAL_CAPACITY_VALUE_XPATH = "//*[@id='capacity-total']/td[3]"
 
 # Menus
 MANAGE_MENU_ID = 'Manage'
-S3_ACCOUNTS_TAB_ID = 's3accountstab'
+ADMINISTRATIVE_USER_TAB_ID = "tab-1"
+CSM_S3_ACCOUNTS_TAB_ID = 'tab-2'
 SETTINGS_ID = 'Settings'
 EMAIL_NOTIFICATION_ID = 'goToNotifications'
-IAM_USER_TAB_ID = "s3iamuserstab"
-BUCKET_TAB_ID = "s3bucketstab"
+S3_ACCOUNTS_TAB_ID = 'tab-1'
+S3_IAM_USER_TAB_ID = 'tab-2'
+S3_BUCKET_TAB_ID   = 'tab-3'
 DASHBOARD_MENU_ID = "Dashboard"
 MAINTENANCE_MENU_ID = 'Maintenance'
 AUDIT_LOG_TAB_ID = "goToAuditLog"
@@ -161,7 +164,6 @@ CSM_USERS_NUMBER_OF_COLUMNS_XPATH = '//*[@id="localuser-tabledata"]//table/tbody
 CSM_USER_EDIT_XPATH = '//*[@id="localuser-tabledata"]//table//td[contains(text(), "{0}")]//following-sibling::td//img[@id="localuser-editicon"]'
 CSM_USER_DELETE_XAPTH = '//*[@id="localuser-tabledata"]//table//td[contains(text(), "{0}")]//following-sibling::td//img[@id="localuser-deleteicon"]'
 CSM_TABLE_ELEMENTS_XPATH = '//*[@id="localuser-tabledata"]//table//tbody//tr//td'
-ADMINISTRATIVE_USER_TAB_ID = "userstab"
 ADD_USER_BUTTON_ID = "btnLocalAddNewUser"
 ADD_USER_USER_NAME_INPUT_BOX_ID = "txtLocalHostname"
 ADD_USER_PASSWORD_INPUT_ID = "txtLocalPass"
