af01c24af3e2801040b0c8ff7b78b541b322b35b
Dhananjay Dandapat <dhananjay.dandapat@seagate.com>
2021-07-05 20:06:37 +0530

    Added response for Start_io and stop_io method modified uploader and downloader added unittest
    Signed-off-by: Dhananjay Dandapat <dhananjay.dandapat@seagate.com>

--- libs/di/di_run_man.py
+++ libs/di/di_run_man.py
@@ -20,6 +20,7 @@
 #
 import logging
 import threading
+from multiprocessing import Value
 from libs.di import uploader
 from libs.di.downloader import DataIntegrityValidator
 
@@ -52,7 +53,7 @@ class ASyncIO:
         LOGGER.debug("Joining Run Man API")
         self.bg_thread.join()
         if di_check:
-            self.verify_data_integrity(users)
+            return self.verify_data_integrity(users)
 
 
 class RunDataCheckManager(ASyncIO):
@@ -60,19 +61,59 @@ class RunDataCheckManager(ASyncIO):
     def __init__(self, users):
         self.uploader = uploader.Uploader()
         self.users = users
+        self.future_value = Value('b', False)
+        self.future_thread_value = threading.Event()
         super(RunDataCheckManager, self).__init__(
             upload_cls=self.uploader, users=users)
 
-    def start_io(self, users, buckets, files_count, prefs, event=None):
+    def __check_upload(self):
+        """
+        read upload file uploadInfo.csv
+        check users name in upload file response
+        :return:
+        """
+        from commons import params
+        import os
+        import csv
+        upload_file = params.UPLOADED_FILES
+        if os.path.exists(upload_file):
+            with open(params.UPLOADED_FILES, newline='') as f:
+                reader = csv.reader(f)
+                uploaded_file_lst = [el for el in list(reader) if el[0] in self.users.keys()]
+            result = len(uploaded_file_lst) > 1
+        else:
+            result = False
+
+        return result
+
+    def start_io_async(self, users, buckets, files_count, prefs, event=None):
         event = event if event else self.event
-        self.uploader.start(users, buckets, files_count, prefs, event)
+        super().start_io_async(users, buckets, files_count, prefs, event)
+
+    def stop_io_async(self, users, di_check=True, eventual_stop=False):
+        response = super().stop_io_async(users, di_check, eventual_stop)
+        status = response.get("failed_files") == 0
+
+        return status, response
+
+    def start_io(self, users, buckets, files_count, prefs, event=None, future_class=None):
+        event = event if event else self.event
+        future_class = future_class if future_class else self.future_value
+        self.uploader.start(users, buckets, files_count, prefs, event, future_class)
+
+        return future_class.value
 
     def stop_io(self, users, di_check=True, eventual_stop=False):
+        response = dict()
+        status = False
         if eventual_stop:
             LOGGER.debug("Setting stop event.")
             self.event.set()
         if di_check:
-            self.verify_data_integrity(users)
+            response = self.verify_data_integrity(users)
+            status = response["failed_files"] == 0
+
+        return status, response
 
     def run_io_sequentially(self, users, buckets=None, files_count=10,
                             prefs=None, di_check=True):
