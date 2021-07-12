a83c93a704386857a66b2ace8c83a11a332d2477
Bhairavi22 <bhairavi.alurkar@seagate.com>
2021-07-13 00:37:01 +0530

    Changed lib for collection issue
    Signed-off-by: Bhairavi22 <bhairavi.alurkar@seagate.com>
    
     On branch HotFix
     Changes to be committed:
    	modified:   libs/csm/cli/cortx_cli_bucket_policy.py
    	modified:   libs/csm/cli/cortx_node_cli_resource.py
     Changes not staged for commit:
    	modified:   libs/csm/cli/cortx_node_cli.py

--- libs/csm/cli/cortx_node_cli_resource.py
+++ libs/csm/cli/cortx_node_cli_resource.py
@@ -10,9 +10,9 @@ class CortxNodeCLIResourceOps(CortxNodeCli):
 
     def __init__(
             self,
-            host: str = CMN_CFG["csm"]["mgmt_vip"],
-            username: str = CMN_CFG["nodes"][0]["username"],
-            password: str = CMN_CFG["nodes"][0]["password"],
+            host: str = None,
+            username: str = None,
+            password: str = None,
             **kwargs):
         """
         This method initializes members of CortxNodeCli and its parent class
@@ -23,6 +23,11 @@ class CortxNodeCLIResourceOps(CortxNodeCli):
         :keyword int port: port number
         """
         self.log = logging.getLogger(__name__)
+        csm = CMN_CFG.get("csm")
+        nodes = CMN_CFG.get("nodes")
+        host = host if host else csm["mgmt_vip"] if csm else None
+        username = username if username else nodes[0]["username"] if nodes else None
+        password = password if password else nodes[0]["password"] if nodes else None
         session_obj = kwargs.get("session_obj", None)
         port = kwargs.get("port", 22)
         super().__init__(
