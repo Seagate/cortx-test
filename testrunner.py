86ea541b76449d605596cbb1a7b571ac9d616e38
NiteshMahajan <nitesh.mahajan@seagate.com>
2021-07-14 14:50:04 +0530

    Integrate test runner in sanity (#484)
    * added logic for externally passing config.ini
    
    Signed-off-by: Nitesh Mahajan <nitesh.mahajan@seagate.com>
    
    * added xml report support in testrunner
    
    Signed-off-by: Nitesh Mahajan <nitesh.mahajan@seagate.com>
    
    * integrated testrunner for release sanity
    
    Signed-off-by: Nitesh Mahajan <nitesh.mahajan@seagate.com>
    
    * resolved ssl issue in clone_test_plan
    
    Signed-off-by: Nitesh Mahajan <nitesh.mahajan@seagate.com>
    
    * fixed run time issues in sanity pipelines
    
    Signed-off-by: Nitesh Mahajan <nitesh.mahajan@seagate.com>
    
    * updated git branch and mail ids for notifications
    
    Signed-off-by: Nitesh Mahajan <nitesh.mahajan@seagate.com>
    
    * changed xml_report paramter to boolean type
    
    Signed-off-by: Nitesh Mahajan <nitesh.mahajan@seagate.com>
    
    * changed xml report help message
    
    Signed-off-by: Nitesh Mahajan <nitesh.mahajan@seagate.com>
    
    Co-authored-by: sonalk0209 <53035906+sonalk0209@users.noreply.github.com>
    Signed-off-by: Divya Kachhwaha <divya.kachhwaha@seagate.com>

--- testrunner.py
+++ testrunner.py
@@ -59,6 +59,8 @@ def parse_args():
                                             "perform additional checksum check")
     parser.add_argument("-tt", "--test_type", nargs='+', type=str,
                         default=['ALL'], help="Space separated test types")
+    parser.add_argument("--xml_report", type=str_to_bool, default=False,
+                        help="Generates xml format report if set True, default is False")
     return parser.parse_args()
 
 
@@ -151,6 +153,12 @@ def run_pytest_cmd(args, te_tag=None, parallel_exe=False, env=None, re_execution
 
     if args.data_integrity_chk:  # redo for kafka tests remove when drunner is supported.
         cmd_line = cmd_line + ["--data_integrity_chk=" + str(True)]
+
+    if args.xml_report:
+        if parallel_exe:
+            cmd_line = cmd_line + ["--junitxml=log/parallel_" + te_id + "report.xml"]
+        else:
+            cmd_line = cmd_line + ["--junitxml=log/non_parallel_" + te_id + "report.xml"]
 
     cmd_line = cmd_line + ['--build=' + build, '--build_type=' + build_type,
                            '--tp_ticket=' + args.test_plan]
