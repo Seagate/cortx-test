f789b9fd9208b0ce2a705bd6cb66e44d3f48d07a
Ankita Thummar <83633380+ankita-93@users.noreply.github.com>
2021-07-21 13:37:33 +0530

    RBF HA 10B/C Feature (#501)
    * RBF for F10B/C
    
    Signed-off-by: ankita-93 <ankita.thummar@seagate.com>
    
    * RBF minor update
    
    Signed-off-by: ankita-93 <ankita.thummar@seagate.com>
    
    * Review comment update
    
    Signed-off-by: ankita-93 <ankita.thummar@seagate.com>
    
    * Review comment update
    
    Signed-off-by: ankita-93 <ankita.thummar@seagate.com>

--- tests/ha/test_ha_node_health.py
+++ tests/ha/test_ha_node_health.py
@@ -106,6 +106,7 @@ class TestHANodeHealth:
         """
         LOGGER.info("STARTED: Setup Operations")
         self.starttime = time.time()
+        self.nw_data = None
         LOGGER.info(
             "Checking in cortxcli and REST that all nodes are shown online and PCS clean.")
         for hlt_obj in self.hlt_list:
@@ -133,6 +134,22 @@ class TestHANodeHealth:
                     resp = self.ha_obj.host_power_on(host=self.host_list[node], bmc_obj=self.bmc_list[node])
                     assert_utils.assert_true(
                         resp, f"Failed to power on {self.srvnode_list[node]}.")
+                if self.nw_data:
+                    resp = self.node_list[node].execute_cmd(
+                        common_cmds.GET_IFCS_STATUS.format(self.nw_data[1][node]), read_lines=True)
+                    LOGGER.debug("%s interface status for %s = %s",
+                    self.nw_data[0][node], self.srvnode_list[node], resp[0])
+                    if "DOWN" in resp[0]:
+                        LOGGER.info(
+                            "Make the %s interface back up for %s", self.nw_data[0][node], self.srvnode_list[node])
+                        self.node_list[node].execute_cmd(
+                            common_cmds.IP_LINK_CMD.format(
+                                self.nw_data[0][node], "up"), read_lines=True)
+                        resp = self.node_list[node].execute_cmd(common_cmds.CMD_PING.format(
+                                self.nw_data[1][node]), read_lines=True, exc=False)
+                        assert_utils.assert_not_in("Name or service not known", resp[1][0],
+                                                "Node interface still down.")
+                    LOGGER.info("All network interfaces are up")
 
         for hlt_obj in self.hlt_list:
             res = hlt_obj.check_node_health()
@@ -188,7 +205,7 @@ class TestHANodeHealth:
 
             LOGGER.info("Check for the node down alert.")
             resp = self.csm_alerts_obj.verify_csm_response(
-                self.starttime, self.alert_type["fault"], False, "iem")
+                self.starttime, self.alert_type["get"], False, "iem")
             assert_utils.assert_true(resp, "Failed to get alert in CSM")
             # TODO: If CSM REST getting changed, add alert check from msg bus
 
@@ -214,6 +231,12 @@ class TestHANodeHealth:
                 srvnode_list=self.srvnode_list,
                 sys_list=self.sys_list,
                 no_nodes=self.num_nodes)
+
+            LOGGER.info("Checking PCS clean after powered on %s", node_name)
+            for hlt_obj in self.hlt_list:
+                res = hlt_obj.check_node_health()
+                assert_utils.assert_true(res[0], res[1])
+            LOGGER.info("All nodes are online and PCS looks clean.")
 
             LOGGER.info("Check for the node back up alert.")
             resp = self.csm_alerts_obj.verify_csm_response(
@@ -277,7 +300,7 @@ class TestHANodeHealth:
 
             LOGGER.info("Check for the node down alert.")
             resp = self.csm_alerts_obj.verify_csm_response(
-                self.starttime, self.alert_type["fault"], False, "iem")
+                self.starttime, self.alert_type["get"], False, "iem")
             assert_utils.assert_true(resp, "Failed to get alert in CSM")
             # TODO: If CSM REST getting changed, add alert check from msg bus
             LOGGER.info(
@@ -300,7 +323,13 @@ class TestHANodeHealth:
                 srvnode_list=self.srvnode_list,
                 sys_list=self.sys_list,
                 no_nodes=self.num_nodes)
-            LOGGER.info("All nodes are online in CLI and REST.")
+
+            LOGGER.info("Checking PCS clean after powered on %s", self.host_list[node])
+            for hlt_obj in self.hlt_list:
+                res = hlt_obj.check_node_health()
+                assert_utils.assert_true(res[0], res[1])
+            LOGGER.info("All nodes are online and PCS looks clean.")
+
             LOGGER.info("Check for the node back up alert.")
             resp = self.csm_alerts_obj.verify_csm_response(
                 self.starttime, self.alert_type["resolved"], True, "iem")
@@ -325,12 +354,14 @@ class TestHANodeHealth:
         LOGGER.info(
             "Started: Test to check node status one by one on all nodes when nw interface on node goes"
             "down and comes back up")
+        self.restored = False
 
         LOGGER.info("Get the list of private data interfaces for all nodes.")
         response = self.ha_obj.get_iface_ip_list(
             node_list=self.node_list, num_nodes=self.num_nodes)
         iface_list = response[0]
         private_ip_list = response[1]
+        self.nw_data = [iface_list, private_ip_list]
         LOGGER.debug(
             "List of private data IP : {} and interfaces on all nodes: {}" .format(
                 private_ip_list, iface_list))
@@ -338,10 +369,11 @@ class TestHANodeHealth:
         for node in range(self.num_nodes):
             node_name = self.srvnode_list[node]
             LOGGER.info(
-                "Make the private data interface down for {}".format(node_name))
+                "Make the private data interface %s down for %s", iface_list[node], node_name)
             self.node_list[node].execute_cmd(
                 common_cmds.IP_LINK_CMD.format(
                     iface_list[node], "down"), read_lines=True)
+
             if node_name == self.srvnode_list[-1]:
                 nd_obj = self.node_list[0]
             else:
@@ -385,7 +417,7 @@ class TestHANodeHealth:
                 resp, "Some services are down for other nodes.")
 
             LOGGER.info(
-                "Make the private data interface back up for {}".format(node_name))
+                "Make the private data interface %s back up for %s", iface_list[node], node_name)
             self.node_list[node].execute_cmd(
                 common_cmds.IP_LINK_CMD.format(
                     iface_list[node], "up"), read_lines=True)
@@ -396,6 +428,7 @@ class TestHANodeHealth:
                 exc=False)
             assert_utils.assert_not_in("Name or service not known", resp[1][0],
                                        "Node interface still down.")
+            self.restored = True
             # To get all the services up and running
             time.sleep(40)
             LOGGER.info("Check all nodes are back online in CLI and REST.")
@@ -404,6 +437,13 @@ class TestHANodeHealth:
                 srvnode_list=self.srvnode_list,
                 sys_list=self.sys_list,
                 no_nodes=self.num_nodes)
+
+            LOGGER.info("Checking PCS clean after making the private data interface %s down for %s",
+            iface_list[node], node_name)
+            for hlt_obj in self.hlt_list:
+                res = hlt_obj.check_node_health()
+                assert_utils.assert_true(res[0], res[1])
+            LOGGER.info("All nodes are online and PCS looks clean.")
 
             LOGGER.info("Check for the node back up alert.")
             resp = self.csm_alerts_obj.verify_csm_response(
@@ -476,7 +516,7 @@ class TestHANodeHealth:
 
             LOGGER.info("Check for the node down alert.")
             resp = self.csm_alerts_obj.verify_csm_response(
-                self.starttime, self.alert_type["fault"], False, "iem")
+                self.starttime, self.alert_type["get"], False, "iem")
             assert_utils.assert_true(resp, "Failed to get alert in CSM")
             # TODO: If CSM REST getting changed, add alert check from msg bus
 
@@ -501,6 +541,13 @@ class TestHANodeHealth:
                 srvnode_list=self.srvnode_list,
                 sys_list=self.sys_list,
                 no_nodes=self.num_nodes)
+
+            LOGGER.info("Checking PCS clean after powered on %s", self.host_list[node_index])
+            for hlt_obj in self.hlt_list:
+                res = hlt_obj.check_node_health()
+                assert_utils.assert_true(res[0], res[1])
+            LOGGER.info("All nodes are online and PCS looks clean.")
+
             LOGGER.info("Check for the node back up alert.")
 
             resp = self.csm_alerts_obj.verify_csm_response(
@@ -572,7 +619,7 @@ class TestHANodeHealth:
 
             LOGGER.info("Check for the node down alert.")
             resp = self.csm_alerts_obj.verify_csm_response(
-                self.starttime, self.alert_type["fault"], False, "iem")
+                self.starttime, self.alert_type["get"], False, "iem")
             assert_utils.assert_true(resp, "Failed to get alert in CSM")
             # TODO: If CSM REST getting changed, add alert check from msg bus
 
@@ -597,7 +644,11 @@ class TestHANodeHealth:
                 srvnode_list=self.srvnode_list,
                 sys_list=self.sys_list,
                 no_nodes=self.num_nodes)
-            LOGGER.info("Checked All nodes are online in CLI and REST.")
+            LOGGER.info("Checking PCS clean after powered on %s", self.host_list[node_index])
+            for hlt_obj in self.hlt_list:
+                res = hlt_obj.check_node_health()
+                assert_utils.assert_true(res[0], res[1])
+            LOGGER.info("All nodes are online and PCS looks clean.")
 
             LOGGER.info("Check for the node back up alert.")
             resp = self.csm_alerts_obj.verify_csm_response(
