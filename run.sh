8ffb7509a560998db1e942f418f634016cb1ffaa
kulrk <rohan.1.kulkarni@seagate.com>
2021-07-22 14:56:43 +0530

    codacy fix
    Signed-off-by: kulrk <rohan.1.kulkarni@seagate.com>

--- run.sh
+++ run.sh
@@ -11,10 +11,12 @@ export PYTHONPATH=$LIB:$SITE_PKGS:$CORTX_TEST_ROOT
 
 if [ "$SKIP_TE" != "None" ];
 then
-  "$PYTHON_VAR" -u tools/clone_test_plan/clone_test_plan.py -tp="$TEST_PLAN_NUMBER" -b="$BUILD" -s="$SETUP_TYPE" -c="$COMMENT_JIRA" -br="$BRANCH" -n="$NODES" -sr="$SERVER_TYPE" -e="$ENCLOSURE_TYPE" -st $SKIP_TE
+  cmd="$PYTHON_VAR -u tools/clone_test_plan/clone_test_plan.py -tp=$TEST_PLAN_NUMBER -b=$BUILD -s=$SETUP_TYPE -c=$COMMENT_JIRA -br=$BRANCH -n=$NODES -sr=$SERVER_TYPE -e=$ENCLOSURE_TYPE -st $SKIP_TE"
 else
-  "$PYTHON_VAR" -u tools/clone_test_plan/clone_test_plan.py -tp="$TEST_PLAN_NUMBER" -b="$BUILD" -s="$SETUP_TYPE" -c="$COMMENT_JIRA" -br="$BRANCH" -n="$NODES" -sr="$SERVER_TYPE" -e="$ENCLOSURE_TYPE"
+  cmd="$PYTHON_VAR -u tools/clone_test_plan/clone_test_plan.py -tp=$TEST_PLAN_NUMBER -b=$BUILD -s=$SETUP_TYPE -c=$COMMENT_JIRA -br=$BRANCH -n=$NODES -sr=$SERVER_TYPE -e=$ENCLOSURE_TYPE"
 fi
+
+$cmd
 
 INPUT=cloned_tp_info.csv
 OLDIFS=$IFS
