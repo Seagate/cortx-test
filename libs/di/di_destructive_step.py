3b99552d6dbdffe862c4b157d49f05f77ca2eb54
Vikas Kumar Choudhary <vikas.kumar@seagate.com>
2021-07-27 13:12:43 +0530

    Review Comments Incorporated and removed s3_dns entry

--- libs/di/di_destructive_step.py
+++ libs/di/di_destructive_step.py
@@ -20,19 +20,21 @@
 #
 
 """ Destructive operations."""
-import os
-import random
-import logging
 import csv
 import fcntl
 import hashlib
+import logging
 import multiprocessing as mp
-import boto3
+import os
+import random
 import time
 from multiprocessing import Manager
+
+import boto3
+
+from config import S3_CFG
 from libs.di import di_lib
 from libs.di import di_params
-from config import CMN_CFG
 
 logger = logging.getLogger(__name__)
 
@@ -73,7 +75,7 @@ def destructive_step():
 
         try:
             s3 = boto3.resource('s3', aws_access_key_id=access_key, aws_secret_access_key=secret_key,
-                                endpoint_url=CMN_CFG["s3_loadbalancer"]["s3_url"])
+                                endpoint_url=S3_CFG["s3_url"])
         except Exception as e:
             logger.info(
                 f'could not create s3 object for user {user_name} with access key {access_key} secret key {secret_key} exception:{e}')
