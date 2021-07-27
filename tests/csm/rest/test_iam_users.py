7c6735eebecdfeee3d585a0ba8cbbf3b9342c220
Divya Kachhwaha <divya.kachhwaha@seagate.com>
2021-07-27 09:25:45 +0530

    codacy
    Signed-off-by: Divya Kachhwaha <divya.kachhwaha@seagate.com>

--- tests/csm/rest/test_iam_users.py
+++ tests/csm/rest/test_iam_users.py
@@ -99,7 +99,7 @@ class TestIamUser():
             self.rest_iam_user.config["s3account_user"]["username"], response)
 
         self.log.info("##### Test ended -  %s #####", test_case_name)
-    
+
     @pytest.mark.skip("Test invalid for R2")
     @pytest.mark.csmrest
     @pytest.mark.cluster_user_ops
