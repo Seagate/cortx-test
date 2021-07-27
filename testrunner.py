d23368d76cbc4a67cc611ea10a86cd2a5f36e208
Dhananjay Dandapat <dhananjay.dandapat@seagate.com>
2021-07-27 09:36:30 +0530

    Close consumer right after topics messages enteirs are done
    Signed-off-by: Dhananjay Dandapat <dhananjay.dandapat@seagate.com>

--- testrunner.py
+++ testrunner.py
@@ -165,12 +165,12 @@ def run_pytest_cmd(args, te_tag=None, parallel_exe=False, env=None, re_execution
     LOGGER.debug('Running pytest command %s', cmd_line)
     prc = subprocess.Popen(cmd_line, env=env)
     prc.communicate()
-    if prc.returncode == 3:
+    if prc.returncode == 1:
         print('Exiting test runner due to bad health of deployment')
-        sys.exit(3)
-    if prc.returncode == 4:
+        sys.exit(1)
+    if prc.returncode == 2:
         print('Exiting test runner due to health check script error')
-        sys.exit(4)
+        sys.exit(2)
 
 
 def delete_status_files():
@@ -324,10 +324,7 @@ def create_test_meta_data_file(args, test_list, jira_obj=None):
             item['test_run_id'] = test_dict[test]
             test_meta.append(item)
         tp_meta['test_meta'] = test_meta
-        #import fcntl
-        #fcntl.flock(tp_meta, fcntl.LOCK_EX)
         json.dump(tp_meta, t_meta, ensure_ascii=False)
-        #fcntl.flock(tp_meta, fcntl.LOCK_UN)
     return tp_meta
 
 
@@ -490,12 +487,16 @@ def check_kafka_msg_trigger_test(args):
     consumer = kafka_consumer.get_consumer()
     print(consumer)
     received_stop_signal = False
-
+    max_iteration = 0
     while not received_stop_signal:
         try:
             # SIGINT can't be handled when polling, limit timeout to 60 seconds.
-            msg = consumer.poll(1.0)
+            msg = consumer.poll(60)
             if msg is None:
+                max_iteration += 1
+                # break while in case consumer doesn't have any further messages
+                if max_iteration >= 4:
+                    received_stop_signal = True
                 continue
             kafka_msg = msg.value()
             print(kafka_msg)
@@ -571,7 +572,7 @@ def main(args):
         out, err = prc.communicate()
     elif args.te_ticket:
         trigger_tests_from_te(args)
-    else:                                     # check distrubuted
+    else:
         check_kafka_msg_trigger_test(args)
 
 
