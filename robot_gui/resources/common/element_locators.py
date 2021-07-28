a1147e41bc54f3a0371ab7dfa567a14ba55dc1a6
aditipande96 <86783066+aditipande96@users.noreply.github.com>
2021-07-28 08:35:42 +0530

    New PR for manage user test cases (#540)
    * New PR for manage user test cases
    
    Signed-off-by: Aditi Pande<aditi.pande@seagate.com>
    
    * Modified self delete test case for various users
    
    Signed-off-by: Aditi Pande<aditi.pande@seagate.com>
    
    * Modified test for deleting monitor user
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    Co-authored-by: swapnil-khandare <66417964+swapnil-khandare@users.noreply.github.com>
    Signed-off-by: Divya Kachhwaha <divya.kachhwaha@seagate.com>

--- robot_gui/resources/common/element_locators.py
+++ robot_gui/resources/common/element_locators.py
@@ -172,6 +172,10 @@ CSM_USER_DELETE_XAPTH = '//td//div[contains(text(), "{0}")]//parent::td//parent:
 CSM_TABLE_ELEMENTS_XPATH = '//*[@class="v-data-table cortx-table theme--light"]//table//tbody//tr//td'
 CSM_TABLE_ROW_XPATH = '//*[@class="v-data-table cortx-table theme--light"]//table//tbody//tr'
 CSM_TABLE_COLUMN_XPATH = '//*[@class="v-data-table cortx-table theme--light"]//table//tbody//tr[*]//td[{0}]'
+CSM_TABLE_DROPDOWN_XPATH ='//*[@class="cortx-dropdown-title"]'
+CSM_TABLE_DROPDOWN_MENU_XPATH ='//*[@class="cortx-dropdown-container menu-on-top"]//div[@class="cortx-dropdown-menu"]'
+PAGINATION_LIST_ICON_XPATH = '//*[@class="cortx-dropdown-title"]'
+PAGINATION_PAGE_OPTIONS_XPATH = '//*[@class="cortx-dropdown-container menu-on-top"]//div[@class="cortx-dropdown-menu-item"]'
 ADD_USER_BUTTON_ID = "btnLocalAddNewUser"
 ADD_USER_USER_NAME_INPUT_BOX_ID = "txtUsername" #"txtLocalHostname"
 ADD_USER_PASSWORD_INPUT_ID = "txtLocalPass"
@@ -207,9 +211,8 @@ UPDATE_USER_BUTTON_ID = "lblLocalApplyInterface"
 CONFIRM_NEW_PASSWORD_INPUT_ID = "txtLocalConfirmNewPass"
 OLD_PASSWORD_INPUT_ID = "txtLocalOldPass"
 PAGINATION_BAR_XPATH = "//div[@class='v-data-footer']"
-PAGINATION_PAGE_OPTIONS_XPATH = "//*[@role='option']"
 RADIO_BTN_VALUE_XPATH = "//*[@type='radio']"
-PAGINATION_LIST_ICON_XPATH = "//*[@class='v-select__selection v-select__selection--comma']"
+
 
 #  IAM Users
 ADD_IAM_USER_BUTTON_ID = "iam-user-create-formbtn"
