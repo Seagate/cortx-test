fcd3c0383c1a00c723b54a47147545a25a173e18
NiteshMahajan <nitesh.mahajan@seagate.com>
2021-08-11 17:13:27 +0530

    Automated server psu alerts tests (#572)
    * added logic for externally passing config.ini
    
    Signed-off-by: Nitesh Mahajan <nitesh.mahajan@seagate.com>
    
    * updated url and rest endpoints for csm boarding
    
    Signed-off-by: Nitesh Mahajan <nitesh.mahajan@seagate.com>
    
    * reverted rest login api
    
    Signed-off-by: Nitesh Mahajan <nitesh.mahajan@seagate.com>
    
    * hotfix rearranging prov tests to disable healthcheck
    
    Signed-off-by: Nitesh Mahajan <nitesh.mahajan@seagate.com>
    
    * added support for custom build and optimized code
    
    Signed-off-by: Nitesh Mahajan <nitesh.mahajan@seagate.com>
    
    * changed build_branch env variable
    
    Signed-off-by: Nitesh Mahajan <nitesh.mahajan@seagate.com>
    
    * Fixed codacy issues and review comments
    
    Signed-off-by: Nitesh Mahajan <nitesh.mahajan@seagate.com>
    
    * Hotfix to resolve xml logs generating issue in cicd
    
    Signed-off-by: Nitesh Mahajan <nitesh.mahajan@seagate.com>
    
    * created libraries and automated server psu alert tests
    
    Signed-off-by: Nitesh Mahajan <nitesh.mahajan@seagate.com>
    
    * created alert for server psu faults
    
    Signed-off-by: Nitesh Mahajan <nitesh.mahajan@seagate.com>
    
    * change method to return error instead of raise
    
    Signed-off-by: Nitesh Mahajan <nitesh.mahajan@seagate.com>
    
    * resolved codacy issues
    
    Signed-off-by: Nitesh Mahajan <nitesh.mahajan@seagate.com>
    
    * modified test name server psu alert
    
    Signed-off-by: Nitesh Mahajan <nitesh.mahajan@seagate.com>
    
    Co-authored-by: sonalk0209 <53035906+sonalk0209@users.noreply.github.com>
    Co-authored-by: seagate-sarang-sawant <sarang.sawant@seagate.com>
    Signed-off-by: Divya Kachhwaha <divya.kachhwaha@seagate.com>

--- commons/alerts_simulator/constants.py
+++ commons/alerts_simulator/constants.py
@@ -25,7 +25,10 @@ DISK_ENABLE = {"enclid": 0, "ctrl_name": "A", "phy_num": 5,
                "telnet_file": "/root/telnet.xml"}
 DISK_FAULT_NO_ALERT = {"du_val": 8, "fault": False, "fault_resolved": False}
 DISK_FAULT_ALERT = {"du_val": 8, "fault": True, "fault_resolved": False}
-DISK_FAULT_RESOLVED_ALERT = {"du_val": 8, "fault": True, "fault_resolved": True}
+DISK_FAULT_RESOLVED_ALERT = {
+    "du_val": 8,
+    "fault": True,
+    "fault_resolved": True}
 CPU_USAGE_NO_ALERT = {"delta_cpu_usage": 0.3}
 CPU_USAGE_ALERT = {"delta_cpu_usage": -0.3}
 CPU_USAGE_RESOLVED_ALERT = {"delta_cpu_usage": 0}
@@ -40,7 +43,10 @@ RAID_FAIL_DISK_ALERT = {"operation": "fail_disk", "md_device": None,
                         "disk": None}
 RAID_REMOVE_DISK_ALERT = {"operation": "remove_disk", "md_device": None,
                           "disk": None}
-RAID_ADD_DISK_ALERT = {"operation": "add_disk", "md_device": None, "disk": None}
+RAID_ADD_DISK_ALERT = {
+    "operation": "add_disk",
+    "md_device": None,
+    "disk": None}
 IEM_TEST_ERROR_ALERT = {"cmd": 'logger -i -p local3.err '
                                'IEC: EO0090090900:Test IEM'}
 DG_FAULT = {"enclid": 0, "ctrl_name": ['A', 'B'], "operation": "Disabled",
@@ -48,3 +54,17 @@ DG_FAULT = {"enclid": 0, "ctrl_name": ['A', 'B'], "operation": "Disabled",
 DG_FAULT_RESOLVED = {"enclid": 0, "ctrl_name": ["A", "B"],
                      "operation": "Enabled", "disk_group": "dg00",
                      "phy_num": [], "poll": True}
+SERVER_PSU_FAULT = {
+    "sensor_type": "Power Supply",
+    "sensor_states": [
+        "Presence detected",
+        "Failure detected",
+        "Power Supply AC lost"],
+    "deassert": False}
+SERVER_PSU_FAULT_RESOLVED = {
+    "sensor_type": "Power Supply",
+    "sensor_states": [
+        "Presence detected",
+        "Failure detected",
+        "Power Supply AC lost"],
+    "deassert": True}
