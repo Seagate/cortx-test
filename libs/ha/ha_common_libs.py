98e40539a58d6c6bea3d26a6fd0b0891c9b84dec
Ankita Thummar <83633380+ankita-93@users.noreply.github.com>
2021-09-07 19:03:15 +0530

    EOS-22523 (#658)
    * Initial commit for EOS-22523
    
    Signed-off-by: ankita-93 <ankita.thummar@seagate.com>
    
    * Minor commit to check codecy
    
    Signed-off-by: ankita-93 <ankita.thummar@seagate.com>
    
    * Fix the codecy issye
    
    Signed-off-by: ankita-93 <ankita.thummar@seagate.com>
    
    * Codecy issue fix
    
    Signed-off-by: ankita-93 <ankita.thummar@seagate.com>
    
    * review comment update
    
    Signed-off-by: ankita-93 <ankita.thummar@seagate.com>
    
    * Minor update in logger info line
    
    Signed-off-by: ankita-93 <ankita.thummar@seagate.com>
    
    Co-authored-by: sonalk0209 <53035906+sonalk0209@users.noreply.github.com>
    Signed-off-by: Divya Kachhwaha <divya.kachhwaha@seagate.com>

--- libs/ha/ha_common_libs.py
+++ libs/ha/ha_common_libs.py
@@ -65,6 +65,7 @@ class HALibs:
         self.mgnt_ops = ManagementOPs()
         self.num_nodes = len(CMN_CFG["nodes"])
         self.s3_rest_obj = S3AccountOperationsRestAPI()
+        self.parallel_ios = None
 
     @staticmethod
     def check_csm_service(node_object, srvnode_list, sys_list):
