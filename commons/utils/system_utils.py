6a03babd6bd54a93a51f240c5077b49c678599a7
Vishal Dhobale <69301843+DhobaleVishal@users.noreply.github.com>
2021-09-23 11:07:06 +0530

    Fix to catch error in command failure. (#705)
    Signed-off-by: Vishal Dhobale <vishal.dhobale@seagate.com>

--- commons/utils/system_utils.py
+++ commons/utils/system_utils.py
@@ -147,7 +147,7 @@ def run_remote_cmd_wo_decision(
     return output, error, exit_status
 
 
-def run_local_cmd(cmd: str = None, flg: bool = False, chk_stderr = False) -> tuple:
+def run_local_cmd(cmd: str = None, flg: bool = False, chk_stderr=False) -> tuple:
     """
     Execute any given command on local machine(Windows, Linux).
     :param cmd: command to be executed.
@@ -166,8 +166,7 @@ def run_local_cmd(cmd: str = None, flg: bool = False, chk_stderr = False) -> tup
         return True, str((output, error))
     if chk_stderr:
         if error:
-            return False, str(output)
-        return True, str(output)
+            return False, str(error)
     if proc.returncode != 0:
         return False, str(error)
     if b"Number of key(s) added: 1" in output:
