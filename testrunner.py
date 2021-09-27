43e46071ef89793e917d82db4f4256b90c59fb01
Vishal Dhobale <69301843+DhobaleVishal@users.noreply.github.com>
2021-09-27 20:00:28 +0530

    Eos 24254 test runner support for validate certificate and use ssl (#714)
    * Added paramters: validate certificate and use ssl for s3 endpoint.
    
    Signed-off-by: Vishal Dhobale <vishal.dhobale@seagate.com>
    
    * Method: To construct s3 endpoint based on validate_certs, use-ssl
    
    Signed-off-by: Vishal Dhobale <vishal.dhobale@seagate.com>
    
    * Minor fix
    
    Signed-off-by: Vishal Dhobale <vishal.dhobale@seagate.com>
    
    * Added iam port parameter.
    
    Signed-off-by: Vishal Dhobale <vishal.dhobale@seagate.com>
    
    * Variable to retry/polling in multinode.
    
    Signed-off-by: Vishal Dhobale <vishal.dhobale@seagate.com>
    
    * Addressed and incorporated review comments. Fixed codacy issue.
    
    Signed-off-by: Vishal Dhobale <vishal.dhobale@seagate.com>

--- testrunner.py
+++ testrunner.py
@@ -73,6 +73,10 @@ def parse_args():
                         action="store_true", help="Stop test execution on first failure")
     parser.add_argument("-pf", "--product_family", type=str, default='LC',
                         help="Product family LR or LC.")
+    parser.add_argument("--validate_certs", type=str_to_bool, default=True,
+                        help="Validate HTTPS/SSL certificate to S3 endpoint.")
+    parser.add_argument("--use_ssl", type=str_to_bool, default=True,
+                        help="Use HTTPS/SSL connection for S3 endpoint.")
     return parser.parse_args()
 
 
@@ -181,7 +185,9 @@ def run_pytest_cmd(args, te_tag=None, parallel_exe=False, env=None, re_execution
 
     cmd_line = cmd_line + ['--build=' + build, '--build_type=' + build_type,
                            '--tp_ticket=' + args.test_plan,
-                           '--product_family=' + args.product_family]
+                           '--product_family=' + args.product_family,
+                           '--validate_certs=' + str(args.validate_certs),
+                           '--use_ssl=' + str(args.use_ssl)]
     LOGGER.debug('Running pytest command %s', cmd_line)
     prc = subprocess.Popen(cmd_line, env=env)
     prc.communicate()
