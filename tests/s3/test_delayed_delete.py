52713d830d59a4c23ff123253b5cd98b493cd318
Ketan Arlulkar <6256964+ketanarlulkar@users.noreply.github.com>
2021-10-01 21:27:50 +0530

    Moved code in library
    Signed-off-by: Ketan Arlulkar <6256964+ketanarlulkar@users.noreply.github.com>

--- tests/s3/test_delayed_delete.py
+++ tests/s3/test_delayed_delete.py
@@ -32,9 +32,12 @@ from commons.errorcodes import error_handler
 from commons.exceptions import CTException
 from commons.params import TEST_DATA_PATH
 from commons.utils import assert_utils, system_utils
+from commons.utils.config_utils import read_yaml
 from libs.s3 import ACCESS_KEY, SECRET_KEY, s3_test_lib
 from libs.s3.s3_multipart_test_lib import S3MultipartTestLib
 from scripts.s3_bench import s3bench
+
+DEL_CFG = read_yaml("config/s3/test_delayed_delete.yaml")[1]
 
 
 class TestDelayedDelete:
@@ -56,9 +59,8 @@ class TestDelayedDelete:
         if not system_utils.path_exists(self.test_dir_path):
             resp = system_utils.make_dirs(self.test_dir_path)
             self.log.info("Created path: %s", resp)
-        self.test_file_path = os.path.join(
-            self.test_dir_path,
-            self.test_file.format(str(int(time.time()))))
+        self.test_file_path = os.path.join(self.test_dir_path,
+                                           self.test_file.format(str(int(time.time()))))
 
     def teardown_method(self):
         """Delete test data file"""
@@ -66,64 +68,9 @@ class TestDelayedDelete:
         if system_utils.path_exists(self.test_file_path):
             resp = system_utils.remove_file(self.test_file_path)
             assert_utils.assert_true(resp[0], resp[1])
-            self.log.info(
-                "removed path: %s, resp: %s",
-                self.test_file_path,
-                resp)
+            self.log.info("removed path: %s, resp: %s", self.test_file_path, resp)
 
-    def s3bench_workload(self, bucket_name, log_prefix):
-        """S3bench Workload worker"""
-        object_size = "2Mb"
-        client = 3
-        sample = 400
-        self.log.info("Workload: %s objects of %s with %s parallel clients",
-                      sample, object_size, client)
-        resp = s3bench.s3bench(ACCESS_KEY, SECRET_KEY, bucket=f"{bucket_name}", num_clients=client,
-                               num_sample=sample, obj_name_pref="loadgen_test_",
-                               obj_size=object_size, skip_cleanup=False, duration=None,
-                               log_file_prefix=log_prefix)
-        self.log.info("Log Path %s", resp[1])
-        assert_utils.assert_false(s3bench.check_log_file_error(resp[1]),
-                                  f"S3bench workload on bucket {bucket_name} with {client} "
-                                  f"client failed. Please read log file {resp[1]}")
-
-    def simple_multipart_upload(self, bucket, obj_name):
-        """
-        Create a file of 5GiB, upload the file as first part.
-        Create a file of 5MiB, upload the file as second part.
-        Complete multipart upload without any error.
-        """
-        parts = []
-        self.log.info("Verifying actual part size limit")
-        self.log.info("Initiating multipart uploads")
-        res = self.s3_mp_obj.create_multipart_upload(bucket, obj_name)
-        assert_utils.assert_true(res[0], res[1])
-        mpu_id = res[1]["UploadId"]
-        self.log.info("Multipart Upload initiated with mpu_id %s", mpu_id)
-        self.log.info("Creating a object: %s size 5GiB", obj_name)
-        system_utils.create_file(self.test_file_path, 5120, "/dev/urandom", '1M')
-        with open(self.test_file_path, "rb") as file_pointer:
-            data = file_pointer.read()
-        self.log.info("Uploading part 1 of length %s", str(len(data)))
-        response = self.s3_mp_obj.upload_part(
-            data, bucket, obj_name, upload_id=mpu_id, part_number=1)
-        assert_utils.assert_true(response[0], response[1])
-        parts.append({"PartNumber": 1, "ETag": response[1]["ETag"]})
-        self.log.info("Creating a object: %s size 5MiB", obj_name)
-        system_utils.create_file(self.test_file_path, 5, "/dev/urandom", '1M')
-        with open(self.test_file_path, "rb") as file_pointer:
-            data = file_pointer.read()
-        self.log.info("Uploading part 2 of length %s", str(len(data)))
-        response = self.s3_mp_obj.upload_part(
-            data, bucket, obj_name, upload_id=mpu_id, part_number=2)
-        assert_utils.assert_true(response[0], response[1])
-        parts.append({"PartNumber": 2, "ETag": response[1]["ETag"]})
-        response = self.s3_mp_obj.complete_multipart_upload(mpu_id, parts,
-                                                            bucket, obj_name)
-        assert_utils.assert_true(response[0], response[1])
-        self.log.info("Multipart upload with part 1 of 5GiB and part 2 of 5MiB is successful.")
-
-    @pytest.mark.s3_ops
+    @pytest.mark.s3_delete
     @pytest.mark.tags("TEST-28990")
     @CTFailOn(error_handler)
     def test_28990(self):
@@ -136,6 +83,8 @@ class TestDelayedDelete:
         object_size = "2Mb"
 
         # Run s3bench workload of 1000 objects with cleanup option
+        resp = s3bench.setup_s3bench()
+        assert_utils.assert_true(resp, "Could not setup s3bench.")
         resp = s3bench.s3bench(ACCESS_KEY, SECRET_KEY, bucket=bucket_name, num_clients=clients,
                                num_sample=samples, obj_name_pref="test-object-",
                                obj_size=object_size, skip_cleanup=False, duration=None,
@@ -152,7 +101,7 @@ class TestDelayedDelete:
         self.log.info("Completed: Verify background deletes using multiple "
                       "objects delete operation")
 
-    @pytest.mark.s3_ops
+    @pytest.mark.s3_delete
     @pytest.mark.tags("TEST-28991")
     @CTFailOn(error_handler)
     def test_28991(self):
@@ -162,12 +111,13 @@ class TestDelayedDelete:
 
         # Run s3bench workload of 1000 objects parallel on 3 buckets with cleanup option
         resp = s3bench.setup_s3bench()
-        assert (resp, resp), "Could not setup s3bench."
+        assert_utils.assert_true(resp, "Could not setup s3bench.")
         pool = Pool(processes=3)
         buckets = [f"test-28991-bucket-{i}-{str(int(time.time()))}" for i in range(3)]
-        pool.starmap(self.s3bench_workload, [(buckets[0], "TEST-28991"),
-                                             (buckets[1], "TEST-28991"),
-                                             (buckets[2], "TEST-28991")])
+        pool.starmap(s3bench.s3bench_workload,
+                     [(buckets[0], "TEST-28991", "2Mb", 3, 400, ACCESS_KEY, SECRET_KEY),
+                      (buckets[1], "TEST-28991", "2Mb", 3, 400, ACCESS_KEY, SECRET_KEY),
+                      (buckets[2], "TEST-28991", "2Mb", 3, 400, ACCESS_KEY, SECRET_KEY)])
 
         # Check if entries are getting deleted
         listed_buckets = self.s3_obj.bucket_list()[1]
@@ -177,7 +127,7 @@ class TestDelayedDelete:
         self.log.info("Completed: Verify background deletes when ran s3bench workload "
                       "on multiple buckets")
 
-    @pytest.mark.s3_ops
+    @pytest.mark.s3_delete
     @pytest.mark.tags("TEST-28992")
     @CTFailOn(error_handler)
     def test_28992(self):
@@ -226,12 +176,14 @@ class TestDelayedDelete:
 
         self.log.info("Completed: Verify if deletion is successful post simple object delete")
 
-    @pytest.mark.s3_ops
+    @pytest.mark.s3_delete
     @pytest.mark.tags("TEST-28993")
     @CTFailOn(error_handler)
     def test_28993(self):
         """Verify if deletion is successful post Multipart object delete"""
         self.log.info("Started: Verify if deletion is successful post Multipart object delete")
+
+        test_config = DEL_CFG["test_28993"]
 
         # Create bucket
         bucket_name = f"test-28993-bucket-{int(time.time())}"
@@ -241,7 +193,8 @@ class TestDelayedDelete:
 
         # Do multipart upload
         object_name = self.test_file
-        self.simple_multipart_upload(bucket_name, object_name)
+        self.s3_mp_obj.simple_multipart_upload(bucket_name, object_name, test_config["file_size"],
+                                               self.test_file_path, test_config["total_parts"])
 
         # Delete object
         res = self.s3_obj.delete_object(bucket_name, object_name)
