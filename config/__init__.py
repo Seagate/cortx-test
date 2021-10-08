d3215e3b735efbc7b19f1034de89b2f10ffc7478
seagate-sarang-sawant <70256535+seagate-sarang-sawant@users.noreply.github.com>
2021-10-08 13:26:17 +0530

    EOS-25304 Corrected config init file. Removed all s3 configs from config init.

--- config/__init__.py
+++ config/__init__.py
@@ -91,13 +91,6 @@ else:
     CSM_REST_CFG = configmanager.get_config_wrapper(fpath=CSM_CONFIG, config_key="Restcall",
                                                 target=target, target_key="csm")
 CSM_CFG = configmanager.get_config_wrapper(fpath=CSM_CONFIG)
-S3_LDAP_TST_CFG = configmanager.get_config_wrapper(fpath=S3_LDAP_TEST_CONFIG, target=target)
-S3_CFG = configmanager.get_config_wrapper(fpath=S3_CONFIG)
-S3_OBJ_TST = configmanager.get_config_wrapper(fpath=S3_OBJ_TEST_CONFIG)
-S3_BKT_TST = configmanager.get_config_wrapper(fpath=S3_BKT_TEST_CONFIG)
-S3CMD_CNF = configmanager.get_config_wrapper(fpath=S3CMD_TEST_CONFIG)
-S3_LDAP_TST_CFG = configmanager.get_config_wrapper(fpath=S3_LDAP_TEST_CONFIG, target=target,
-                                                   target_key="")
 RAS_VAL = configmanager.get_config_wrapper(fpath=RAS_CONFIG_PATH,
                                            target=target, target_key="csm")
 CMN_DESTRUCTIVE_CFG = configmanager.get_config_wrapper(fpath=COMMON_DESTRUCTIVE_CONFIG_PATH)
