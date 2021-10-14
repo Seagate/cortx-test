99bc3cf4a4112a3cd1dbc8fd20f8ae89c3e91af7
Zoheb Khan <zoheb.khan@seagate.com>
2021-10-14 05:16:05 -0600

    Motr lib for K8s env

---
+++ comptests/motr/test_motr_K8s_sanity.py
@@ -0,0 +1,30 @@
+import pytest
+import logging
+from libs.motr.motr_core_k8s_lib import MotrCoreK8s
+
+LOGGER = logging.getLogger(__name__)
+
+
+class TestExecuteK8Sanity:
+    """Execute Motr K8s Test suite"""
+
+    @pytest.yield_fixture(autouse=True)
+    def setup_class(self):
+        """ Setup class for running Motr tests"""
+        LOGGER.info("STARTED: Setup Operation")
+        self.motr_obj = MotrCoreK8s()
+        LOGGER.info("ENDED: Setup Operation")
+        yield
+        # Perform the clean up for each test.
+        LOGGER.info("STARTED: Teardown Operation")
+        del self.motr_obj
+        LOGGER.info("ENDED: Teardown Operation")
+
+    def test_motr_k8s_lib(self):
+        # TODO: This a sample test for the usuage, need to deleted it later
+        print(self.motr_obj.cluster_info)
+        print(self.motr_obj.profile_fid)
+        print(self.motr_obj.node_dict)    
+        print(self.motr_obj.storage_nodes)
+        print(self.motr_obj.get_primary_podNode())
+        print(self.motr_obj.get_podNode_endpoints())
---
+++ libs/motr/motr_core_k8s_lib.py
@@ -0,0 +1,68 @@
+"""
+Python library contains methods which allows provides the services endpoints.
+"""
+
+import json
+import logging
+from commons.helpers.pods_helper import LogicalNode
+from config import CMN_CFG
+
+log = logging.getLogger(__name__)
+
+class MotrCoreK8s:
+    
+    def __init__(self):
+        self.profile_fid = None
+        self.cluster_info = None 
+        self.node_obj = LogicalNode(hostname=CMN_CFG["nodes"][0]["hostname"],
+                                username=CMN_CFG["nodes"][0]["username"],
+                                password=CMN_CFG["nodes"][0]["password"])
+        self.storage_nodes = self._get_storage_nodes_list
+        self.node_dict = self._get_cluster_info
+        self.num_nodes = len(self.storage_nodes)
+    
+    @property
+    def _get_storage_nodes_list(self):
+        cmd = "kubectl get pods | awk '/storage-node/ {print $1}'"
+        result = self.node_obj.execute_cmd(cmd=cmd, read_lines=True, exc=True)
+        return [node.strip() for node in result]
+         
+    @property
+    def _get_cluster_info(self):
+        storage_node=self.storage_nodes[0]
+        node_dict = {}
+        cmd = "kubectl exec -it {} -c cortx-hax -- hctl status --json".format(storage_node)
+        log.debug("Excuting commands inside hax container: {}".format(cmd))
+        self.cluster_info = json.loads(self.node_obj.execute_cmd(cmd=cmd, exc=True))
+        if self.cluster_info is not None:
+            self.profile_fid = self.cluster_info["profiles"][0]["fid"]
+            nodes_data = self.cluster_info["nodes"]
+            for node in nodes_data:
+                nodename = node["name"]
+                node_dict[nodename] = {}
+                node_dict[nodename]['m0client'] = []
+                for svc in node["svcs"]:
+                    if svc["name"] == "hax":
+                        node_dict[nodename]['hax_fid'] = svc["fid"]
+                        node_dict[nodename]['hax_ep'] = svc["ep"]
+                    if svc["name"] == "m0_client":
+                        node_dict[nodename]['m0client'].append({"ep":svc["ep"], "fid":svc["fid"]})
+            return node_dict
+
+    def get_primary_podNode(self):
+        storage_node=self.storage_nodes[0]
+        cmd1 = "kubectl exec -it {} -c cortx-hax -- hctl status".format(storage_node)
+        cmd2 = "awk -F ' '  '/(RC)/ { print $1 }'"
+        cmd = cmd1 + " | " + cmd2
+        log.debug("Excuting command inside hax container: {}".format(cmd))
+        primary_node = self.node_obj.execute_cmd(cmd=cmd, read_lines=False, exc=True).strip().decode("utf-8") 
+        return primary_node
+
+    def get_podNode_endpoints(self, storage_node=None):
+        if not storage_node:
+            storage_node = self.get_primary_podNode()
+        for node in self.node_dict.keys():
+            if node == storage_node:
+                return self.node_dict[node]
+
+        
