3197f5976cf2c2bb8a9b28d9ea5c1fed1c0e4fc8
Aditi Pande <aditi.pande@seagate.com>
2021-10-14 00:10:34 -0600

    TEST-28934 and TEST-28935 CSM Rest K8s tests
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>

--- commons/constants.py
+++ commons/constants.py
@@ -31,12 +31,6 @@ NWORKERS = 32
 # run in parallel.
 NGREENLETS = 32
 
-# Product Family and versions
-PROD_FAMILY_LC = "LC"
-PROD_FAMILY_LR = "LR"
-PROD_TYPE_K8S = "k8s"
-PROD_TYPE_NODE = "node"
-
 # RAS Paths
 BYTES_TO_READ = 8000
 ONE_BYTE_TO_READ = 1
@@ -53,7 +47,7 @@ RABBIT_MQ_LOCAL_PATH = "scripts/server_scripts/rabbitmq_reader.py"
 MSG_BUS_READER_LOCAL_PATH = "scripts/server_scripts/read_message_bus.py"
 ENCRYPTOR_FILE_PATH = "scripts/server_scripts/encryptor.py"
 STORAGE_ENCLOSURE_PATH = "/opt/seagate/cortx/provisioner/pillar/components" \
-                         "/storage_enclosure.sls"
+                        "/storage_enclosure.sls"
 CLUSTER_PATH = "/opt/seagate/cortx/provisioner/pillar/components/cluster.sls"
 RAS_CONFIG_PATH = "config/ras_config.yaml"
 SSPL_TEST_CONFIG_PATH = "config/ras_test.yaml"
@@ -79,7 +73,7 @@ SELINUX_FILE_PATH = "/etc/selinux/config"
 HEADERS_STREAM_UTILITIES = {"Content-type": "application/x-www-form-urlencoded",
                             "Accept": "text/plain"}
 URL_STREAM_UTILITIES = "http://utils-stream.sw.lcd.colo.seagate.com/utility" \
-                       "/api/public/v1/get_tripw"
+                      "/api/public/v1/get_tripw"
 NO_CMD_RECEIVED_MSG = "No command response received !!!"
 PCS_SSPL_SECTION = " Master/Slave Set: sspl-master [sspl]\n"
 RAS_CFG = "config/ras_config.yaml"
@@ -107,6 +101,9 @@ CONF_SYSFS_BASE_PATH = "SYSTEM_INFORMATION>sysfs_base_path"
 CONF_RAID_INTEGRITY = "RAIDINTEGRITYSENSOR>retry_interval"
 AUTHSERVER_CONFIG = "/opt/seagate/cortx/auth/resources/authserver.properties"
 LOCAL_COPY_PATH = tempfile.gettempdir()+"/authserver.properties"
+LOCAL_CONF_PATH = tempfile.gettempdir()+"/cluster.conf"
+CLUSTER_CONF_PATH = "/etc/cortx/cluster.conf"
+LDAP_SEARCH_DATA = "ldapsearch -x -b \"dc=s3,dc=seagate,dc=com\""
 
 """ S3 constants """
 LOCAL_S3_CERT_PATH = "/etc/ssl/stx-s3-clients/s3/ca.crt"
@@ -185,11 +182,6 @@ class Rest:
                                   "\"Principal\": {\"AWS\":\"$principal\"}}]}"
     SORT_BY_ERROR = "{\'sort_by\': [\'Must be one of: user_id, username," \
                     " user_type, created_time, updated_time.\']}"
-    CUSTOM_S3_USER = ["account_name", "account_email", "password", "access_key", "secret_key"]
-    S3_ACCESS_UL = 128
-    S3_ACCESS_LL = 16
-    S3_SECRET_UL = 40
-    S3_SECRET_LL = 8
     CSM_USER_LIST_OFFSET = 1
     CSM_USER_LIST_LIMIT = 5
     CSM_USER_LIST_SORT_BY = "username"
@@ -213,19 +205,6 @@ class Rest:
         },
         "required": ["total", "good"]
     }
-
-
-# aws cli errors
-AWS_CLI_ERROR = ["ServiceUnavailable",
-                 "MalformedPolicy",
-                 "InvalidRequest",
-                 "Forbidden",
-                 "Conflict",
-                 "InternalError",
-                 "InvalidArgument",
-                 "AccessDenied",
-                 "Failed:"]
-
 
 # cortxcli constants
 S3BUCKET_HELP = [
@@ -255,12 +234,12 @@ S3BUCKET_DELETE_HELP = [
     "optional arguments:",
     "-h, --help   show this help message and exit"]
 S3ACCOUNT_HELP_CMDS = [
-    "s3iamusers",
-    "support_bundle",
-    "system",
-    "s3buckets",
-    "s3accounts",
-    "s3bucketpolicy"]
+        "s3iamusers",
+        "support_bundle",
+        "system",
+        "s3buckets",
+        "s3accounts",
+        "s3bucketpolicy"]
 S3ACCOUNT_HELP = ["positional arguments:",
                   "{show,create,reset_password}",
                   "show                Displays S3 Accounts On the cli",
@@ -289,7 +268,7 @@ JSON_LIST_FORMAT = "json"
 TABLE_LIST_FORMAT = "table"
 XML_LIST_FORMAT = "xml"
 SUPPORT_BUNDLE_MSG = "Support bundle generation completed"
-CSM_USER_HELP = [
+CSM_USER_HELP =[
     "support_bundle",
     "alerts",
     "s3accounts",
@@ -303,19 +282,18 @@ TOKEN_NAME = "10Mnx/XE4tEN8xrzQTNp2iSGQxPjpcHXbIdZgJyIN7Y="
 PARAMS = {"CORTX_BUILD": "{0}", "HOST": "{1}", "HOST_PASS": "{2}", "DEBUG": "True"}
 PIP_CONFIG = "/etc/pip.conf"
 
-# Locking server
+#Locking server
 SHARED_LOCK = 'shared'
 EXCLUSIVE_LOCK = 'exclusive'
 
-
 class SwAlerts:
     SVCS_3P = [
-        #  "elasticsearch.service", # brings down the csm
-        #  "hare-consul-agent.service", # Disabled on VM EOS-20861
-        #  "slapd.service", # brings down the csm
+#        "elasticsearch.service", # brings down the csm
+#        "hare-consul-agent.service", # Disabled on VM EOS-20861
+#        "slapd.service", # brings down the csm
         "statsd.service",
         "rsyslog.service",
-        #  "lnet.service", brings down motr-io service
+#        "lnet.service", brings down motr-io service
         "salt-master.service",
         "salt-minion.service",
         "glusterd.service",
@@ -331,7 +309,6 @@ class SwAlerts:
     SVCS_3P_ENABLED_VM = list(set(SVCS_3P) - set(SVCS_3P_UNAVAIL_VM))
 
     SVC_LOAD_TIMEOUT_SEC = 30
-
     class AlertType:
         FAULT = "fault"
         RESOLVED = "fault_resolved"
