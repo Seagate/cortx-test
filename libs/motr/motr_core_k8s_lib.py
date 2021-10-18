bd4592c23d05db1daa21b9bb4b9b6e0bb540a248
Zoheb Khan <zoheb.khan@seagate.com>
2021-10-18 03:11:55 -0600

    Added doc strings
    Signed-off-by: Zoheb Khan <zoheb.khan@seagate.com>

--- libs/motr/motr_core_k8s_lib.py
+++ libs/motr/motr_core_k8s_lib.py
@@ -23,12 +23,18 @@ class MotrCoreK8s:
     
     @property
     def _get_storage_nodes_list(self):
+        """
+        Returns all the storage node names
+        """
         cmd = "kubectl get pods | awk '/storage-node/ {print $1}'"
         result = self.node_obj.execute_cmd(cmd=cmd, read_lines=True, exc=True)
         return [node.strip() for node in result]
          
     @property
     def _get_cluster_info(self):
+        """
+        Returns all the podNode's endpoints in a dict format
+        """
         storage_node=self.storage_nodes[0]
         node_dict = {}
         cmd = "kubectl exec -it {} -c cortx-hax -- hctl status --json".format(storage_node)
@@ -50,6 +56,12 @@ class MotrCoreK8s:
             return node_dict
 
     def get_primary_podNode(self):
+        """ 
+        To get the primary pod node name
+
+        :returns: Primary(RC) node name in the cluster
+        :rtype: str
+        """
         storage_node=self.storage_nodes[0]
         cmd1 = "kubectl exec -it {} -c cortx-hax -- hctl status".format(storage_node)
         cmd2 = "awk -F ' '  '/(RC)/ { print $1 }'"
@@ -59,6 +71,14 @@ class MotrCoreK8s:
         return primary_node
 
     def get_podNode_endpoints(self, storage_node=None):
+        """ 
+        To get the endpoints details of the podNode/storage node
+        
+        :param storage_node: Name of the storage node
+        :type: str
+        :returns: Node dict of a storage node containing the endpoints details
+        :rtype: dict
+        """
         if not storage_node:
             storage_node = self.get_primary_podNode()
         for node in self.node_dict.keys():
