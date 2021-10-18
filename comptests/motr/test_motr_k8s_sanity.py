6d1ab1eb34b3702680a7ff3814dedcd6ed7db471
Zoheb Khan <zoheb.khan@seagate.com>
2021-10-18 07:50:50 -0600

    codacy issues fixes
    Signed-off-by: Zoheb Khan <zoheb.khan@seagate.com>

--- comptests/motr/test_motr_k8s_sanity.py
+++ comptests/motr/test_motr_k8s_sanity.py
@@ -1,3 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+
+"""
+Test class that contains K8s tests.
+"""
+
 import logging
 import pytest
 from libs.motr.motr_core_k8s_lib import MotrCoreK8s
@@ -27,7 +51,7 @@ class TestExecuteK8Sanity:
         # TODO: This a sample test for the usage, need to delete it later
         print(self.motr_obj.cluster_info)
         print(self.motr_obj.profile_fid)
-        print(self.motr_obj.node_dict)    
+        print(self.motr_obj.node_dict)
         print(self.motr_obj.storage_nodes)
         print(self.motr_obj.get_primary_podNode())
         print(self.motr_obj.get_podNode_endpoints())
