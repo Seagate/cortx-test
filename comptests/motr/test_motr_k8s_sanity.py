575900a049048b80af0e3b917abf0fbb8a16c554
Zoheb Khan <zoheb.khan@seagate.com>
2021-10-18 08:08:22 -0600

    review comments
    Signed-off-by: Zoheb Khan <zoheb.khan@seagate.com>

--- comptests/motr/test_motr_k8s_sanity.py
+++ comptests/motr/test_motr_k8s_sanity.py
@@ -26,7 +26,7 @@ import logging
 import pytest
 from libs.motr.motr_core_k8s_lib import MotrCoreK8s
 
-LOGGER = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 class TestExecuteK8Sanity:
@@ -35,23 +35,23 @@ class TestExecuteK8Sanity:
     @pytest.yield_fixture(autouse=True)
     def setup_class(self):
         """ Setup class for running Motr tests"""
-        LOGGER.info("STARTED: Setup Operation")
+        logger.info("STARTED: Setup Operation")
         self.motr_obj = MotrCoreK8s()
-        LOGGER.info("ENDED: Setup Operation")
+        logger.info("ENDED: Setup Operation")
         yield
         # Perform the clean up for each test.
-        LOGGER.info("STARTED: Teardown Operation")
+        logger.info("STARTED: Teardown Operation")
         del self.motr_obj
-        LOGGER.info("ENDED: Teardown Operation")
+        logger.info("ENDED: Teardown Operation")
 
     def test_motr_k8s_lib(self):
         """
         Sample test
         """
         # TODO: This a sample test for the usage, need to delete it later
-        print(self.motr_obj.cluster_info)
-        print(self.motr_obj.profile_fid)
-        print(self.motr_obj.node_dict)
-        print(self.motr_obj.storage_nodes)
-        print(self.motr_obj.get_primary_podNode())
-        print(self.motr_obj.get_podNode_endpoints())
+        logger.info(self.motr_obj.cluster_info)
+        logger.info(self.motr_obj.profile_fid)
+        logger.info(self.motr_obj.node_dict)
+        logger.info(self.motr_obj.storage_nodes)
+        logger.info(self.motr_obj.get_primary_podNode())
+        logger.info(self.motr_obj.get_podNode_endpoints())
