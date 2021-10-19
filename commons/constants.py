d353471f7a7557b087a019b6c597f48064273ed2
aditipande96 <86783066+aditipande96@users.noreply.github.com>
2021-10-19 17:19:44 +0530

    Eos 25219 CSM REST K8S tests (#782)
    * TEST-28934 and TEST-28935 CSM Rest K8s tests
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    * Retained changes in constants.py
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    * Codacy fix
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    * Codacy fix
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    * Codacy fix
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    * Codacy fix
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    * Codacy fix
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    * Moved decryption functions to new library file
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    * Codacy fix
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    * Codacy fixes
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    * Codacy fix
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    * S3 account creation step
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    * Codacy fix
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    * Codacy fix
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    * Addressing PR comments
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    * Changed file names and location
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    * Correct ldap_utils location
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    * Deleting old file
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    * Codacy fixes
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    * Codacy fix
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    * Codacy fixes
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    * Removed class in ldap utils
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    * Codacy fixes
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    * Codacy fixes
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    * Codacy fix
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>
    
    * Codacy fix
    
    Signed-off-by: Aditi Pande <aditi.pande@seagate.com>

--- commons/constants.py
+++ commons/constants.py
@@ -112,6 +112,8 @@ CONF_SYSFS_BASE_PATH = "SYSTEM_INFORMATION>sysfs_base_path"
 CONF_RAID_INTEGRITY = "RAIDINTEGRITYSENSOR>retry_interval"
 AUTHSERVER_CONFIG = "/opt/seagate/cortx/auth/resources/authserver.properties"
 LOCAL_COPY_PATH = tempfile.gettempdir()+"/authserver.properties"
+LOCAL_CONF_PATH = tempfile.gettempdir()+"/cluster.conf"
+CLUSTER_CONF_PATH = "/etc/cortx/cluster.conf"
 
 """ S3 constants """
 LOCAL_S3_CERT_PATH = "/etc/ssl/stx-s3-clients/s3/ca.crt"
