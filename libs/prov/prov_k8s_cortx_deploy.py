ef1d857bcaa7f3dfaf9f7cfc6d1a7770443d777e
pragamj <86817153+pragamj@users.noreply.github.com>
2021-10-19 15:58:56 +0530

    Added the code to update the solution.yaml file (#771)
    * Added the code to update the solution.yaml file
    
    Signed-off-by: pragam <pragam.jain@seagate.com>
    
    * Updated the code to update the solution.yaml file
    
    Signed-off-by: pragam <pragam.jain@seagate.com>
    
    * Updated the code to update the solution.yaml file
    
    Signed-off-by: pragam <pragam.jain@seagate.com>
    
    * Updated in the lib file
    
    Signed-off-by: pragam <pragam.jain@seagate.com>
    
    * Removed the standalone script
    
    Signed-off-by: pragam <pragam.jain@seagate.com>
    
    * Implemented review comments
    
    Signed-off-by: pragam <pragam.jain@seagate.com>
    
    * Implemented review comments
    
    Signed-off-by: pragam <pragam.jain@seagate.com>
    
    * Updated with latest solution.yaml changes
    
    Signed-off-by: pragam <pragam.jain@seagate.com>
    
    * Updated with filepath changes
    
    Signed-off-by: pragam <pragam.jain@seagate.com>
    
    * Implemented review comments
    
    Signed-off-by: pragam <pragam.jain@seagate.com>

--- libs/prov/prov_k8s_cortx_deploy.py
+++ libs/prov/prov_k8s_cortx_deploy.py
@@ -278,3 +278,160 @@ class ProvDeployK8sCortxLib:
             return resp
 
         return resp
+
+    def checkout_solution_file(self, token, git_tag):
+        url = self.deploy_cfg["git_k8_repo_file"].format(token, git_tag)
+        cmd = common_cmd.CMD_CURL.format(self.deploy_cfg["template_path"], url)
+        system_utils.execute_cmd(cmd=cmd)
+        return self.deploy_cfg["template_path"]
+
+    def update_sol_yaml(self, worker_obj: list, filepath,
+                        **kwargs):
+        """
+        This function updates the yaml file
+        :Param: obj: list of node object
+        :Param: node_list:int the count of worker nodes
+        :Param: filepath: Filename with complete path
+        :Keyword: cluster_id: cluster id
+        :Keyword: cvg_count: cvg_count per node
+        :Keyword: type_cvg: ios or cas
+        :Keyword: data_disk_per_cvg: data disk required per cvg
+        :Keyword: sns_data: N
+        :Keyword: sns_parity: K
+        :Keyword: sns_spare: S
+        :Keyword: dix_data:
+        :Keyword: dix_parity:
+        :Keyword: dix_spare:
+        :Keyword: size_metadata: size of metadata disk
+        :Keyword: size_data_disk: size of data disk
+        :Keyword: skip_disk_count_check: disk count check
+        returns the status, filepath and system reserved disk
+
+        """
+        # cluster_id = kwargs.get("cluster_id", 1)
+        cvg_count = kwargs.get("cvg_count", 1)
+        cvg_type = kwargs.get("cvg_type", "ios")
+        data_disk_per_cvg = kwargs.get("data_disk_per_cvg", "0")
+        sns_data = kwargs.get("sns_data", 1)
+        sns_parity = kwargs.get("sns_parity", 0)
+        sns_spare = kwargs.get("sns_spare", 0)
+        dix_data = kwargs.get("dix_data", 1)
+        dix_parity = kwargs.get("dix_parity", 2)
+        dix_spare = kwargs.get("dix_spare", 0)
+        size_metadata = kwargs.get("size_metadata", '5Gi')
+        size_data_disk = kwargs.get("size_data_disk", '5Gi')
+        skip_disk_count_check = kwargs.get("skip_disk_count_check", False)
+        new_filepath = self.deploy_cfg['new_file_path']
+        data_devices = list()  # empty list for data disk
+        sys_disk_pernode = {}  # empty dict
+        node_list = len(worker_obj)
+        nks = "{}+{}+{}".format(sns_data, sns_parity, sns_spare)  # Value of N+K+S for sns
+        dix = "{}+{}+{}".format(dix_data, dix_parity, dix_spare)  # Value of N+K+S for dix
+        valid_disk_count = sns_spare + sns_data + sns_parity
+
+        for node_count, node_obj in enumerate(worker_obj, start=1):
+            LOGGER.info(node_count)
+            device_list = node_obj.execute_cmd(cmd=common_cmd.CMD_LIST_DEVICES,
+                                               read_lines=True)[0].split(",")
+            device_list[-1] = device_list[-1].replace("\n", "")
+            metadata_devices = device_list[1:cvg_count + 1]
+            # This will split the metadata disk list
+            # into metadata devices per cvg
+            # 2 is defined the split size based
+            # on disk required for metadata,system
+            device_list_len = len(device_list)
+            new_device_lst_len = (device_list_len - cvg_count)
+            count = cvg_count
+            if data_disk_per_cvg == "0":
+                data_disk_per_cvg = len(device_list[cvg_count + 1:])
+            # The condition to validate the config.
+            if not skip_disk_count_check and valid_disk_count > \
+                    (data_disk_per_cvg * cvg_count * node_list):
+                return False, "The sum of data disks per cvg " \
+                              "is less than N+K+S count"
+            if len(data_devices) < data_disk_per_cvg*cvg_count:
+                return False, "The requested data disk is more than" \
+                              " the data disk available on the system"
+            # This condition validated the total available disk count
+            # and split the disks per cvg.
+
+            if (data_disk_per_cvg * cvg_count) < new_device_lst_len and data_disk_per_cvg != "0":
+                count_end = int(data_disk_per_cvg + cvg_count + 1)
+                data_devices.append(device_list[cvg_count + 1:count_end])
+                while count:
+                    count = count - 1
+                    new_end = int(count_end + data_disk_per_cvg)
+                    if new_end > new_device_lst_len:
+                        break
+                    data_devices_ad = device_list[count_end:new_end]
+                    count_end = int(count_end + data_disk_per_cvg)
+                    data_devices.append(data_devices_ad)
+            else:
+                data_devices_f = device_list[cvg_count:]
+                data_devices = [data_devices_f[i:i + data_disk_per_cvg]
+                                for i in range(0, len(data_devices_f), data_disk_per_cvg)]
+
+            # Create dict for host and disk
+            system_disk = device_list[0]
+            schema = {node_obj.hostname: system_disk}
+            sys_disk_pernode.update(schema)
+
+        # Reading the yaml file
+        with open(filepath) as soln:
+            conf = yaml.safe_load(soln)
+            parent_key = conf['solution']  # Parent key
+            common = parent_key['common']  # Parent key
+            storage = parent_key['storage']  # child of child key
+            cmn_storage_sets = common['storage_sets']  # child of child key
+            node = parent_key['nodes']  # Child Key
+            total_nodes = node.keys()
+            total_cvg = storage.keys()
+            # Creating Default Schema to update the yaml file
+            share_value = "/mnt/fs-local-volume"  # This needs to changed
+            device_schema = {'system': share_value}
+            device_key = {'devices': device_schema}
+            # SNS and dix value update
+            cmn_storage_sets['durability']['sns'] = nks
+            cmn_storage_sets['durability']['dix'] = dix
+            # Removing the elements from the node dict
+            for key_count in list(total_nodes):
+                node.pop(key_count)
+            for cvg in list(total_cvg):
+                storage.pop(cvg)
+            # Updating the node dict
+            for item, host in zip(list(range(node_list)), worker_obj):
+                dict_node = {}
+                name = {'name': host.hostname}
+                dict_node.update(name)
+                dict_node.update(device_key)
+                new_node = {'node{}'.format(item + 1): dict_node}
+                node.update(new_node)
+            # Updating the metadata and data disk
+            for cvg in range(0, cvg_count):
+                cvg_dict = {}
+                metadata_schema_upd = {'devices': metadata_devices[cvg], 'size': size_metadata}
+                data_schema = {}
+                for disk in range(0, data_disk_per_cvg):
+                    disk_schema_upd = {'device': data_devices[cvg][disk], 'size': size_data_disk}
+                    c_data_device_schema = {'d{}'.format(disk + 1): disk_schema_upd}
+                    data_schema.update(c_data_device_schema)
+                c_device_schema = {'metadata': metadata_schema_upd, 'data': data_schema}
+                key_cvg_devices = {'devices': c_device_schema}
+                cvg_name = {'name': 'cvg-0{}'.format(cvg + 1)}
+                cvg_type_schema = {'type': cvg_type}
+                cvg_dict.update(cvg_name)
+                cvg_dict.update(cvg_type_schema)
+                cvg_dict.update(key_cvg_devices)
+                cvg_key = {'cvg{}'.format(cvg + 1): cvg_dict}
+                storage.update(cvg_key)
+
+            conf['solution']['nodes'] = node
+            conf['solution']['storage'] = storage
+            soln.close()
+        noalias_dumper = yaml.dumper.SafeDumper
+        noalias_dumper.ignore_aliases = lambda self, data: True
+        with open(new_filepath, 'w') as soln:
+            yaml.dump(conf, soln, default_flow_style=False,
+                      sort_keys=False, Dumper=noalias_dumper)
+            soln.close()
+        return True, new_filepath, sys_disk_pernode
