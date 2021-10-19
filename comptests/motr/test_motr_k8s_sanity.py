d2d5cbdeb7bde6d9c785898f4ac6226a0abb15a9
zohebkhann <89624145+zohebkhann@users.noreply.github.com>
2021-10-19 10:17:54 +0530

    Eos 25347 motr k8s (#786)
    * Motr lib for K8s env
    
    * cosmetic changes
    
    * minor changes
    
    * Added doc strings
    
    Signed-off-by: Zoheb Khan <zoheb.khan@seagate.com>
    
    * codacy issues fix
    
    Signed-off-by: Zoheb Khan <zoheb.khan@seagate.com>
    
    * codacy issues fixes
    
    Signed-off-by: Zoheb Khan <zoheb.khan@seagate.com>
    
    * review comments
    
    Signed-off-by: Zoheb Khan <zoheb.khan@seagate.com>
    
    * Delete test_motr_K8s_sanity.py
    
    * codacy fix
    
    Signed-off-by: Zoheb Khan <zoheb.khan@seagate.com>
    
    * codacy fixes 2
    
    Signed-off-by: Zoheb Khan <zoheb.khan@seagate.com>
    
    * minor fix
    
    Signed-off-by: Zoheb Khan <zoheb.khan@seagate.com>
    
    * Review comments
    
    Signed-off-by: Zoheb Khan <zoheb.khan@seagate.com>

---
+++ comptests/motr/test_motr_k8s_sanity.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+
+"""
+Test class that contains MOTR K8s tests.
+"""
+
+import logging
+from libs.motr.motr_core_k8s_lib import MotrCoreK8s
+
+logger = logging.getLogger(__name__)
+
+
+class TestExecuteK8Sanity:
+    """Execute Motr K8s Test suite"""
+
+    @classmethod
+    def setup_class(cls):
+        """ Setup class for running Motr tests"""
+        logger.info("STARTED: Setup Operation")
+        logger.info("ENDED: Setup Operation")
+        cls.motr_obj = MotrCoreK8s()
+        yield
+        # Perform the clean up for each test.
+        logger.info("STARTED: Teardown Operation")
+        del cls.motr_obj
+        logger.info("ENDED: Teardown Operation")
+
+    @classmethod
+    def test_motr_k8s_lib(cls):
+        """
+        Sample test
+        """
+        # TODO: This a sample test for the usage, need to delete it later
+        logger.info(cls.motr_obj.cluster_info)
+        logger.info(cls.motr_obj.profile_fid)
+        logger.info(cls.motr_obj.node_dict)
+        logger.info(cls.motr_obj.storage_nodes)
+        logger.info(cls.motr_obj.get_primary_podNode())
+        logger.info(cls.motr_obj.get_podNode_endpoints())
