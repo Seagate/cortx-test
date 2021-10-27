9797baa7204d510a5b31bcf2f0b9c0e01c678b93
Sampada Petkar <sampada.petkar@seagate.com>
2021-10-27 04:55:17 -0600

    EOS-24517: review changes
    Signed-off-by: Sampada Petkar <sampada.petkar@seagate.com>

--- tools/dash_server/Performance/statistics/statistics_callbacks.py
+++ tools/dash_server/Performance/statistics/statistics_callbacks.py
@@ -24,41 +24,41 @@ from dash.exceptions import PreventUpdate
 from common import app
 from Performance.backend import *
 
-'''
-@app.callback(
-    Output('statistics_workload', 'children'),
-    Input('perf_release_dropdown', 'value'),
-    Input('perf_branch_dropdown', 'value'),
-    Input('perf_build_dropdown', 'value'),
-    Input('perf_nodes_dropdown', 'value'),
-    Input('perf_pfull_dropdown', 'value'),
-    Input('perf_iteration_dropdown', 'value'),
-    Input('perf_custom_dropdown', 'value'),
-    Input('perf_submit_button', 'n_clicks'),
-    Input('perf_sessions_dropdown', 'value'),
-    Input('perf_buckets_dropdown', 'value'),
-)
-def update_workload(release_combined, branch, build, nodes,
-                    pfull, itrns, custom, n_clicks, sessions, buckets):
-    workload = None
-    if not (all([
-        release_combined, branch, build, nodes, itrns, custom, n_clicks, sessions, buckets
-    ])) and pfull is None:
-        raise PreventUpdate
 
-    if n_clicks > 0:
-        release = release_combined.split("_")[0]
-        op_sys = release_combined.split("_")[1]
-        data = {
-            'release': release, 'OS': op_sys, 'build': build, 'branch': branch,
-            'nodes': nodes, 'pfull': pfull, 'itrns': itrns, 'custom': custom,
-            'buckets': buckets, 'sessions': sessions, 'name': 'S3bench'
-        }
+# @app.callback(
+#     Output('statistics_workload', 'children'),
+#     Input('perf_release_dropdown', 'value'),
+#     Input('perf_branch_dropdown', 'value'),
+#     Input('perf_build_dropdown', 'value'),
+#     Input('perf_nodes_dropdown', 'value'),
+#     Input('perf_pfull_dropdown', 'value'),
+#     Input('perf_iteration_dropdown', 'value'),
+#     Input('perf_custom_dropdown', 'value'),
+#     Input('perf_submit_button', 'n_clicks'),
+#     Input('perf_sessions_dropdown', 'value'),
+#     Input('perf_buckets_dropdown', 'value'),
+# )
+# def update_workload(release_combined, branch, build, nodes,
+#                     pfull, itrns, custom, n_clicks, sessions, buckets):
+#     workload = None
+#     if not (all([
+#         release_combined, branch, build, nodes, itrns, custom, n_clicks, sessions, buckets
+#     ])) and pfull is None:
+#         raise PreventUpdate
 
-        workload = get_workload_headings(data)
+#     if n_clicks > 0:
+#         release = release_combined.split("_")[0]
+#         op_sys = release_combined.split("_")[1]
+#         data = {
+#             'release': release, 'OS': op_sys, 'build': build, 'branch': branch,
+#             'nodes': nodes, 'pfull': pfull, 'itrns': itrns, 'custom': custom,
+#             'buckets': buckets, 'sessions': sessions, 'name': 'S3bench'
+#         }
 
-    return workload
-'''
+#         workload = get_workload_headings(data)
+
+#     return workload
+
 
 @app.callback(
     Output('statistics_s3bench_table', 'children'),
