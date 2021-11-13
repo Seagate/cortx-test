80a84cc02edc624fdbcc238aa6910139d094c118
Sonal Kalbende <sonal.kalbende@seagate.com>
2021-11-13 15:56:24 +0530

    y
    Signed-off-by: Sonal Kalbende <sonal.kalbende@seagate.com>

--- scripts/cicd_k8s/lb_haproxy.sh
+++ scripts/cicd_k8s/lb_haproxy.sh
@@ -3,7 +3,7 @@ yum install gcc pcre-devel tar make -y
 yum install openssl-devel pcre-devel -y
 
 yum-config-manager --add-repo http://cortx-storage.colo.seagate.com/releases/cortx/third-party-deps/centos/centos-7.9.2009-2.0.0-k8/commons/haproxy-packages/
-yum install haproxy22 --nogpgcheck
+yum install haproxy22 --nogpgcheck -y
 
 sudo mkdir -p /etc/haproxy
 sudo mkdir -p /var/lib/haproxy
