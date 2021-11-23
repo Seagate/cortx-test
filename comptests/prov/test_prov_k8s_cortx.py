c45ab70ae6fbd223fec359ec076c948eca030866
ashukakkar <karan.kakkar@seagate.com>
2021-11-23 13:52:43 +0530

    Resolved review comments.
    Signed-off-by: ashukakkar <karan.kakkar@seagate.com>

--- comptests/prov/test_prov_k8s_cortx.py
+++ comptests/prov/test_prov_k8s_cortx.py
@@ -226,21 +226,21 @@ class TestProvK8Cortx:
         LOGGER.info("Step 1: Get all running data pods from cluster.")
         data_pod_list = ProvDeployK8sCortxLib.get_data_pods(self.master_node_obj)
         assert_utils.assert_true(data_pod_list[0])
-        cluster_yaml_path = "cat /etc/cortx/solution/cluster.yaml | grep id"
-        cluster_conf_path = "cat /etc/cortx/cluster.conf | grep cluster_id"
+        cluster_yaml_cmd = "cat " + self.deploy_cfg["cluster_yaml_path"] + " | grep id"
+        cluster_conf_cmd = "cat " + self.deploy_cfg["cluster_conf_path"] + " | grep cluster_id"
         LOGGER.info("Step 2: Fetch Cluster ID from cluster.yaml and cluster.conf.")
         for pod_name in data_pod_list[1]:
             cluster_id_yaml = self.master_node_obj.execute_cmd(cmd=commands.
                                                                K8S_POD_INTERACTIVE_CMD.
                                                                format(pod_name,
-                                                                      cluster_yaml_path),
+                                                                      cluster_yaml_cmd),
                                                                read_lines=True)
             assert_utils.assert_is_not_none(cluster_id_yaml)
             cluster_id_yaml = cluster_id_yaml[0].split("\n")[0].strip().split(":")[1].strip()
             cluster_id_conf = self.master_node_obj.execute_cmd(cmd=commands.
                                                                K8S_POD_INTERACTIVE_CMD.
                                                                format(pod_name,
-                                                                      cluster_conf_path),
+                                                                      cluster_conf_cmd),
                                                                read_lines=True)
             assert_utils.assert_is_not_none(cluster_id_conf)
             cluster_id_conf = cluster_id_conf[0].split("\n")[0].strip().split(":")[1].strip()
