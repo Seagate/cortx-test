c1b04ec76a10e5d97a5ef4eea0818839926c87db
pragam <pragam.jain@seagate.com>
2021-11-26 13:03:14 +0530

    added script in params to skip health chk
    Signed-off-by: pragam <pragam.jain@seagate.com>

--- commons/params.py
+++ commons/params.py
@@ -122,4 +122,4 @@ LOCAL_S3_CONFIG = os.path.join(tempfile.gettempdir(), 's3config.yaml')
 DT_PATTERN_PREFIX = '%Y%m%d-%H%M%S'
 
 PROV_SKIP_TEST_FILES_HEALTH_CHECK_PREFIX = ['test_prov', 'test_failure_domain',
-                                            'test_multiple_config_deploy']
+                                            'test_multiple_config_deploy', 'test_cont_deployment']
