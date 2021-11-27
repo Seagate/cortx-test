fe64fe56f79b19db22f8090a92465a1b69e03ea1
pragam <pragam.jain@seagate.com>
2021-11-27 10:36:11 +0530

    Fix codacy error
    Signed-off-by: pragam <pragam.jain@seagate.com>

--- scripts/cicd_k8s_cortx_deploy/log_collecter.sh
+++ scripts/cicd_k8s_cortx_deploy/log_collecter.sh
@@ -4,6 +4,7 @@
   LOG_PATH=$dir_path/$log_path
   cmd="mkdir -p $dir_path"
   cmd2="mount -l|grep nfs4"
+  cmd3="mkdir -p $LOG_PATH"
   mount_cmd="mount cftic2.pun.seagate.com:/cftshare_temp $dir_path"
   mv_cmd="mv $3/log/latest/TEST-N* $LOG_PATH"
 
@@ -12,7 +13,7 @@
       echo "INFO: Directory exists."
   else
       echo "DEBUG: Directory does not exists.Will create the dir"
-      eval $cmd
+      eval "$cmd"
   fi
 
   if [[ $(mount -l|grep "$dir_path") ]]; then
@@ -20,21 +21,21 @@
   else
       echo "DEBUG: Not mounted"
           echo "INFO: Mount the dir"
-          eval $mount_cmd
+          eval "$mount_cmd"
           echo "INFO: Verify the nfs share is mounted"
-          eval $cmd2
+          eval "$cmd2"
 
   fi
   if [ -d $LOG_PATH ]
   then
       echo "INFO: Directory exists"
       echo "INFO: Copying logs to nfs share"
-      eval $mv_cmd
+      eval "$mv_cmd"
       echo "INFO: Copied the logs: $LOG_PATH"
   else
       echo "INFO: Directory not exists"
       echo "INFO: Creating dir"
-      `mkdir -p $LOG_PATH`
-      eval $mv_cmd
+      eval "$cmd3"
+      eval "$mv_cmd"
 	  echo "INFO: Copied the logs: $LOG_PATH"
   fi
