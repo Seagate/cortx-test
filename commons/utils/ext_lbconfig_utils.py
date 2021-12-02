b30dbf59b30db868b624afd53b4bf72ffebe5409
Ankita Thummar <ankita.thummar@seagate.com>
2021-12-02 12:15:12 +0530

    haproxy config chagne to latest and logic for rsysconf configuration
    Signed-off-by: Ankita Thummar <ankita.thummar@seagate.com>

--- commons/utils/ext_lbconfig_utils.py
+++ commons/utils/ext_lbconfig_utils.py
@@ -35,18 +35,56 @@ from commons.utils import system_utils as sys_utils
 LOGGER = logging.getLogger(__name__)
 
 
+def configure_rsyslog():
+    """
+     Configure rsyslog to configure logging of the HAProxy service
+    """
+    rsysconf_path = "/etc/rsyslog.conf"
+    if os.path.exists(rsysconf_path):
+        with open(rsysconf_path, 'r') as f_read:
+            rsyslog = f_read.readlines()
+        with open("/etc/rsyslog_dummy.conf", 'w') as f_write:
+            for line in rsyslog:
+                if "# Provides UDP syslog reception" in line:
+                    f_write.write(line)
+                    f_write.write(
+                        f"$ModLoad imudp\n$UDPServerAddress 127.0.0.1\n$UDPServerRun 514\n")
+                    continue
+                if "$ModLoad imudp" in line or "#$UDPServerAddress 127.0.0.1" in line\
+                        or "#$UDPServerRun 514" in line:
+                    continue
+                f_write.write(line)
+        sys_utils.execute_cmd("rm -f {}".format(rsysconf_path))
+        sys_utils.execute_cmd("mv {} {}".format("/etc/rsyslog_dummy.conf", rsysconf_path))
+        sys_utils.execute_cmd("rm -f {}".format("/etc/rsyslog_dummy.conf"))
+    else:
+        LOGGER.info(f"Couldn't find {rsysconf_path}")
+    rsyslogd_path = "/etc/rsyslog.d/haproxy.conf"
+    haproxylog_path = "/var/log/haproxy.log"
+    if os.path.exists(rsyslogd_path):
+        sys_utils.execute_cmd("rm -f {}".format(rsyslogd_path))
+    sys_utils.execute_cmd(cmd="mkdir -p /etc/rsyslog.d/")
+    with open(rsyslogd_path, 'w') as f_write:
+        f_write.write(f"if ($programname == 'haproxy') then -{haproxylog_path}\n")
+
+    if os.path.exists(haproxylog_path):
+        sys_utils.execute_cmd("rm -f {}".format(haproxylog_path))
+    sys_utils.execute_cmd(f"touch {haproxylog_path}")
+    sys_utils.execute_cmd(f"chmod 755 {haproxylog_path}")
+    resp = sys_utils.execute_cmd(cmd=cm_cmd.SYSTEM_CTL_RESTART_CMD.format("rsyslog"))
+    assert_utils.assert_true(resp[0], resp[1])
+
+
 # pylint: disable=too-many-branches
 # pylint: disable=too-many-statements
 # pylint: disable=too-many-locals
-def configure_haproxy_lb(m_node: str, username: str, password: str, ext_ip: str,
-                         pem_remote_path: str = cm_const.K8S_PEM_PATH):
+def configure_haproxy_lb(m_node: str, username: str, password: str, ext_ip: str):
     """
     Implement external Haproxy LB
     :param m_node: hostname for master node
     :param username: username for node
     :param password: password for node
     :param ext_ip: External LB IP from client node setup
-    :param pem_remote_path: Remote file path from master node for .pem file
     """
     m_node_obj = LogicalNode(hostname=m_node, username=username, password=password)
     resp = m_node_obj.execute_cmd(cmd=cm_cmd.K8S_WORKER_NODES, read_lines=True)
@@ -95,34 +133,41 @@ def configure_haproxy_lb(m_node: str, username: str, password: str, ext_ip: str,
             if "# 443 cortx_setup_https" in line:
                 for index, worker in enumerate(worker_node.keys(), 1):
                     line = f"    server ha-s3-ssl-{index} {worker_node[worker]['eth1']}:" \
-                           f"{worker_node[worker]['443']} ssl verify none    #port mapped to 443\n"
+                           f"{worker_node[worker]['443']} check ssl verify none    #port mapped to 443\n"
                     f_write.write(line)
                 continue
             if "# 9080 s3_auth" in line:
                 for index, worker in enumerate(worker_node.keys(), 1):
                     line = f"    server s3authserver-instance{index} " \
                            f"{worker_node[worker]['eth1']}:{worker_node[worker]['9080']}    " \
-                           f"#port mapped to 9080\n"
+                           f" check ssl verify none #port mapped to 9080\n"
                     f_write.write(line)
                 continue
             if "# 9443 s3_auth_https" in line:
                 for index, worker in enumerate(worker_node.keys(), 1):
                     line = f"    server s3authserver-instance-ssl-{index} " \
                            f"{worker_node[worker]['eth1']}:{worker_node[worker]['9443']} " \
-                           f"ssl verify none    #port mapped to 9443\n"
+                           f" check ssl verify none    #port mapped to 9443\n"
                     f_write.write(line)
                 continue
             f_write.write(line)
-    LOGGER.info("Coping the PEM from one of the nodes of CORTX deployment to the LB host.")
+    LOGGER.info("Configuring rsyslog to Configure Logging for HAProxy")
+    configure_rsyslog()
+    LOGGER.info("Coping the PEM from /Seagate/cortx-s3server/kubernetes/scripts/haproxy/ssl/")
     pem_local_path = "/etc/ssl/stx/stx.pem"
     if os.path.exists(pem_local_path):
         sys_utils.execute_cmd("rm -f {}".format(pem_local_path))
     sys_utils.execute_cmd(cmd="mkdir -p /etc/ssl/stx/")
-    m_node_obj.copy_file_to_local(remote_path=pem_remote_path, local_path=pem_local_path)
-
+    sys_utils.execute_cmd("curl https://raw.githubusercontent.com/Seagate/cortx-s3server/"
+                          "kubernetes/scripts/haproxy/ssl/s3.seagate.com.crt"
+                          " -o /etc/ssl/stx-s3-clients/s3/ca.crt")
+    sys_utils.execute_cmd("curl https://raw.githubusercontent.com/"
+                          "Seagate/cortx-prvsnr/4c2afe1c19e269ecb6fbf1cba62fdb7613508182/srv/"
+                          "components/misc_pkgs/ssl_certs/files/stx.pem -o /etc/ssl/stx/stx.pem")
     resp = sys_utils.execute_cmd(cmd=cm_cmd.SYSTEM_CTL_RESTART_CMD.format("haproxy"))
     assert_utils.assert_true(resp[0], resp[1])
-
+    resp = sys_utils.execute_cmd("puppet agent --disable")
+    assert_utils.assert_true(resp[0], resp[1])
     LOGGER.info("Setting s3 endpoints of ext LB on client.")
     sys_utils.execute_cmd(cmd="rm -f /etc/hosts")
     with open("/etc/hosts", 'w') as file:
