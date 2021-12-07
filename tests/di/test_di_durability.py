c61c200436608209b939b9e170cf9c05e2528417
seagate-sarang-sawant <70256535+seagate-sarang-sawant@users.noreply.github.com>
2021-12-07 14:19:33 +0530

    EOS-26515 Removed duplicate test.

--- tests/di/test_di_durability.py
+++ tests/di/test_di_durability.py
@@ -863,7 +863,7 @@ class TestDIDurability:
     @pytest.mark.skip(reason="Feature is not in place hence marking skip.")
     @pytest.mark.data_integrity
     @pytest.mark.data_durability
-    @pytest.mark.tags('TEST-22912')
+    @pytest.mark.tags('TEST-22913')
     def test_22913(self):
         """
         Test to verify object integrity during the the upload with correct checksum.
