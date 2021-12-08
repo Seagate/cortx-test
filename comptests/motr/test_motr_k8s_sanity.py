08623b0f1da145e5b803de3a8ddc194ea73d2fa4
Gaurav Gaur <gaurav.gaur@seagate.com>
2021-12-08 12:47:15 +0530

    Added Motr m0* commands and a basic sanity test
    Signed-off-by: Gaurav Gaur <gaurav.gaur@seagate.com>

--- comptests/motr/test_motr_k8s_sanity.py
+++ comptests/motr/test_motr_k8s_sanity.py
@@ -24,6 +24,8 @@ Test class that contains MOTR K8s tests.
 
 import logging
 from libs.motr.motr_core_k8s_lib import MotrCoreK8s
+from libs.motr import TEMP_PATH
+from random import SystemRandom
 
 logger = logging.getLogger(__name__)
 
@@ -37,13 +39,8 @@ class TestExecuteK8Sanity:
         logger.info("STARTED: Setup Operation")
         logger.info("ENDED: Setup Operation")
         cls.motr_obj = MotrCoreK8s()
-        yield
-        # Perform the clean up for each test.
-        logger.info("STARTED: Teardown Operation")
-        del cls.motr_obj
-        logger.info("ENDED: Teardown Operation")
+        cls.system_random = SystemRandom()
 
-    @classmethod
     def test_motr_k8s_lib(cls):
         """
         Sample test
@@ -55,3 +52,33 @@ class TestExecuteK8Sanity:
         logger.info(cls.motr_obj.cortx_node_list)
         logger.info(cls.motr_obj.get_primary_cortx_node())
         logger.info(cls.motr_obj.get_cortx_node_endpoints())
+
+    def test_m0cp_m0cat_workload(self):
+        """
+        Verify different size object m0cp m0cat operation
+        """
+        logger.info("STARTED: Verify multiple m0cp/m0cat operation")
+        infile = TEMP_PATH + 'input'
+        outfile = TEMP_PATH + 'output'
+        node_pod_dict = self.motr_obj.get_node_pod_dict()
+        m0client_num = self.motr_obj.get_number_of_m0clients()
+        for client_num in range(m0client_num):
+            for node in node_pod_dict:
+                count_list = ['1', '2', '4', '4', '4', '2', '4', '4', '250',
+                              '2', '4', '2', '3', '4', '8', '4', '1024']
+                bsize_list = ['4K', '4K', '4K', '8K', '16K', '64K', '64K', '128K',
+                              '4K', '1M', '1M', '4M', '4M', '4M', '4M', '16M', '1M']
+                layout_ids = ['1', '1', '1', '2', '3', '5', '5', '6', '1',
+                              '9', '9', '11', '11', '11', '11', '13', '9']
+                for b_size, count, layout in zip(bsize_list, count_list, layout_ids):
+                    object_id = str(self.system_random.randint(1, 100)) + ":" + \
+                                str(self.system_random.randint(1, 100))
+                    self.motr_obj.dd_cmd(b_size, count, infile, node)
+                    self.motr_obj.cp_cmd(b_size, count, object_id, layout, infile, node, client_num)
+                    self.motr_obj.cat_cmd(b_size, count, object_id, layout, outfile, node,
+                                          client_num)
+                    self.motr_obj.diff_cmd(infile, outfile, node)
+                    self.motr_obj.md5sum_cmd(infile, outfile, node)
+                    self.motr_obj.unlink_cmd(object_id, layout, node, client_num)
+
+            logger.info("Stop: Verify multiple m0cp/cat operation")
