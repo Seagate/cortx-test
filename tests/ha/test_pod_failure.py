e4a94fdc95c8d9dbd8b6bfe8b522f8ca16c33a9d
Rahul Hatwar <93516151+rahul-hatwar@users.noreply.github.com>
2021-12-14 14:55:25 +0530

    EOS-26534: Verify IO using safe pod shutdown (#988)
    * EOS-26534: Verify IO using safe pod shutdown
    
    * Added the review comments changes
    
    Co-authored-by: Rahul Hatwar <rahulchandrakant.hatwar@gmail.com>

--- tests/ha/test_pod_failure.py
+++ tests/ha/test_pod_failure.py
@@ -962,8 +962,8 @@ class TestPodFailure:
             download_checksum = self.ha_obj.cal_compare_checksum(file_list=[download_path],
                                                                  compare=False)[0]
             assert_utils.assert_equal(s3data[bucket_name][1], download_checksum,
-                                    f"Failed to match checksum: {s3data[bucket_name][1]},"
-                                    f" {download_checksum}")
+                                      f"Failed to match checksum: {s3data[bucket_name][1]},"
+                                      f" {download_checksum}")
         LOGGER.info("Step 8: Verified status for In-flight DELETEs while pod is going down"
                     "and downloaded and verified checksum for remaining/FailedToDelete buckets.")
 
@@ -1074,3 +1074,75 @@ class TestPodFailure:
         LOGGER.info("Step 7: Successfully created multiple buckets and ran IOs")
         LOGGER.info("ENDED: Test to verify Continuous WRITEs during data pod down by delete "
                     "deployment.")
+
+    @pytest.mark.ha
+    @pytest.mark.lc
+    @pytest.mark.tags("TEST-32454")
+    @CTFailOn(error_handler)
+    def test_io_operation_pod_shutdown_scale_replicas(self):
+        """
+        Verify IOs before and after data pod failure; pod shutdown by making replicas=0
+        """
+        LOGGER.info(
+            "STARTED: Verify IOs before and after data pod failure; pod shutdown "
+            "by making replicas=0")
+
+        LOGGER.info(
+            "Step 1: Start IOs (create s3 acc, buckets and upload objects).")
+        resp = self.ha_obj.perform_ios_ops(prefix_data='TEST-32454')
+        assert_utils.assert_true(resp[0], resp[1])
+        di_check_data = (resp[1], resp[2])
+        self.s3_clean = resp[2]
+        resp = self.ha_obj.perform_ios_ops(
+            di_data=di_check_data, is_di=True)
+        assert_utils.assert_true(resp[0], resp[1])
+        self.s3_clean = None
+        LOGGER.info("Step 1: IOs are completed successfully.")
+
+        LOGGER.info("Step 2: Shutdown the data pod safely by making replicas=0")
+        LOGGER.info("Get pod name to be shutdown")
+        pod_list = self.node_master_list[0].get_all_pods(pod_prefix=const.POD_NAME_PREFIX)
+        pod_name = random.sample(pod_list, 1)
+        LOGGER.info("Shutdown pod %s", pod_name)
+        resp = self.node_master_list[0].create_pod_replicas(num_replica=0, pod_name=pod_name)
+        LOGGER.debug("Response: %s", resp)
+        assert_utils.assert_false(resp[0], f"Failed to shutdown pod {pod_name} by making "
+                                  "replicas=0")
+        LOGGER.info("Step 2: Successfully shutdown pod %s by making replicas=0", pod_name)
+        self.deployment_name = resp[1]
+        self.restore_pod = True
+        self.restore_method = const.RESTORE_SCALE_REPLICAS
+
+        LOGGER.info("Step 3: Check cluster status")
+        resp = self.ha_obj.check_cluster_status(self.node_master_list[0])
+        assert_utils.assert_false(resp[0], resp)
+        LOGGER.info("Step 3: Cluster is in degraded state")
+
+        LOGGER.info("Step 4: Check services status that were running on pod %s", pod_name)
+        resp = self.hlth_master_list[0].get_pod_svc_status(pod_list=[pod_name], fail=True)
+        LOGGER.debug("Response: %s", resp)
+        assert_utils.assert_true(resp[0], resp)
+        LOGGER.info("Step 4: Services of pod are in offline state")
+
+        LOGGER.info("Step 5: Check services status on remaining pods %s", pod_list.remove(pod_name))
+        resp = self.hlth_master_list[0].get_pod_svc_status(pod_list=pod_list.remove(pod_name),
+                                                           fail=False)
+        LOGGER.debug("Response: %s", resp)
+        assert_utils.assert_true(resp[0], resp)
+        LOGGER.info("Step 5: Services of remaining pods are in online state")
+
+        LOGGER.info(
+            "Step 6: Start IOs on degraded cluster.")
+        resp = self.ha_obj.perform_ios_ops(prefix_data='TEST-32454-1')
+        assert_utils.assert_true(resp[0], resp[1])
+        di_check_data = (resp[1], resp[2])
+        self.s3_clean = resp[2]
+        resp = self.ha_obj.perform_ios_ops(
+            di_data=di_check_data, is_di=True)
+        assert_utils.assert_true(resp[0], resp[1])
+        self.s3_clean = None
+        LOGGER.info("Step 6: IOs are completed successfully.")
+
+        LOGGER.info(
+            "Completed: Verify IOs before and after data pod failure; pod shutdown "
+            "by making replicas 0")
