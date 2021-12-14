eddb3f106332f3d97f2024adaf42e1230ecca959
Naveen-Kumar-seagate <siriga.naveenkumargowd@seagate.com>
2021-12-14 02:14:23 -0700

    Automation of test cases for size fileter and service filter to SB
    Signed-off-by: Naveen-Kumar-seagate <siriga.naveenkumargowd@seagate.com>

--- commons/utils/support_bundle_utils.py
+++ commons/utils/support_bundle_utils.py
@@ -84,7 +84,7 @@ def create_support_bundle_individual_cmd(node, username, password, remote_dir, l
 
 
 # pylint: disable=too-many-arguments
-def create_support_bundle_single_cmd(local_dir, bundle_name, comp_list=None):
+def create_support_bundle_single_cmd(local_dir, bundle_name, comp_list=None, size=None, services=None):
     """
     Collect support bundles from various components using single support bundle cmd
     :param local_dir: Local directory where support bundles will be copied
@@ -117,6 +117,14 @@ def create_support_bundle_single_cmd(local_dir, bundle_name, comp_list=None):
     if comp_list is not None:
         command = command + ''.join(" -c ")
         command = command + ''.join(comp_list)
+    #Form the commnad if component list is provided in parameter
+    if size is not None:
+        command = command + ''.join(" --size_limit ")
+        command = command + ''.join(size)
+    #Form the command if component list is provided in parameter
+    if services is not None:
+        command = command + ''.join(" --modules ")
+        command = command + ''.join(services)
     resp = node_list[0].execute_cmd(cmd=command)
     LOGGER.debug("Response for support bundle generate: {}".format(resp))
     assert_utils.assert_true(resp[0], resp[1])
