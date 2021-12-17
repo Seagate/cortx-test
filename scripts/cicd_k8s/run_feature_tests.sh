ffb006a588cb401ac37c73b432e75a1db170210b
Akshay Mankar <akshay.s.mankar@seagate.com>
2021-12-17 13:20:32 +0530

    added validate certs false as we r using ip in s3 endpoint
    Signed-off-by: Akshay Mankar <akshay.s.mankar@seagate.com>

--- scripts/cicd_k8s/run_feature_tests.sh
+++ scripts/cicd_k8s/run_feature_tests.sh
@@ -2,5 +2,5 @@
 TGT_NAME=$1
 echo "Target node entry name: $TGT_NAME"
 
-pytest scripts/jenkins_job/aws_configure.py::test_create_acc_aws_conf --local True --target ${TGT_NAME}
+pytest scripts/jenkins_job/aws_configure.py::test_create_acc_aws_conf --local True --target ${TGT_NAME} --validate_certs False
 pytest -m "sanity" --local True --target ${TGT_NAME} --junitxml "log/latest/results.xml" --html "log/latest/results.html"
