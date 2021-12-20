c3cb3d27b3bbf3c414d41d31d53880635b89b8a8
Aayushi Sharma <aayushi.sharma@seagate.com>
2021-12-19 22:06:32 -0700

    Shutdown and Restart Command
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>

--- comptests/prov/test_prov_k8s_cortx.py
+++ comptests/prov/test_prov_k8s_cortx.py
@@ -243,3 +243,37 @@ class TestProvK8Cortx:
             assert_utils.assert_exact_string(cluster_id_yaml, cluster_id_conf,
                                              "Cluster ID does not match in both files..")
         LOGGER.info("Test Completed.")
+
+    @pytest.mark.lc
+    @pytest.mark.comp_prov
+    @pytest.mark.tags("TEST-32940")
+    def test_32940(self):
+        """
+        Verify cortx cluster shutdown command.
+        """
+        LOGGER.info("Test Started.")
+        LOGGER.info("Executing cortx cluster shutdown command.")
+        LOGGER.info("Step 1: Check whether cluster shutdown command run.")
+        resp = self.master_node_obj.execute_cmd(cmd=commands.CLSTR_STOP_CMD.format('deploy-scripts/k8_cortx_cloud'),read_lines=True)
+        assert_utils.assert_true(resp)
+        LOGGER.info("Step 2: Checking whether all CORTX Data pods have been shutdown.")
+        resp = self.master_node_obj.execute_cmd(cmd=commands.CMD_POD_STATUS)
+        assert_utils.assert_true(resp)
+        LOGGER.info("Test Completed.")
+    
+    @pytest.mark.lc
+    @pytest.mark.comp_prov
+    @pytest.mark.tags("TEST-32939")
+    def test_32939(self):
+        """
+        Verify cortx cluster restart command.
+        """
+        LOGGER.info("Test Started.")
+        LOGGER.info("Executing cortx cluster restart command.")
+        LOGGER.info("Step 1: Check whether cluster restart command run.")
+        resp = self.master_node_obj.execute_cmd(cmd=commands.CLSTR_START_CMD.format('deploy-scripts/k8_cortx_cloud'),read_lines=True)
+        assert_utils.assert_true(resp)
+        LOGGER.info("Step 2: Checking whether all CORTX Data pods have been restarted.")
+        resp = self.master_node_obj.execute_cmd(cmd=commands.CMD_POD_STATUS)
+        assert_utils.assert_true(resp)
+        LOGGER.info("Test Completed.")
