7d2e82462a92ee5cf095d4c146f04d7401d05409
Rahul Hatwar <rahulchandrakant.hatwar@seagate.com>
2021-12-22 15:14:55 +0530

    EOS-26537 - Test-32452 and Test-32461

--- commons/constants.py
+++ commons/constants.py
@@ -46,6 +46,7 @@ POD_NAME_PREFIX = "cortx-data-pod"
 HAX_CONTAINER_NAME = "cortx-motr-hax"
 NAMESPACE = "default"
 CONTROL_POD_NAME_PREFIX = "cortx-control-pod"
+SERVER_POD_NAME_PREFIX = "cortx-server-pod"
 
 # RAS Paths
 BYTES_TO_READ = 8000
