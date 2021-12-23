7ef99ff736a9a0b5843869c53e1cd2ef818fae97
rajesh.chouhan <rajesh.chouhan@seagate.com>
2021-12-24 00:34:37 +0530

    5_RBF on custom build
    Signed-off-by: rajesh.chouhan <rajesh.chouhan@seagate.com>

--- libs/s3/s3_misc.py
+++ libs/s3/s3_misc.py
@@ -36,18 +36,20 @@ def create_iam_user(user_name, access_key: str, secret_key: str, **kwargs):
     """
     LOGGER.debug("Access Key : %s", access_key)
     LOGGER.debug("Secret Key : %s", secret_key)
-    endpoint = kwargs.get("endpoint_url", S3_CFG["iam_url"])
-    LOGGER.debug("IAM endpoint : %s", endpoint)
-    iam_test_obj = iam_test_lib.IamTestLib(access_key, secret_key, endpoint)
+    # endpoint = kwargs.get("endpoint_url", S3_CFG["iam_url"])
+    # LOGGER.debug("IAM endpoint : %s", endpoint)
+    # s3_cert_path = S3_CFG['s3_cert_path'] if S3_CFG["validate_certs"] else False
+    # LOGGER.debug("s3_cert_path : %s", s3_cert_path)
+    # iam_test_obj = iam_test_lib.IamTestLib(access_key, secret_key, endpoint, verify=s3_cert_path)
+    iam_test_obj = iam_test_lib.IamTestLib(access_key, secret_key)
     iam_test_obj.create_user(user_name)
     LOGGER.debug("Create IAM user command success")
     result = False
     resp = iam_test_obj.list_users()
     user_list = [user["UserName"] for user in resp[1] if "iam_user" in user["UserName"]]
-    # for iam_user in iam_test_obj.list_users()["Users"]:
-    for iam_user in user_list:
-        if user_name == iam_user['UserName']:
-            LOGGER.debug("IAM user %s found", iam_user)
+    LOGGER.info("user list: %s", user_list)
+    if user_name in user_list:
+            LOGGER.debug("IAM user %s found", user_name)
             result = True
     del iam_test_obj
     LOGGER.debug("Verified created IAM user exits")
@@ -60,19 +62,22 @@ def delete_iam_user(user_name, access_key: str, secret_key: str, **kwargs):
     """
     LOGGER.debug("Access Key : %s", access_key)
     LOGGER.debug("Secret Key : %s", secret_key)
-    endpoint = kwargs.get("endpoint_url", S3_CFG["iam_url"])
-    LOGGER.debug("IAM endpoint : %s", endpoint)
-    region = kwargs.get("region_name", S3_CFG["region"])
-    LOGGER.debug("Region : %s", region)
-    iam_test_obj = iam_test_lib.IamTestLib(access_key, secret_key, endpoint)
+    # endpoint = kwargs.get("endpoint_url", S3_CFG["iam_url"])
+    # LOGGER.debug("IAM endpoint : %s", endpoint)
+    # region = kwargs.get("region_name", S3_CFG["region"])
+    # LOGGER.debug("Region : %s", region)
+    # s3_cert_path = S3_CFG['s3_cert_path'] if S3_CFG["validate_certs"] else False
+    # LOGGER.debug("s3_cert_path : %s", s3_cert_path)
+    # iam_test_obj = iam_test_lib.IamTestLib(access_key, secret_key, endpoint, verify=s3_cert_path)
+    iam_test_obj = iam_test_lib.IamTestLib(access_key, secret_key)
     iam_test_obj.delete_user(user_name)
     LOGGER.debug("Delete IAM user command success")
     result = False
     resp = iam_test_obj.list_users()
     user_list = [user["UserName"] for user in resp[1] if "iam_user" in user["UserName"]]
-    # for iam_user in iam_test_obj.list_users()["Users"]:
-    for iam_user in user_list:
-        if user_name == iam_user['UserName']:
+    LOGGER.info("user list: %s", user_list)
+    if user_name in user_list:
+            LOGGER.debug("IAM user %s found", user_name)
             result = True
     del iam_test_obj
     LOGGER.debug("Verified deleted IAM user does not exits")
@@ -85,18 +90,23 @@ def create_bucket(bucket_name, access_key: str, secret_key: str, **kwargs):
     """
     LOGGER.debug("Access Key : %s", access_key)
     LOGGER.debug("Secret Key : %s", secret_key)
-    endpoint = kwargs.get("endpoint_url", S3_CFG["s3_url"])
-    LOGGER.debug("S3 Endpoint : %s", endpoint)
-    s3_obj = s3_test_lib.S3TestLib(access_key, secret_key, endpoint)
-    s3_obj.create_bucket(Bucket=bucket_name)
+    # endpoint = kwargs.get("endpoint_url", S3_CFG["s3_url"])
+    # LOGGER.debug("S3 Endpoint : %s", endpoint)
+    # region = kwargs.get("region_name", S3_CFG["region"])
+    # LOGGER.debug("Region : %s", region)
+    # s3_cert_path = S3_CFG['s3_cert_path'] if S3_CFG["validate_certs"] else False
+    # LOGGER.debug("s3_cert_path : %s", s3_cert_path)
+    # s3_obj = s3_test_lib.S3TestLib(
+    #     access_key, secret_key, endpoint,region=region, verify=s3_cert_path )
+    s3_obj = s3_test_lib.S3TestLib(access_key, secret_key)
+    s3_obj.create_bucket(bucket_name)
     LOGGER.debug("S3 bucket created")
     _ , bktlist = s3_obj.bucket_list()
     result = False
-    for bucket in bktlist:
-        if bucket.name == bucket_name:
-            LOGGER.debug("S3 bucket %s is listed", bucket)
+    LOGGER.info("Bucket list: %s", bktlist)
+    if bucket_name in bktlist:
+            LOGGER.debug("S3 bucket %s is listed", bucket_name)
             result = True
-            break
     del s3_obj
     LOGGER.debug("Verified created bucket exists")
     return result
@@ -108,18 +118,23 @@ def delete_objects_bucket(bucket_name, access_key: str, secret_key: str, **kwarg
     """
     LOGGER.debug("Access Key : %s", access_key)
     LOGGER.debug("Secret Key : %s", secret_key)
-    endpoint = kwargs.get("endpoint_url", S3_CFG["s3_url"])
-    LOGGER.debug("S3 Endpoint : %s", endpoint)
-    s3_obj = s3_test_lib.S3TestLib(access_key, secret_key, endpoint)
+    # endpoint = kwargs.get("endpoint_url", S3_CFG["s3_url"])
+    # LOGGER.debug("S3 Endpoint : %s", endpoint)
+    # region = kwargs.get("region_name", S3_CFG["region"])
+    # LOGGER.debug("Region : %s", region)
+    # s3_cert_path = S3_CFG['s3_cert_path'] if S3_CFG["validate_certs"] else False
+    # LOGGER.debug("s3_cert_path : %s", s3_cert_path)
+    # s3_obj = s3_test_lib.S3TestLib(
+    #     access_key, secret_key, endpoint,region=region, verify=s3_cert_path )
+    s3_obj = s3_test_lib.S3TestLib(access_key, secret_key)
     LOGGER.debug("Delete all associated objects & bucket.")
     s3_obj.delete_bucket(bucket_name, force=True)
     _ , bktlist = s3_obj.bucket_list()
     result = False
-    for bucket in bktlist:
-        if bucket.name == bucket_name:
-            LOGGER.debug("S3 bucket %s is listed", bucket)
+    LOGGER.info("Bucket list: %s", bktlist)
+    if bucket_name in bktlist:
+            LOGGER.debug("S3 bucket %s is listed", bucket_name)
             result = True
-            break
     del s3_obj
     LOGGER.debug("Verified bucket is deleted.")
     return not result
@@ -132,9 +147,15 @@ def create_put_objects(object_name: str, bucket_name: str,
     """
     LOGGER.debug("Access Key : %s", access_key)
     LOGGER.debug("Secret Key : %s", secret_key)
-    endpoint = kwargs.get("endpoint_url", S3_CFG["s3_url"])
-    LOGGER.debug("S3 Endpoint : %s", endpoint)
-    s3_obj = s3_test_lib.S3TestLib(access_key, secret_key, endpoint)
+    # endpoint = kwargs.get("endpoint_url", S3_CFG["s3_url"])
+    # LOGGER.debug("S3 Endpoint : %s", endpoint)
+    # region = kwargs.get("region_name", S3_CFG["region"])
+    # LOGGER.debug("Region : %s", region)
+    # s3_cert_path = S3_CFG['s3_cert_path'] if S3_CFG["validate_certs"] else False
+    # LOGGER.debug("s3_cert_path : %s", s3_cert_path)
+    # s3_obj = s3_test_lib.S3TestLib(
+    #     access_key, secret_key, endpoint,region=region, verify=s3_cert_path )
+    s3_obj = s3_test_lib.S3TestLib(access_key, secret_key)
     LOGGER.debug("Created an object : %s", object_name)
     file_path = os.path.join(TEST_DATA_FOLDER, object_name)
     resp = system_utils.create_file(file_path, object_size)
