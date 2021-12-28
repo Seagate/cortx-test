0dd4edf3e6503e38833b51c6c793cdd0aa457dcb
939001 <amit.singh@seagate.com>
2021-12-28 13:01:29 +0530

    initial 7 test cases bucket versioning commit.

--- tests/s3/versioning/test_versioning_put_bucket.py
+++ tests/s3/versioning/test_versioning_put_bucket.py
@@ -193,6 +193,13 @@ class TestVersioningPutBucket:
             #assert_utils.assert_equal(res_code[1], 400)
         except (ClientError, Exception) as error:
             self.log.info("Http error raised:::::: %s", error)
+            httpCode = error.args[3][['httpCode']]
+            self.log.info("Http code2::::%s", error.args[3][['httpCode']])
+            self.log.info("Http error raisedMessage:::::: %s", error["Error Message"])
+            self.log.info("Http code raised:::::: %s", error["Other info"]['httpCode'])
+            self.log.info("Http code1::::%s", error.args[0])
+            self.log.info("Http code2::::%s", error.args[3][['httpCode']])
+            self.log.info("Http error raised:::::: %s", error["Error Message"])
             self.log.info("response returned : %s", error.response)
             httpCode = error.response['ResponseMetadata']['HTTPStatusCode']
             self.log.info("HTTP status code returned : %s", httpCode)
