c348d3f26123cefd84d1b2c0edc179d97a7f72f6
Vidya Kabber <vidya.kabber@seagate.com>
2021-12-28 20:47:11 +0530

    Updated review comments

--- tests/s3/versioning/test_get_bucket_versioning.py
+++ tests/s3/versioning/test_get_bucket_versioning.py
@@ -18,7 +18,7 @@
 # For any questions about this software or licensing,
 # please email opensource@seagate.com or cortx-questions@seagate.com.
 
-"""PUT Object test module for Object Versioning."""
+"""GET Object test module for Object Versioning."""
 
 import logging
 import os
@@ -29,7 +29,7 @@ from commons.ct_fail_on import CTFailOn
 from commons.errorcodes import error_handler
 from commons.exceptions import CTException
 from commons.params import TEST_DATA_FOLDER
-from commons.utils.system_utils import create_file, path_exists, remove_file
+from commons.utils.system_utils import path_exists, remove_file
 from commons.utils.system_utils import make_dirs, remove_dirs
 from commons.utils import assert_utils
 from config.s3 import S3_CFG
@@ -38,9 +38,11 @@ from libs.s3.s3_versioning_test_lib import S3VersioningTestLib
 from libs.s3.cortxcli_test_lib import CSMAccountOperations
 
 
-class TestVersioningGetObject:
-    """Test PUT Object API with Object Versioning"""
+class TestGetBucketVersioning:
+    """Test Get Object API with Object Versioning"""
 
+    # pylint:disable=attribute-defined-outside-init
+    # pylint:disable-msg=too-many-instance-attributes
     def setup_method(self):
         """
         Function will be invoked prior to each test case.
@@ -56,16 +58,11 @@ class TestVersioningGetObject:
         self.account_name = "s3getbucketversioning_user"
         self.email_id = f"{self.account_name}_email@seagate.com"
 
-        self.test_dir_path = os.path.join(TEST_DATA_FOLDER, "TestVersioningPutObject")
+        self.test_dir_path = os.path.join(TEST_DATA_FOLDER, "TestGetBucketVersioning")
         if not path_exists(self.test_dir_path):
             make_dirs(self.test_dir_path)
             self.log.info("Created path: %s", self.test_dir_path)
-        file_name = "{0}{1}".format("ver_put_obj", time.perf_counter_ns())
-        self.file_path = os.path.join(self.test_dir_path, file_name)
-        create_file(fpath=self.file_path, count=1)
-        self.log.info("Created file: %s", self.file_path)
         self.bucket_name = "ver-bkt-{}".format(time.perf_counter_ns())
-        self.object_name = "ver-obj-{}".format(time.perf_counter_ns())
         res = self.s3_test_obj.create_bucket(self.bucket_name)
         assert_utils.assert_true(res[0], res[1])
         assert_utils.assert_equal(res[1], self.bucket_name, res[1])
@@ -78,10 +75,6 @@ class TestVersioningGetObject:
         It will clean up resources which are getting created during test case execution.
         """
         self.log.info("STARTED: Teardown operations")
-        self.log.info("Clean : %s", self.test_dir_path)
-        if path_exists(self.file_path):
-            res = remove_file(self.file_path)
-            self.log.info("cleaned path: %s, res: %s", self.file_path, res)
         if path_exists(self.test_dir_path):
             remove_dirs(self.test_dir_path)
         self.log.info("Cleanup test directory: %s", self.test_dir_path)
@@ -104,21 +97,15 @@ class TestVersioningGetObject:
     @pytest.mark.tags('TEST-32715')
     @CTFailOn(error_handler)
     def test_get_object_unversioned_32715(self):
-        """
-        Verify that HTTP status code 200 returned to bucket owner when versioning not Enabled.
+        """Verify bucket owner received 200 ok when versioning is not enabled
 
         Create bucket.
-        Upload object
         Perform GET Bucket Versioning on created bucket
         """
-        self.log.info(
-            "STARTED : Verify that HTTP status code 200 returned to bucket owner when versioning not Enabled.")
-        self.log.info("Step 1: Upload object")
-        res = self.s3_test_obj.put_object(
-            bucket_name=self.bucket_name, object_name=self.object_name, file_path=self.file_path)
-        assert_utils.assert_true(res[0], res[1])
-        self.log.info("Step 2: Perform GET Bucket Versioning on created bucket")
-        res = self.s3_ver_test_obj.get_bucket_versioning(bucket_name=self.bucket_name)
+        self.log.info("STARTED: Verify bucket owner received 200 ok when versioning is not enabled")
+        self.log.info("Step 1: GET Bucket Versioning on created bucket")
+        res = self.s3_ver_test_obj.get_bucket_versioning(
+            bucket_name=self.bucket_name)
         assert_utils.assert_equal(200, res[1]['ResponseMetadata']['HTTPStatusCode'])
         assert_utils.assert_not_in('Status', res[1])
 
@@ -126,8 +113,7 @@ class TestVersioningGetObject:
     @pytest.mark.tags('TEST-32716')
     @CTFailOn(error_handler)
     def test_get_object_enabled_suspended_32716(self):
-        """
-        Verify that bucket versioning status Enabled/Suspended is not returned in response to user who is not bucket owner.
+        """Verify that bucket versioning status is not returned to non-owner user.
 
         Create bucket.
         PUT Bucket Versioning with status=Enabled
@@ -135,30 +121,34 @@ class TestVersioningGetObject:
         PUT Bucket Versioning with status=Suspended
         Perform GET Bucket Versioning API by non bucket owner/user.
         """
-        self.log.info(
-            "STARTED : Verify that bucket versioning status Enabled/Suspended is not returned to bucket owner/user.")
-        self.log.info("Prerequisite: Crete a new S3 user account to perform non bucket owner/user actions")
+        self.log.info("STARTED : Verify bucket versioning status is not returned to non-owner user")
+        self.log.info("Prerequisite: New S3 account creation for non-owner user actions")
         access_key, secret_key = self.create_s3account()
-        self.s3_new_test_obj = S3VersioningTestLib(access_key=access_key, secret_key=secret_key,
-                                                   endpoint_url=S3_CFG["s3_url"])
+        s3_new_test_obj = S3VersioningTestLib(access_key=access_key,
+                                              secret_key=secret_key,
+                                              endpoint_url=S3_CFG["s3_url"])
         err_message = "AccessDenied"
         self.log.info("Step 1: PUT Bucket Versioning with status=Enabled")
-        res = self.s3_ver_test_obj.put_bucket_versioning(bucket_name=self.bucket_name)
+        res = self.s3_ver_test_obj.put_bucket_versioning(
+            bucket_name=self.bucket_name)
         assert_utils.assert_true(res[0], res[1])
-        self.log.info("Step 2: Perform GET Bucket Versioning API by non bucket owner/user")
+        self.log.info("Step 2: GET Bucket Versioning by non bucket owner/user")
         try:
-            res = self.s3_new_test_obj.get_bucket_versioning(bucket_name=self.bucket_name)
+            res = s3_new_test_obj.get_bucket_versioning(
+                bucket_name=self.bucket_name)
             self.log.info(res)
             assert_utils.assert_not_in('Status', res[1])
         except CTException as error:
             self.log.debug(res)
             assert err_message in error.message, error.message
         self.log.info("Step 3: PUT Bucket Versioning with status=Suspended")
-        res = self.s3_ver_test_obj.put_bucket_versioning(bucket_name=self.bucket_name, status="Suspended")
+        res = self.s3_ver_test_obj.put_bucket_versioning(
+            bucket_name=self.bucket_name, status="Suspended")
         assert_utils.assert_true(res[0], res[1])
-        self.log.info("Step 4: Perform GET Bucket Versioning API by non bucket owner/user")
+        self.log.info("Step 4: GET Bucket Versioning by non bucket owner/user")
         try:
-            res = self.s3_new_test_obj.get_bucket_versioning(bucket_name=self.bucket_name)
+            res = s3_new_test_obj.get_bucket_versioning(
+                bucket_name=self.bucket_name)
             self.log.info(res)
             assert_utils.assert_not_in('Status', res[1])
         except CTException as error:
