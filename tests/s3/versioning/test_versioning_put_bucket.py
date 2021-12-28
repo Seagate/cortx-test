c1ddf66463fee0c0dfeeed223d3c723b6affe6a9
939001 <amit.singh@seagate.com>
2021-12-28 13:23:20 +0530

    initial 7 test cases bucket versioning commit.

--- tests/s3/versioning/test_versioning_put_bucket.py
+++ tests/s3/versioning/test_versioning_put_bucket.py
@@ -195,12 +195,12 @@ class TestVersioningPutBucket:
             self.log.info("Http error raised:::::: %s", error)
             #httpCode = error.args[3][['httpCode']]
             #self.log.info("Http code2::::%s", error.args[3][['httpCode']])
-            self.log.info("Http error raisedMessage:::::: %s", error["Error Message"])
-            self.log.info("Http code raised:::::: %s", error["Other info"]['httpCode'])
-            self.log.info("Http code1::::%s", error.args[0])
+            self.log.info("Http error raisedMessage02:::::: %s", error.error_code)
+            #self.log.info("Http code raised:::::: %s", error["Other info"]['httpCode'])
+            #self.log.info("Http code1::::%s", error.args[0])
             #self.log.info("Http code2::::%s", error.args[3][['httpCode']])
-            self.log.info("Http error raised:::::: %s", error["Error Message"])
-            self.log.info("response returned : %s", error.response)
+            #self.log.info("Http error raised:::::: %s", error["Error Message"])
+            #self.log.info("response returned : %s", error.response)
             httpCode = error.response['ResponseMetadata']['HTTPStatusCode']
             self.log.info("HTTP status code returned : %s", httpCode)
             if httpCode == 400:
