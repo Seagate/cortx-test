92e6186425b48aa1ceaf3a9cb3203bb076d3c4b5
root <root@ssc-vm-g2-rhev4-2619.colo.seagate.com>
2022-01-05 07:49:55 -0700

    Test-30698 online pod status
    Signed-off-by: root <root@ssc-vm-g2-rhev4-2619.colo.seagate.com>

---
+++ comptests/ha/test_pod_online.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+
+"""
+HA component test suite for stop cluster.
+"""
+import logging
+import time
+import pytest
+from commons.helpers.pods_helper import LogicalNode
+from libs.ha.ha_common_libs_k8s import HAK8s
+from libs.di.di_mgmt_ops import ManagementOPs
+from commons.utils import assert_utils
+from config import CMN_CFG
+
+# Global Constants
+LOGGER = logging.getLogger(__name__)
+
+
+class TestPodOnlineStatus:
+    """
+    Test suite for pod online status.
+    """
+
+    @classmethod
+    def setup_class(cls):
+        """
+        Setup operations for the test file
+        """
+        LOGGER.info("STARTED: Setup Module operations.")
+        cls.num_nodes = len(CMN_CFG["nodes"])
+        cls.username = []
+        cls.password = []
+        cls.worker_node_list = []
+        cls.master_node_list = []
+        cls.ha_obj = HAK8s()
+        cls.restored =  True
+        cls.mgnt_ops = ManagementOPs()
+        for node in range(cls.num_nodes):
+            node_obj = LogicalNode(hostname=CMN_CFG["nodes"][node]["hostname"],
+	                           username=CMN_CFG["nodes"][node]["username"],
+				   password=CMN_CFG["nodes"][node]["password"])
+            cls.host = CMN_CFG["nodes"][node]["hostname"]
+            cls.username.append(CMN_CFG["nodes"][node]["username"])
+            cls.password.append(CMN_CFG["nodes"][node]["password"])
+            if CMN_CFG["nodes"][node]["node_type"].lower() == "master":
+                cls.master_node_obj = node_obj
+                cls.master_node_list.append(node_obj)
+            else:
+                cls.worker_node_list.append(node_obj)
+
+    def setup_method(self):
+        """
+        This function will be invoked prior to each test case.
+        """
+        LOGGER.info("STARTED: Setup Operations")
+        self.random_time = int(time.time())
+        self.restored = True
+        LOGGER.info("Check the overall status of the cluster.")
+        resp = self.ha_obj.check_cluster_status(self.master_node_list[0])
+        #assert_utils.assert_true(resp[0], resp[1])
+        LOGGER.info("Cluster status is online.")
+        LOGGER.info("Done: Setup operations. ")
+
+    def teardown_method(self):
+        """
+        This function will be invoked after each test function in the module.
+        """
+        LOGGER.info("STARTED: Teardown Operations.")
+        if self.restored:
+            LOGGER.info("Cleanup: Check cluster status and start it if not up.")
+            resp = self.ha_obj.check_cluster_status(self.master_node_list[0])
+            if not resp[0]:
+                resp = self.ha_obj.restart_cluster(self.master_node_list[0])
+                assert_utils.assert_true(resp[0], resp[1])
+        LOGGER.info("Done: Teardown completed.")
+
+    @pytest.mark.ha
+    @pytest.mark.lc
+    @pytest.mark.tags("TEST-30698")
+    def test_all_nodes_online(self):
+        """
+        Stop Cluster - All the nodes are online.
+        """
+        LOGGER.info("STARTED: Test to verify cluster shutdown.")
+        LOGGER.info("Step 1: Check the status of the pods running in cluster.")
+        resp = self.ha_obj.check_pod_status(self.master_node_list[0])
+        assert_utils.assert_true(resp[0], resp[1])
+        LOGGER.info("Step 1: All pods are running.")
+
+        LOGGER.info("Step 1: Shutdown down the node.")
+        resp = self.worker_node_list[0].shutdown_node()
+        assert resp, "Failed to shutdown node "
+
+
+
+
