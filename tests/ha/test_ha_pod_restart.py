b88fe202ba2508baa37df5b186aeb8f872a592e2
ApurwaMohite <55824145+ApurwaMohite@users.noreply.github.com>
2022-01-06 16:41:42 +0530

    review
    Signed-off-by: ApurwaMohite <55824145+ApurwaMohite@users.noreply.github.com>

--- tests/ha/test_ha_pod_restart.py
+++ tests/ha/test_ha_pod_restart.py
@@ -1158,6 +1158,7 @@ class TestPodRestart:
 
         for pod in pod_list:
             LOGGER.info("Step 1: Shutdown the data pod by deleting deployment (unsafe)")
+            pod_host = self.node_master_list[0].get_pod_hostname(pod_name=pod)
             LOGGER.info("Deleting pod %s", pod)
             resp = self.node_master_list[0].delete_deployment(pod_name=pod)
             LOGGER.debug("Response: %s", resp)
@@ -1177,7 +1178,8 @@ class TestPodRestart:
             LOGGER.info("Step 2: Cluster is in degraded state")
 
             LOGGER.info("Step 3: Check services status that were running on pod %s", pod)
-            resp = self.hlth_master_list[0].get_pod_svc_status(pod_list=[pod], fail=True)
+            resp = self.hlth_master_list[0].get_pod_svc_status(pod_list=[pod], fail=True,
+                                                               hostname=pod_host)
             LOGGER.debug("Response: %s", resp)
             assert_utils.assert_true(resp[0], resp)
             LOGGER.info("Step 3: Services of pod are in offline state")
@@ -1245,7 +1247,8 @@ class TestPodRestart:
 
         LOGGER.info("Get data pod list to be deleted")
         pod_list = self.node_master_list[0].get_all_pods(pod_prefix=const.POD_NAME_PREFIX)
-        pod = random.sample(pod_list, 1)
+        pod = random.sample(pod_list, 1)[0]
+        pod_host = self.node_master_list[0].get_pod_hostname(pod_name=pod)
 
         loop_count = HA_CFG["common_params"]["loop_count"]
         for loop in range(1, loop_count):
@@ -1271,7 +1274,8 @@ class TestPodRestart:
             LOGGER.info("Step 2: Cluster is in degraded state")
 
             LOGGER.info("Step 3: Check services status that were running on pod %s", pod)
-            resp = self.hlth_master_list[0].get_pod_svc_status(pod_list=[pod], fail=True)
+            resp = self.hlth_master_list[0].get_pod_svc_status(pod_list=[pod], fail=True,
+                                                               hostname=pod_host)
             LOGGER.debug("Response: %s", resp)
             assert_utils.assert_true(resp[0], resp)
             LOGGER.info("Step 3: Services of pod are in offline state")
@@ -1325,6 +1329,7 @@ class TestPodRestart:
             LOGGER.info("Get recently created data pod name using deployment %s",
                         self.deployment_name)
             pod = self.node_master_list[0].get_recent_pod_name(deployment_name=self.deployment_name)
+            pod_list = self.node_master_list[0].get_all_pods(pod_prefix=const.POD_NAME_PREFIX)
 
         LOGGER.info("ENDED: Test to verify IOs in degraded mode and after data pod restart in "
                     "loop (same pod down every time)")
