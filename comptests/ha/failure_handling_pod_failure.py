82211c5831772ec6bf2d4e1de7db7532b092ec6b
Jyoti-cs <Jyoti.baral@seagate.com>
2022-01-20 10:37:57 +0530

    EOS-26744 - End-to-end testing of HA services for node failure including health monitor module
    Signed-off-by: Jyoti-cs <Jyoti.baral@seagate.com>

---
+++ comptests/ha/failure_handling_pod_failure.py
@@ -0,0 +1,142 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+
+"""
+HA component test suite for stop cluster.
+"""
+import logging
+import random
+import os
+import subprocess
+
+import pytest
+
+from commons.helpers.pods_helper import LogicalNode
+from libs.ha.ha_common_libs_k8s import HAK8s
+from commons.utils import assert_utils
+from config import CMN_CFG
+from commons import constants as const
+from commons import constants as common_const
+from commons import commands as common_cmd
+
+LOGGER = logging.getLogger(__name__)
+
+class TestFailureHandlingPodFailure:
+    @classmethod
+    def setup_class(cls):
+        """Setup class"""
+        LOGGER.info("STARTED: Setup Module operations")
+        cls.num_nodes = len(CMN_CFG["nodes"])
+        cls.node_worker_list = []
+        cls.node_master_list = []
+        cls.host_list = []
+        cls.ha_obj = HAK8s()
+        for node in range(cls.num_nodes):
+            node_obj = LogicalNode(hostname=CMN_CFG["nodes"][node]["hostname"],
+                                   username=CMN_CFG["nodes"][node]["username"],
+                                   password=CMN_CFG["nodes"][node]["password"])
+
+            if CMN_CFG["nodes"][node]["node_type"].lower() == "master":
+                cls.master_node_obj = node_obj
+                cls.node_master_list.append(node_obj)
+            else:
+                cls.node_worker_list.append(node_obj)
+        LOGGER.info("Done: Setup operations finished.")
+
+        def setup_method(self):
+            """
+            This function will be invoked prior to each test case.
+            """
+            LOGGER.info("STARTED: Setup Operations")
+            LOGGER.info("Check the overall status of the cluster.")
+            resp = self.ha_obj.check_cluster_status(self.master_node_list[0])
+            assert_utils.assert_true(resp[0], resp[1])
+            LOGGER.info("Cluster status is online.")
+            pod_list = self.node_master_list[0].get_all_pods(pod_prefix=const.HA_POD_NAME_PREFIX)
+            pod_name = pod_list
+            res = self.node_master_list.send_k8s_cmd(
+                operation="exec", pod=pod_name, namespace=common_const.NAMESPACE,
+                command_suffix=f"-c {common_const.HA_K8S_CONTAINER_NAME} -- {common_cmd.SERVICE_HA_STATUS}",
+                decode=True)
+            assert res, "K8s monitor service is not running"
+            res = self.node_master_list.send_k8s_cmd(
+                operation="exec", pod=pod_name, namespace=common_const.NAMESPACE,
+                command_suffix=f"-c {common_const.HA_FAULT_TOLERANCE_CONTAINER_NAME} -- {common_cmd.SERVICE_HA_STATUS}",
+                decode=True)
+            assert res, "K8s monitor service is not running"
+            res = self.node_master_list.send_k8s_cmd(
+                operation="exec", pod=pod_name, namespace=common_const.NAMESPACE,
+                command_suffix=f"-c {common_const.HA_HEALTH_MONITOR_CONTAINER_NAME} -- {common_cmd.SERVICE_HA_STATUS}",
+                decode=True)
+            assert res, "K8s monitor service is not running"
+            LOGGER.info("Done: Setup operations.")
+
+            @pytest.mark.ha
+            @pytest.mark.lc
+            @pytest.mark.tags("TEST-30218")
+            def delete_pod(self):
+                """
+                This test tests delete pod using kubectl cmd
+                """
+                LOGGER.info("STARTED: Publish the pod failure event in message bus to Hare - Delete pod.")
+
+                datapod_list = self.node_master_list[0].get_all_pods(pod_prefix=const.POD_NAME_PREFIX)
+                pod_name = random.sample(datapod_list, 1)[0]
+                for pod in datapod_list:
+                    LOGGER.info("Step 1: Delete data pod and check Pods status(kubectl delete pods <pod>)")
+                    LOGGER.info("Deleting pod %s", pod)
+                    resp = self.node_master_list[0].delete_pod(pod_name=pod_name, force=False)
+                    assert resp, "Data pod didn't deleted successfully"
+                    LOGGER.info("Step 2: Check the node status.")
+                    resp1 = self.node_master_list[0].status_pods_online(no_pods =6)
+                    assert resp1, "New data pod didn't gets created"
+                    pod_list = self.node_master_list[0].get_all_pods(pod_prefix=const.HA_POD_NAME_PREFIX)
+                    pod_name = pod_list
+                    res = self.node_master_list.send_k8s_cmd(
+                        operation="exec", pod=pod_name, namespace=common_const.NAMESPACE,
+                        command_suffix=f"{common_cmd.HA_LOG_CMD}",
+                        decode=True)
+                    assert res, "Not in HA log dir"
+
+                    output = os.popen("cat /etc/cortx/log/ha/*/ha_setup.log").read()
+                    with open('outfile.txt', 'w+') as outfile:
+                        outfile.write(output)
+                        outfile.close()
+                    str_to_search = "to component hare"
+                    output = open("/outfile.txt", "r")
+                    index = 0
+                    for line in output:
+                        index += 1
+                        if str_to_search in line:
+                            print(f"String found at index {index} : {line}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
