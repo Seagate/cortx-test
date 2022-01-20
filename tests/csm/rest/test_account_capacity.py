5622d66f8d6fd512aaef4ba96d058c6da317c6d7
Kulkarni-Kaustubh <kaustubh.j.kulkarni@seagate.com>
2022-01-20 09:50:36 -0700

    account capacity1
    Signed-off-by: Kulkarni-Kaustubh <kaustubh.j.kulkarni@seagate.com>

--- tests/csm/rest/test_account_capacity.py
+++ tests/csm/rest/test_account_capacity.py
@@ -1061,7 +1061,6 @@ class TestAccountCapacity():
         test_case_name = cortxlogging.get_frame()
         self.log.info("##### Test started -  %s #####", test_case_name)
         mp_config = MPART_CFG["test_33366"]
-        file_size = mp_config["file_size"]
         self.log.info("Step 1: Create s3 Account")
         resp = self.s3user.create_s3_account()
         assert_utils.assert_true(resp.status_code == HTTPStatus.CREATED,
