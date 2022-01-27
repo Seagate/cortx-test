56f0ea1789c63912c887933a6460bce0174e9b62
Divya Kachhwaha <divya.kachhwaha@seagate.com>
2022-01-27 00:54:27 -0700

    commit

--- tests/csm/rest/test_csm_users.py
+++ tests/csm/rest/test_csm_users.py
@@ -5364,7 +5364,7 @@ class TestCsmUser():
         self.log.info("Step 7: Login with first manage user and change password for all s3 account users")
         payload = {"password": new_password, "current_password": current_password}
         for usr in self.created_s3_users:
-            response = self.s3user.edit_s3_account_user_invalid_password(
+            response = self.s3user.edit_s3_account(
                 username=usr,
                 payload=json.dumps(payload),
                 login_as=new_user)
