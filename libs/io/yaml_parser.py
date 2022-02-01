8dbd52f3a649b27d5d136cbb1c4b6648d47f4207
kulrk <rohan.1.kulkarni@seagate.com>
2022-02-01 20:05:43 +0530

    parser code
    Signed-off-by: kulrk <rohan.1.kulkarni@seagate.com>

--- libs/io/yaml_parser.py
+++ libs/io/yaml_parser.py
@@ -20,6 +20,7 @@
 # !/usr/bin/python
 """IO Configs are initialized here."""
 
+import datetime
 import yaml
 import logging
 
@@ -35,9 +36,47 @@ def yaml_parser(yaml_file):
     with open(yaml_file) as obj:
         data = yaml.safe_load(obj)
         yaml_dict.update(data)
-    return yaml_file
+    return yaml_dict
 
 
-def test_parser():
-    s3_io_test = yaml_parser()
-    
+def convert_to_bytes(size):
+    """
+    function to convert any size to bytes
+    """
+    kb = 1000
+    sz = str(size[-2:]).lower()
+    if sz == "kb":
+        return int(size[:-2]) * kb
+    elif sz == "mb":
+        return int(size[:-2]) * kb * kb
+    elif sz == "gb":
+        return int(size[:-2]) * kb * kb * kb
+    elif sz == "tb":
+        return int(size[:-2]) * kb * kb * kb * kb
+
+
+def convert_to_time_delta(time):
+    """
+    function to convert execution time in time delta format
+    """
+    hrs = time[:2]
+    mnt = time[3:5]
+    sec = time[6:8]
+    return datetime.timedelta(hours=hrs, minutes=mnt, seconds=sec)
+
+
+def test_parser(yaml_file):
+    s3_io_test = yaml_parser(yaml_file)
+    delta_list = list()
+    for test, data in s3_io_test.items():
+        start_bytes = convert_to_bytes(data['start_range'])
+        data['start_range'] = start_bytes
+        end_bytes = convert_to_bytes(data['end_range'])
+        data['end_range'] = end_bytes
+        if test == "test_1":
+            data['start_time'] = str(datetime.timedelta(hours=00, minutes=00, seconds=00))
+            delta_list.append(convert_to_time_delta(data['result_duration']))
+        else:
+            data['start_time'] = delta_list.pop()
+            delta_list.append(data['start_time'] + convert_to_time_delta(data['result_duration']))
+    return s3_io_test
