0c1447f6e2e4c8c5d76e27bfc67f53765fd709c9
Aayushi Sharma <aayushi.sharma@seagate.com>
2022-02-01 07:06:03 -0700

    Added Messages
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>

--- comptests/prov/run_lc_tests.sh
+++ comptests/prov/run_lc_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 TGT_NAME=$1
 echo "Target node entry name: $TGT_NAME"
-
+echo -e "\n\n---------------------------------------[ Pytest running aws_configure ]--------------------------------------\n\n"
 pytest scripts/jenkins_job/aws_configure.py::test_create_acc_aws_conf --local True --target ${TGT_NAME}
+echo -e "\n\n---------------------------------------[ Pytest running prov_sanity ]--------------------------------------\n\n"
 pytest -m "prov_sanity" --local True --target ${TGT_NAME} --junitxml "log/latest/results.xml" --html "log/latest/results.html"
