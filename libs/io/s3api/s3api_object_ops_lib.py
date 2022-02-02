5494256b8dd2bb0da6839a65c4b3aa96d940fda1
Vishal Dhobale <vishal.dhobale@seagate.com>
2022-02-02 14:11:20 +0530

    Lib: Object operations using boto3 api.
    Signed-off-by: Vishal Dhobale <vishal.dhobale@seagate.com>

---
+++ libs/io/s3api/s3api_object_ops_lib.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+#
+#
+
+"""Python Library to perform object operations using boto3 module."""
+
+import os
+import logging
+
+from libs.io.s3api.s3api_core_lib import S3ApiRest
+
+LOGGER = logging.getLogger(__name__)
+
+
+class S3Object(S3ApiRest):
+    """Class for object operations."""
+
+    def upload_object(self, bucket_name: str = None, object_name: str = None,
+                      file_path: str = None) -> dict:
+        """
+        Uploading object to the Bucket.
+
+        :param bucket_name: Name of the bucket.
+        :param object_name: Name of the object.
+        :param file_path: Path of the file.
+        :return: response.
+        """
+        response = self.s3_resource.meta.client.upload_file(file_path, bucket_name, object_name)
+        LOGGER.info(response)
+
+        return response
+
+    def list_objects(self, bucket_name: str = None) -> list:
+        """
+        Listing Objects.
+
+        :param bucket_name: Name of the bucket.
+        :return: response.
+        """
+        bucket = self.s3_resource.Bucket(bucket_name)
+        all_objects = [obj.key for obj in bucket.objects.all()]
+        LOGGER.debug(all_objects)
+
+        return all_objects
+
+    def delete_object(self, bucket_name: str = None, obj_name: str = None) -> dict:
+        """
+        Deleting object.
+
+        :param bucket_name: Name of the bucket.
+        :param obj_name: Name of object.
+        :return: response.
+        """
+        response = self.s3_resource.Object(bucket_name, obj_name).delete()
+        logging.debug(response)
+        LOGGER.info("Object '%s' deleted Successfully", obj_name)
+
+        return response
+
+    def head_object(self, bucket_name: str = None, key: str = None) -> dict:
+        """
+        Retrieve metadata from an object without returning the object itself.
+
+        you must have READ access to the object.
+        :param bucket_name: Name of the bucket.
+        :param key: Key of object.
+        :return: response.
+        """
+        response = self.s3_resource.meta.client.head_object(Bucket=bucket_name, Key=key)
+        LOGGER.debug(response)
+
+        return response
+
+    def download_object(self, bucket_name: str = None, obj_name: str = None,
+                        file_path: str = None) -> str:
+        """
+        Downloading Object of the required Bucket.
+
+        :param bucket_name: Name of the bucket.
+        :param obj_name: Name of the object.
+        :param file_path: Path of the file.
+        :return: response.
+        """
+        response = self.s3_resource.Bucket(bucket_name).download_file(obj_name, file_path)
+        if os.path.exists(file_path):
+            LOGGER.debug("Object '%s' downloaded successfully on '%s'", obj_name, file_path)
+
+        return response
