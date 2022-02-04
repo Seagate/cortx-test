5b1a2b26b106f5aa5e64cb947fd54b65aedca2cc
Aayushi Sharma <aayushi.sharma@seagate.com>
2022-02-04 01:48:46 -0700

    Added Configurations
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>

--- comptests/prov/run_lc_tests.sh
+++ comptests/prov/run_lc_tests.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 TGT_NAME=$1
+WORK_SPACE=$2
 echo "Target node entry name: $TGT_NAME"
+echo -e "Workspace: $WORK_SPACE"
 echo -e "\n\n---------------------------------------[ Pytest running aws_configure ]--------------------------------------\n\n"
 pytest scripts/jenkins_job/aws_configure.py::test_create_acc_aws_conf --local True --target ${TGT_NAME}
 echo -e "\n\n---------------------------------------[ Pytest running prov_sanity ]--------------------------------------\n\n"
-pytest -m "prov_sanity" --local True --target ${TGT_NAME} --validate_certs False --junitxml "log/latest/results.xml" --html "log/latest/results.html"
+cd $WORK_SPACE && pytest -m "prov_sanity" --local True --target ${TGT_NAME} comptests/prov --validate_certs False --junitxml "log/latest/results.xml" --html "log/latest/results.html"
