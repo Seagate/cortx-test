bb78833cfcfebbb46071199d65b1c10b00ee6541
Swanand S Gadre <swanand.s.gadre@seagate.com>
2022-02-08 02:51:56 -0700

    Add port scanner
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>

--- tests/security/test_cortx_port_scanner_kubectl_svc.py
+++ tests/security/test_cortx_port_scanner_kubectl_svc.py
@@ -1,3 +1,6 @@
+"""
+This is cortx port scanner
+"""
 from kubernetes import client, config, watch
 from kubernetes.client import configuration
 from kubernetes.stream import stream
@@ -21,29 +24,29 @@ def main():
 
     # listing all pods
 
-    v1 = client.CoreV1Api()
+    core_client = client.CoreV1Api()
     LOGGER.info(" Listing pods with their IPs:")
-    ret = v1.list_pod_for_all_namespaces(watch=False)
+    ret = core_client.list_pod_for_all_namespaces(watch=False)
     for item in ret.items:
-        LOGGER.info( " %s\t%s\t%s" % (item.status.pod_ip, item.metadata.namespace, item.metadata.name))
-        list_containers = item.spec.containers
-        for list_each_con in list_containers:
-            LOGGER.info(list_each_con.name)
+         LOGGER.info( " %s\t%s\t%s" % (item.status.pod_ip, item.metadata.namespace, item.metadata.name))
+         list_containers = item.spec.containers
+         for list_each_con in list_containers:
+              LOGGER.info(list_each_con.name)
 
     # listing all services
 
     LOGGER.info(" Listing All the services running in cluster:")
-    ret = v1.list_service_for_all_namespaces(watch=False)
+    ret = core_client.list_service_for_all_namespaces(watch=False)
     for item in ret.items:
-        LOGGER.info(item.spec.cluster_ip)
-        if item.spec.cluster_ip != "None":
-           list_of_ports=item.spec.ports
-           LOGGER.info(" Listing all ports")
-           for port_item in list_of_ports:
-               LOGGER.info(port_item.name)
-               LOGGER.info(port_item.port)
-               if port_item.port != "NoneType":
-                  actual_ports.append(int(port_item.port))
+         LOGGER.info(item.spec.cluster_ip)
+         if item.spec.cluster_ip != "None":
+             list_of_ports=item.spec.ports
+             LOGGER.info(" Listing all ports")
+             for port_item in list_of_ports:
+                  LOGGER.info(port_item.name)
+                  LOGGER.info(port_item.port)
+                  if port_item.port != "NoneType":
+                      actual_ports.append(int(port_item.port))
 
     # Prepare a list of actual ports
 
@@ -53,9 +56,9 @@ def main():
     req_port_list = []
 
     with open("requirement_ports.txt") as file:
-         for req_ports in file:
-             LOGGER.info(req_ports.rstrip())
-             req_port_list.append(int(req_ports.rstrip()))
+          for req_ports in file:
+               LOGGER.info(req_ports.rstrip())
+               req_port_list.append(int(req_ports.rstrip()))
 
     req_port_list.sort()
 
@@ -79,8 +82,10 @@ def main():
     final_list_of_fault_ports.sort()
     LOGGER.info(final_list_of_fault_ports)
 
-    if final_list_of_fault_ports: assert_utils.assert_true(False, "Incorrect ports opened in cortx cluster...")
-    else: Logger.info(" Test Case successful!!")
+    if final_list_of_fault_ports: 
+        assert_utils.assert_true(False, "Incorrect ports opened in cortx cluster...")
+    else: 
+        LOGGER.info(" Test Case successful!!")
 
 
 if __name__ == '__main__':
--- tests/security/test_cortx_port_scanner_netstat.py
+++ tests/security/test_cortx_port_scanner_netstat.py
@@ -1,3 +1,7 @@
+"""
+This is cortx port scanner
+"""
+
 from kubernetes import client, config, watch
 from kubernetes.client import configuration
 from kubernetes.stream import stream
@@ -10,7 +14,6 @@ def main():
 
     # Initialization
 
-    actual_ports = []
     netstat_port_list = []
 
     LOGGER.info(' This is cortx port scanner!')
@@ -19,15 +22,15 @@ def main():
 
     config.load_kube_config(context=option)
 
-    v1 = client.CoreV1Api()
+    core_client = client.CoreV1Api()
 
     # Read port numbers from file specification
     req_port_list = []
 
     with open("requirement_ports.txt") as file:
-         for req_ports in file:
-             #LOGGER.info(req_ports.rstrip())
-             req_port_list.append(int(req_ports.rstrip()))
+          for req_ports in file:
+               #LOGGER.info(req_ports.rstrip())
+               req_port_list.append(int(req_ports.rstrip()))
 
     req_port_list.sort()
 
@@ -39,26 +42,26 @@ def main():
     exec_command_install_net_tool = ['/bin/sh', '-c', 'yum install net-tools -y']
     exec_command_run_netstat = ['/bin/sh', '-c', 'netstat -plnt | awk \'{print $4}\' | cut -d \":\" -f2']
 
-    ret = v1.list_pod_for_all_namespaces(watch=False)
+    ret = core_client.list_pod_for_all_namespaces(watch=False)
     for item in ret.items:
-        LOGGER.info( " %s\t%s\t%s" % (item.status.pod_ip, item.metadata.namespace, item.metadata.name))
-        if "cortx" in item.metadata.name:
-           list_containers = item.spec.containers
-           LOGGER.info(" --------------------------------------")
-           for list_each_con in list_containers:
-               LOGGER.info(" Open Ports for Container: " + list_each_con.name)
-               LOGGER.info(list_each_con.name)
-               LOGGER.info(" Installing net-tools")
-               resp = stream(v1.connect_get_namespaced_pod_exec, item.metadata.name, "default", container=list_each_con.name, command=exec_command_install_net_tool, stderr=True, stdin=False, stdout=True, tty=False, _preload_content=True)
-               LOGGER.info("Executing netstat")
-               resp = stream(v1.connect_get_namespaced_pod_exec, item.metadata.name, "default", container=list_each_con.name, command=exec_command_run_netstat, stderr=True, stdin=False, stdout=True, tty=False, _preload_content=True)
-               resp_list_of_ports=resp.splitlines()
-               for each_port in resp_list_of_ports:
-                   if has_numbers(each_port):
-                      netstat_port_list.append(int(each_port))
-               LOGGER.info("Response: " + resp)
+         LOGGER.info( " %s\t%s\t%s" % (item.status.pod_ip, item.metadata.namespace, item.metadata.name))
+         if "cortx" in item.metadata.name:
+             list_containers = item.spec.containers
+             LOGGER.info(" --------------------------------------")
+             for list_each_con in list_containers:
+                  LOGGER.info(" Open Ports for Container: " + list_each_con.name)
+                  LOGGER.info(list_each_con.name)
+                  LOGGER.info(" Installing net-tools")
+                  resp = stream(core_client.connect_get_namespaced_pod_exec, item.metadata.name, "default", container=list_each_con.name, command=exec_command_install_net_tool, stderr=True, stdin=False, stdout=True, tty=False, _preload_content=True)
+                  LOGGER.info("Executing netstat")
+                  resp = stream(core_client.connect_get_namespaced_pod_exec, item.metadata.name, "default", container=list_each_con.name, command=exec_command_run_netstat, stderr=True, stdin=False, stdout=True, tty=False, _preload_content=True)
+                  resp_list_of_ports=resp.splitlines()
+                  for each_port in resp_list_of_ports:
+                       if has_numbers(each_port):
+                           netstat_port_list.append(int(each_port))
+                  LOGGER.info("Response: " + resp)
 
-           LOGGER.info("--------------------------------------")
+             LOGGER.info("--------------------------------------")
 
 
     # Print the difference between actual_ports and req_port_list
@@ -86,13 +89,15 @@ def main():
     LOGGER.info(final_list_of_fault_ports)
 
 
-    if final_list_of_fault_ports: assert_utils.assert_true(False, "Incorrect ports opened in cortx cluster...")
-    else: Logger.info(" Test Case successful!!")
+    if final_list_of_fault_ports: 
+        assert_utils.assert_true(False, "Incorrect ports opened in cortx cluster...")
+    else: 
+        LOGGER.info(" Test Case successful!!")
 
 
 
-def has_numbers(inputString):
-    return any(char.isdigit() for char in inputString)
+def has_numbers(input_string):
+    return any(char.isdigit() for char in input_string)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, filename='port_scanner_netstat.log')
