1bd564208d66cce810fcf8755a5a22f15d767cb6
Zoheb Khan <zoheb.khan@seagate.com>
2022-02-10 04:54:10 -0700

    cosmetic change 2

--- comptests/motr/__init__.py
+++ comptests/motr/__init__.py
@@ -1,10 +1,9 @@
 import os
 import csv
 
-M0CRATE_TEST_CSV = os.path.join(os.getcwd(), "config/motr/m0crate_tests.csv")
-
 def read_m0crate_tests_csv():
-    with open(M0CRATE_TEST_CSV) as CSV_FH:
+    m0crate_test_csv = os.path.join(os.getcwd(), "config/motr/m0crate_tests.csv")
+    with open(m0crate_test_csv) as CSV_FH:
         CSV_DATA = [row for row in csv.DictReader(CSV_FH)]
     return CSV_DATA
 
