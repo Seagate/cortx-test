ab13b293e6291c03306373252b5d9a9c0003d47d
Zoheb Khan <zoheb.khan@seagate.com>
2022-02-10 05:28:39 -0700

    fixed codacy issues

--- comptests/motr/__init__.py
+++ comptests/motr/__init__.py
@@ -2,9 +2,14 @@ import os
 import csv
 
 def read_m0crate_tests_csv():
+    """
+    To read the m0crate test csv and return the list of dicts
+    return: list of dicts containing the csv key-value pairs
+    rtype: list
+    """
     m0crate_test_csv = os.path.join(os.getcwd(), "config/motr/m0crate_tests.csv")
-    with open(m0crate_test_csv) as CSV_FH:
-        CSV_DATA = [row for row in csv.DictReader(CSV_FH)]
-    return CSV_DATA
+    with open(m0crate_test_csv) as csv_fh:
+        csv_data = [row for row in csv.DictReader(csv_fh)]
+    return csv_data
 
 CSV_DATA = read_m0crate_tests_csv()
