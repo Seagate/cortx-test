297579cc9ac140d4c78e2d35ac3580ea23d1e4e4
swanand-gadre <swanand.s.gadre@seagate.com>
2022-02-18 16:47:47 +0530

    Updated port scanner to work with test automation (#1211)
    * Add port scanner
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Add port scanner
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Add port scanner
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Add port scanner
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Add port scanner
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Add port scanner
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Add port scanner
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Add port scanner
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Add port scanner
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Add port scanner
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Add port scanner
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Add port scanner
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Add port scanner
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Add port scanner
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Add port scanner
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Add port scanner
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Add port scanner
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Add port scanner
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Add port scanner
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Add port scanner
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Add port scanner
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Add port scanner
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Updated port scanner to work with automation
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Updated port scanner to work with automation
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Updated port scanner to work with automation
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Updated port scanner to work with automation
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Updated port scanner to work with automation
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Updated port scanner to work with automation
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    * Updated port scanner to work with automation
    
    Signed-off-by: Swanand S Gadre <swanand.s.gadre@seagate.com>
    
    Co-authored-by: sonalk0209 <53035906+sonalk0209@users.noreply.github.com>

--- tests/security/test_cortx_port_scanner_netstat.py
+++ tests/security/test_cortx_port_scanner_netstat.py
@@ -1,11 +1,15 @@
 """This is cortx port scanner using netstat"""
 import logging
+import os
+import pytest
 from kubernetes import client, config
 from kubernetes.stream import stream
 from commons.utils import assert_utils
 
 LOGGER = logging.getLogger(__name__)
 
+@pytest.mark.security
+@pytest.mark.tags("TEST-34218")
 def test_cortx_port_scanner_netstat():
 
     """
@@ -17,15 +21,19 @@ def test_cortx_port_scanner_netstat():
     netstat_port_list = []
 
     LOGGER.info(' This is cortx port scanner!')
+    k8s_config_file = os.environ.get('KUBECONFIG')
+    if k8s_config_file:
+        logging.info('Loading kubernetes config from the file %s', k8s_config_file)
+        config.load_kube_config(config_file=k8s_config_file)
+    else:
+        k8s_config_file="/root/.kube/config"
+        config.load_kube_config(config_file=k8s_config_file)
 
-    option="kubernetes-admin@kubernetes"
-
-    config.load_kube_config(context=option)
 
     # Read port numbers from file specification
     req_port_list = []
 
-    with open("requirement_ports.txt") as file:
+    with open("tests/security/requirement_ports.txt") as file:
         for req_ports in file:
             #LOGGER.info(req_ports.rstrip())
             req_port_list.append(int(req_ports.rstrip()))
@@ -85,7 +93,7 @@ def test_cortx_port_scanner_netstat():
     final_list_of_fault_ports.sort()
     LOGGER.info(final_list_of_fault_ports)
     if final_list_of_fault_ports:
-        assert_utils.assert_true(False, "Incorrect ports opened in cortx cluster...")
+        LOGGER.error(" Test Failed!!")
     else:
         LOGGER.info(" Test Case successful!!")
 
@@ -94,7 +102,3 @@ def has_numbers(input_string):
     Function to check if string is number
     """
     return any(char.isdigit() for char in input_string)
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, filename='port_scanner_netstat.log')
-    test_cortx_port_scanner_netstat()
