33f367352fc00e912e8f4d898e8ecc7dc0194e89
akshaym99 <akshay.s.mankar@seagate.com>
2022-02-21 22:43:35 +0530

    Used jira utils
    Signed-off-by: akshaym99 <akshay.s.mankar@seagate.com>

--- scripts/jenkins_job/get_tests_count.py
+++ scripts/jenkins_job/get_tests_count.py
@@ -2,12 +2,13 @@ from http import HTTPStatus
 import argparse
 import csv
 import os
-import requests
+#import requests
+from commons.utils import jira_utils
 
 # CSV file contain test count in sequence[total,passed,fail,skip,todo]
 TOTAL_COUNT_CSV = 'total_count.csv'
 
-
+'''
 def get_test_plan_test_count(test_plan_id, jira_id, jira_password):
     jira_url = f'https://jts.seagate.com/rest/raven/1.0/api/testplan/' \
                    f'{test_plan_id}/test'
@@ -22,7 +23,7 @@ def get_test_plan_test_count(test_plan_id, jira_id, jira_password):
         with open(os.path.join(os.getcwd(), TOTAL_COUNT_CSV), 'w', newline='') as tp_info_csv:
             writer = csv.writer(tp_info_csv)
             writer.writerow([total, passed, fail, skip, todo])
-
+'''
 
 def main():
     parser = argparse.ArgumentParser(description="TODO count")
@@ -32,8 +33,17 @@ def main():
     args = parser.parse_args()
     test_plan_id = args.tp
     jira_password = args.jp
-    jira_id = args.ji 
-    get_test_plan_test_count(test_plan_id, jira_id, jira_password)
+    jira_id = args.ji
+    res = jira_utils.JiraTask.get_test_list_from_test_plan(test_plan_id, jira_id, jira_password)
+    #get_test_plan_test_count(test_plan_id, jira_id, jira_password)
+    total = len(res)
+    todo = len([test for test in res if test['latestStatus'] == 'TODO'])
+    passed = len([test for test in res if test['latestStatus'] == 'PASS'])
+    fail = len([test for test in res if test['latestStatus'] == 'FAIL'])
+    skip = len([test for test in res if test['latestStatus'] == 'SKIPPED'])
+    with open(os.path.join(os.getcwd(), TOTAL_COUNT_CSV), 'w', newline='') as tp_info_csv:
+        writer = csv.writer(tp_info_csv)
+        writer.writerow([total, passed, fail, skip, todo])
 
 
 if __name__ == "__main__":
