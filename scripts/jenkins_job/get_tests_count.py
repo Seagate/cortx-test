86204665a83c2292802534c89ba9fb61661f1d35
akshaym99 <akshay.s.mankar@seagate.com>
2022-02-21 23:57:49 +0530

    Test count by Jira utils
    Signed-off-by: akshaym99 <akshay.s.mankar@seagate.com>

--- scripts/jenkins_job/get_tests_count.py
+++ scripts/jenkins_job/get_tests_count.py
@@ -1,29 +1,11 @@
-from http import HTTPStatus
 import argparse
 import csv
 import os
-#import requests
 from commons.utils import jira_utils
 
 # CSV file contain test count in sequence[total,passed,fail,skip,todo]
 TOTAL_COUNT_CSV = 'total_count.csv'
 
-'''
-def get_test_plan_test_count(test_plan_id, jira_id, jira_password):
-    jira_url = f'https://jts.seagate.com/rest/raven/1.0/api/testplan/' \
-                   f'{test_plan_id}/test'
-    response = requests.get(jira_url, auth=(jira_id, jira_password))
-    if response.status_code == HTTPStatus.OK:
-        res = response.json()
-        total = len(res)
-        todo = len([test for test in res if test['latestStatus'] == 'TODO'])
-        passed = len([test for test in res if test['latestStatus'] == 'PASS'])
-        fail = len([test for test in res if test['latestStatus'] == 'FAIL'])
-        skip = len([test for test in res if test['latestStatus'] == 'SKIPPED'])
-        with open(os.path.join(os.getcwd(), TOTAL_COUNT_CSV), 'w', newline='') as tp_info_csv:
-            writer = csv.writer(tp_info_csv)
-            writer.writerow([total, passed, fail, skip, todo])
-'''
 
 def main():
     parser = argparse.ArgumentParser(description="TODO count")
@@ -35,7 +17,6 @@ def main():
     jira_password = args.jp
     jira_id = args.ji
     res = jira_utils.JiraTask.get_test_list_from_test_plan(test_plan_id, jira_id, jira_password)
-    #get_test_plan_test_count(test_plan_id, jira_id, jira_password)
     total = len(res)
     todo = len([test for test in res if test['latestStatus'] == 'TODO'])
     passed = len([test for test in res if test['latestStatus'] == 'PASS'])
