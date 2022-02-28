13ae9cfa868e9c055aa101b3f8b85bd5d8e3f3d5
Vishal Dhobale <vishal.dhobale@seagate.com>
2022-02-28 18:07:37 +0530

    Refactored s3api rest lib to support asyncio using aiobotocore.
    Signed-off-by: Vishal Dhobale <vishal.dhobale@seagate.com>

--- libs/io/s3api/s3_restapi.py
+++ libs/io/s3api/s3_restapi.py
@@ -21,12 +21,13 @@
 
 """Python Library using boto3 module."""
 
+"""Python Library using boto3 module."""
+
 import logging
-from botocore.config import Config
 
-import boto3
+from aiobotocore.config import AioConfig
+from aiobotocore.session import get_session
 from config.s3 import S3_CFG
-
 
 LOGGER = logging.getLogger(__name__)
 
@@ -40,7 +41,6 @@ class S3RestApi(object):
                  **kwargs):
         """
         method initializes members of S3Lib.
-
         Different instances need to be created as per different parameter values like access_key,
         secret_key etc.
         :param access_key: access key.
@@ -51,38 +51,27 @@ class S3RestApi(object):
         :param aws_session_token: aws_session_token.
         :param debug: debug mode.
         """
-        region = kwargs.get("region", S3_CFG["region"])
-        aws_session_token = kwargs.get("aws_session_token", None)
+        self.access_key = access_key
+        self.secret_key = secret_key
+        self.region = kwargs.get("region", S3_CFG["region"])
+        self.aws_session_token = kwargs.get("aws_session_token", None)
+        self.use_ssl = kwargs.get("use_ssl", S3_CFG["use_ssl"])
+        self.endpoint_url = kwargs.get("endpoint_url", S3_CFG["endpoint"])
+        self.config = AioConfig(retries={'max_attempts': S3_CFG["s3api_retry"]})
+        self.session = get_session()
         debug = kwargs.get("debug", S3_CFG["debug"])
-        use_ssl = kwargs.get("use_ssl", S3_CFG["use_ssl"])
-        endpoint_url = kwargs.get("endpoint_url", S3_CFG["endpoint"])
-        config = Config(retries={'max_attempts': S3_CFG["s3api_retry"]})
         if debug:
             # Uncomment to enable debug
-            boto3.set_stream_logger(name="botocore")
-        self.s3_resource = boto3.resource("s3",
-                                          use_ssl=use_ssl,
-                                          verify=False,
-                                          aws_access_key_id=access_key,
-                                          aws_secret_access_key=secret_key,
-                                          endpoint_url=endpoint_url,
-                                          region_name=region,
-                                          aws_session_token=aws_session_token,
-                                          config=config)
-        self.s3_client = boto3.client("s3",
-                                      use_ssl=use_ssl,
-                                      verify=False,
-                                      aws_access_key_id=access_key,
-                                      aws_secret_access_key=secret_key,
-                                      endpoint_url=endpoint_url,
-                                      region_name=region,
-                                      aws_session_token=aws_session_token,
-                                      config=config)
+            self.session.set_debug_logger(name="botocore")
 
-    def __del__(self):
-        """Destroy all core objects."""
-        try:
-            del self.s3_client
-            del self.s3_resource
-        except NameError as error:
-            LOGGER.warning(error)
+    def get_client(self):
+        """Create s3 client session for asyncio operations."""
+        return self.session.create_client(service_name="s3",
+                                          use_ssl=self.use_ssl,
+                                          verify=False,
+                                          aws_access_key_id=self.access_key,
+                                          aws_secret_access_key=self.secret_key,
+                                          endpoint_url=self.endpoint_url,
+                                          region_name=self.region,
+                                          aws_session_token=self.aws_session_token,
+                                          config=self.config)
