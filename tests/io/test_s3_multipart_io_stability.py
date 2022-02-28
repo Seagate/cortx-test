bac349ad50593f53eabdab62055a61cddbd6384c
Vishal Dhobale <vishal.dhobale@seagate.com>
2022-02-28 20:49:57 +0530

    Refactored s3api multipart test suites.
    Signed-off-by: Vishal Dhobale <vishal.dhobale@seagate.com>

--- tests/io/test_s3_multipart_io_stability.py
+++ tests/io/test_s3_multipart_io_stability.py
@@ -65,7 +65,7 @@ class TestMultiParts(S3MultiParts, S3Object, S3Bucket):
         self.finish_time = datetime.now() + duration if duration else datetime.now() + \
             timedelta(hours=int(100 * 24))
 
-    def execute_multipart_workload(self):
+    async def execute_multipart_workload(self):
         """Execute multipart workload for specific duration."""
         while True:
             logger.info("Iteration %s is started...", self.iteration)
@@ -76,10 +76,11 @@ class TestMultiParts(S3MultiParts, S3Object, S3Bucket):
                 number_of_parts = random.randrange(self.part_range["start"], self.part_range["end"])
                 single_part_size = file_size // number_of_parts
                 logger.info("single part size: %s MB", single_part_size / (1024 ** 2))
-                response = self.create_bucket(self.mpart_bucket)
-                assert response.name == self.mpart_bucket, \
+                response = await self.create_bucket(self.mpart_bucket)
+                assert self.mpart_bucket not in response["Location"], \
                     f"Failed to create bucket: {self.mpart_bucket}"
-                response = self.create_multipart_upload(self.mpart_bucket, self.s3mpart_object)
+                response = await self.create_multipart_upload(
+                    self.mpart_bucket, self.s3mpart_object)
                 assert response["UploadId"] is not None, \
                     f"Failed to initiate multipart upload: {response}"
                 mpu_id = response["UploadId"]
@@ -87,30 +88,31 @@ class TestMultiParts(S3MultiParts, S3Object, S3Bucket):
                 file_hash = hashlib.sha256()
                 for i in range(1, number_of_parts + 1):
                     byte_s = os.urandom(single_part_size)
-                    response = self.upload_part(byte_s, self.mpart_bucket,
-                                                self.object_size, upload_id=mpu_id, part_number=i)
+                    response = await self.upload_part(byte_s, self.mpart_bucket,
+                                                      self.object_size, upload_id=mpu_id,
+                                                      part_number=i)
                     assert response["ETag"] is not None, f"Failed upload part: {response}"
                     parts.append({"PartNumber": i, "ETag": response["ETag"]})
                     file_hash.update(byte_s)
                 upload_obj_checksum = file_hash.hexdigest()
                 logger.info("Checksum of uploaded object: %s", upload_obj_checksum)
-                response = self.list_parts(mpu_id, self.mpart_bucket, self.s3mpart_object)
+                response = await self.list_parts(mpu_id, self.mpart_bucket, self.s3mpart_object)
                 assert response, f"Failed to list parts: {response}"
-                response = self.list_multipart_uploads(self.mpart_bucket)
+                response = await self.list_multipart_uploads(self.mpart_bucket)
                 assert response, f"Failed to list multipart uploads: {response}"
-                response = self.complete_multipart_upload(
+                response = await self.complete_multipart_upload(
                     mpu_id, parts, self.mpart_bucket, self.s3mpart_object)
                 assert response, f"Failed to completed multi parts: {response}"
-                response = self.head_object(self.mpart_bucket, self.s3mpart_object)
+                response = await self.head_object(self.mpart_bucket, self.s3mpart_object)
                 assert response, f"Failed to do head object on {self.s3mpart_object}"
-                download_obj_checksum = self.get_s3object_checksum(
+                download_obj_checksum = await self.get_s3object_checksum(
                     self.mpart_bucket, self.s3mpart_object, single_part_size)
                 logger.info("Checksum of s3 object: %s", download_obj_checksum)
                 if upload_obj_checksum != download_obj_checksum:
                     raise ClientError(
                         f"Failed to match checksum: {upload_obj_checksum}, {download_obj_checksum}",
                         operation_name="Match checksum")
-                response = self.delete_bucket(self.mpart_bucket, force=True)
+                response = await self.delete_bucket(self.mpart_bucket, force=True)
                 assert response["ResponseMetadata"][
                     "HTTPStatusCode"] == 204, f"Failed to delete s3 bucket: {self.mpart_bucket}"
             except (ClientError, IOError, AssertionError) as err:
