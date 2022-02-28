c68b7d66b507d972ca44dc275c96c17bd3a96bd2
Vishal Dhobale <vishal.dhobale@seagate.com>
2022-02-28 19:59:32 +0530

    Refactored s3api bucket lib to support asyncio using aiobotocore.
    Signed-off-by: Vishal Dhobale <vishal.dhobale@seagate.com>

--- libs/io/s3api/s3_bucket_ops.py
+++ libs/io/s3api/s3_bucket_ops.py
@@ -30,55 +30,59 @@ LOGGER = logging.getLogger(__name__)
 class S3Bucket(S3RestApi):
     """Class for bucket operations."""
 
-    def create_bucket(self, bucket_name: str) -> object:
+    async def create_bucket(self, bucket_name: str) -> object:
         """
         Creating Bucket.
 
         :param bucket_name: Name of the bucket.
         :return: Response of create bucket.
         """
-        response = self.s3_resource.create_bucket(Bucket=bucket_name)
-        LOGGER.debug("Response: %s", str(response))
+        async with self.get_client() as client:
+            response = await client.create_bucket(Bucket=bucket_name)
+            LOGGER.debug("Response: %s", str(response))
 
         return response
 
-    def list_bucket(self) -> list:
+    async def list_bucket(self) -> list:
         """
         Listing all the buckets.
 
         :return: Response of bucket list.
         """
-        response = [bucket.name for bucket in self.s3_resource.buckets.all()]
-        LOGGER.debug(response)
+        async with self.get_client() as client:
+            response = [bucket.name for bucket in client.buckets.all()]
+            LOGGER.debug(response)
 
         return response
 
-    def head_bucket(self, bucket_name: str) -> dict:
+    async def head_bucket(self, bucket_name: str) -> dict:
         """
         To determine if a bucket exists and have a permission to access it.
 
         :param bucket_name: Name of the bucket.
         :return: Response of head bucket.
         """
-        response = self.s3_resource.meta.client.head_bucket(Bucket=bucket_name)
-        LOGGER.debug(response)
+        async with self.get_client() as client:
+            response = await client.meta.client.head_bucket(Bucket=bucket_name)
+            LOGGER.debug(response)
 
         return response
 
-    def get_bucket_location(self, bucket_name: str) -> dict:
+    async def get_bucket_location(self, bucket_name: str) -> dict:
         """
         Getting Bucket Location.
 
         :param bucket_name: Name of the bucket.
         :return: Response of bucket location.
         """
-        LOGGER.debug("BucketName: %s", bucket_name)
-        response = self.s3_resource.meta.client.get_bucket_location(Bucket=bucket_name)
-        LOGGER.debug(response)
+        async with self.get_client() as client:
+            LOGGER.debug("BucketName: %s", bucket_name)
+            response = await client.meta.client.get_bucket_location(Bucket=bucket_name)
+            LOGGER.debug(response)
 
         return response
 
-    def delete_bucket(self, bucket_name: str, force: bool = False) -> dict:
+    async def delete_bucket(self, bucket_name: str, force: bool = False) -> dict:
         """
         Deleting the empty bucket or deleting the buckets along with objects stored in it.
 
@@ -86,28 +90,30 @@ class S3Bucket(S3RestApi):
         :param force: Value for delete bucket with object or without object.
         :return: Response of delete bucket.
         """
-        bucket = self.s3_resource.Bucket(bucket_name)
-        if force:
-            LOGGER.debug("This might cause data loss as you have opted for bucket deletion with "
-                         "objects in it")
-            response = bucket.objects.all().delete()
-            LOGGER.debug("Objects deleted successfully. response: %s", response)
-        response = bucket.delete()
-        LOGGER.debug("Bucket '%s' deleted successfully. Response: %s", bucket_name, response)
+        async with self.get_client() as client:
+            bucket = await client.Bucket(bucket_name)
+            if force:
+                LOGGER.debug("This might cause data loss as you have opted for bucket deletion"
+                             " with objects in it")
+                response = bucket.objects.all().delete()
+                LOGGER.debug("Objects deleted successfully. response: %s", response)
+            response = bucket.delete()
+            LOGGER.debug("Bucket '%s' deleted successfully. Response: %s", bucket_name, response)
 
         return response
 
-    def get_bucket_storage(self, bucket_name: str) -> int:
+    async def get_bucket_storage(self, bucket_name: str) -> int:
         """
         Getting consumed storage of the s3 bucket.
 
         :param bucket_name: Name of the bucket.
         :return: storage consumed by s3 bucket.
         """
-        total_size = 0
-        bucket = self.s3_resource.Bucket(bucket_name)
-        for each_object in bucket.objects.all():
-            total_size += each_object.size
-        LOGGER.debug("Total storage: %s", total_size)
+        async with self.get_client() as client:
+            total_size = 0
+            bucket = await client.Bucket(bucket_name)
+            for each_object in bucket.objects.all():
+                total_size += each_object.size
+            LOGGER.debug("Total storage: %s", total_size)
 
         return total_size
