293c206c67ca3fac00fdb65a0e02c8f2c98439a7
kulrk <rohan.1.kulkarni@seagate.com>
2022-03-01 18:06:37 +0530

    minor
    Signed-off-by: kulrk <rohan.1.kulkarni@seagate.com>

--- io_driver.py
+++ io_driver.py
@@ -94,7 +94,7 @@ def parse_args():
     parser.add_argument("-ep", "--endpoint", type=str,
                         help="fqdn of s3 endpoint for io operations.", default="s3.seagate.com")
     parser.add_argument("-nn", "--number-of-nodes", type=int,
-                        help="number of nodes in k8s system", default=3)
+                        help="number of nodes in k8s system")
     return parser.parse_args()
 
 
