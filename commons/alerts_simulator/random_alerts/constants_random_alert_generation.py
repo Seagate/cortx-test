7bcf54f305a21af73eb2017f3417a28d0e725d50
seagate-sarang-sawant <70256535+seagate-sarang-sawant@users.noreply.github.com>
2022-03-01 15:54:59 +0530

    CORTX-28696 Fixed another set of files with header and added llicense file.

--- commons/alerts_simulator/random_alerts/constants_random_alert_generation.py
+++ commons/alerts_simulator/random_alerts/constants_random_alert_generation.py
@@ -1,4 +1,20 @@
-"""File consists constants to be used for generating random alerts."""
+#
+# Copyright (c) 2022 Seagate Technology LLC and/or its Affiliates
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# 
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+#"""File consists constants to be used for generating random alerts."""
 # dicts of type of alerts and alert setup functions
 from aenum import Enum, NoAlias
 
