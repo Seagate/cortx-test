d4416777e3064476b2623d23b57877e404d21655
azahar.khan <azahar.khan@seagate.com>
2022-03-03 17:12:22 +0530

    object-range_read2
    Signed-off-by: azahar.khan <azahar.khan@seagate.com>

--- libs/io/s3api/s3_object_ops.py
+++ libs/io/s3api/s3_object_ops.py
@@ -166,12 +166,4 @@ class S3Object(S3RestApi):
                 file_hash.update(content)
 
         return file_hash.hexdigest()
-
-    @staticmethod
-    def calculate_checksum(file_path, offset_byte, size):
-        with open(file_path, 'rb') as fout:
-            fout.seek(offset_byte - 1)
-            content = fout.read(size)
-        file_hash = hashlib.sha256()
-        file_hash.update(content)
-        return file_hash.hexdigest()
+        
