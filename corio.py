914d3401bf4a8fb30151904ad06df4665420ad8a
azahar.khan <azahar.khan@seagate.com>
2022-03-03 17:26:11 +0530

    object-range_read4
    Signed-off-by: azahar.khan <azahar.khan@seagate.com>

--- corio.py
+++ corio.py
@@ -42,10 +42,7 @@ from tests.io import test_s3_object_io_stability
 nfs_dir = NFS_SERVER_DIR
 mount_dir = MOUNT_DIR
 function_mapping = {
-    'copy_object': [test_s3_copy_object.TestS3CopyObjects, 'execute_copy_object_workload'],
-    'bucket': [test_s3_bucket_io_stability.TestBucketOps, 'execute_bucket_workload'],
-    'multipart': [test_s3_multipart_io_stability.TestMultiParts, 'execute_multipart_workload'],
-    'object': [test_s3_object_io_stability.TestS3Object, 'execute_object_workload']
+    'object_range_read': [test_s3_obj_range_read_io_stability, 'execute_object_range_read_workload']
 }
 
 
