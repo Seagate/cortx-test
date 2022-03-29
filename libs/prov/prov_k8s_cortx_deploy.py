39c22a00b875dd3dedc0633e7c7fcb7313a4b241
pragam <pragam.jain@seagate.com>
2022-03-29 17:10:17 +0530

    Addressed the review comments
    Signed-off-by: pragam <pragam.jain@seagate.com>

--- libs/prov/prov_k8s_cortx_deploy.py
+++ libs/prov/prov_k8s_cortx_deploy.py
@@ -1226,7 +1226,7 @@ class ProvDeployK8sCortxLib:
                 if not resp[0]:
                     LOGGER.debug("Did not get expected response: %s", resp)
                 ext_ip = resp[1]
-                port = resp[3]
+                port = resp[2]
                 ext_port_ip = self.deploy_cfg['protocol'].format(ext_ip, port)
                 LOGGER.debug("External LB value, ip and port will be: %s", ext_port_ip)
             else:
