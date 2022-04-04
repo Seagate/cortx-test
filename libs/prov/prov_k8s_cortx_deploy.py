52321a11fc5d061742f47de6c81f0f428214f544
Aayushi Sharma <aayushi.sharma@seagate.com>
2022-04-04 05:13:45 -0600

    Resolved conflicts
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>

--- libs/prov/prov_k8s_cortx_deploy.py
+++ libs/prov/prov_k8s_cortx_deploy.py
@@ -1540,6 +1540,18 @@ class ProvDeployK8sCortxLib:
             pointer.close()
         return True, file_path
 
+    def service_upgrade_software(node_obj: LogicalNode, upgrade_image_version: str) -> tuple:
+        """
+        Helper function to upgrade.
+        :param node_obj: Master node(Logical Node object)
+        :param upgrade_image_version: Version Image to Upgrade.
+        :return: True/False
+        """
+        LOGGER.info("Upgrading CORTX image to version: %s.", upgrade_image_version)
+        resp = node_obj.execute_cmd(common_cmd.UPGRADE_CLUSTER_DESTRUPTIVE_CMD.format(
+            PROV_CFG['k8s_cortx_deploy']["k8s_dir"]), read_lines=True)
+        return resp
+        
     @staticmethod
     def get_installed_version(resp):
         """
