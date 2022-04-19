ab1543e0a5cbb7c379b50219780290f932e09bb4
Prabhsimran Singh <prabhsimran.singh@seagate.com>
2022-04-19 10:23:24 +0530

    Added function for Get user info.

--- comptests/s3/test_rest_api_rgw.py
+++ comptests/s3/test_rest_api_rgw.py
@@ -63,14 +63,16 @@ class TestRestApiRgw:
                 self.log.warning("Ignoring %s while deleting user: %s", err, usr)
         for usr in delete_success:
             self.created_users.remove(usr)
-        self.log.info("csm delete success list %s", delete_success)
-        self.log.info("csm delete failed list %s", delete_failed)
+        self.log.info("User delete success list %s", delete_success)
+        self.log.info("User delete failed list %s", delete_failed)
 
     @pytest.mark.api_user_ops
     @pytest.mark.tags('TEST-36622')
     def test_user_create_36622(self):
         """Create new IAM user."""
-        self.log.info("START: Test create new IAM user.")
+        test_case_name = cortxlogging.get_frame()
+        self.log.info("##### Test started -  %s #####", test_case_name)
+        self.log.info("START: Test create iam user specifying uid and display name.")
         user_name = f"{self.user_name_prefix}{str(time.perf_counter_ns()).replace('.', '_')}"
         user_params = {
             'display-name': user_name,
@@ -80,20 +82,15 @@ class TestRestApiRgw:
             "Step 1: Creating a new IAM user with name %s", str(user_name))
         loop = asyncio.get_event_loop()
         status, user_info = loop.run_until_complete(self.obj.create_user(user_params))
-        print(user_info)
+        assert status == HTTPStatus.OK , "Not able to create user. Test Failed"
+        self.log.info("Created user details: %s",user_info)
         self.log.info(
             "Step 2: Verifying that new IAM user is created successfully")
-        assert status == 200
-        self.log.info("END: Tested create new IAM user.")
-        self.log.info(
-            "Step 4: Deleting a IAM user with name %s", str(user_name))
-        loop = asyncio.get_event_loop()
-        status , user_info = loop.run_until_complete(self.obj.delete_user(user_params))
-        self.log.info(
-            "Step 5: Verifying that new IAM user is Deleted successfully")
-        assert status == 200
-        self.log.info(
-            "END: Deleted created user : %s", user_name)
+        status, user_info = loop.run_until_complete(self.obj.get_user_info(user_params))
+        assert status == HTTPStatus.OK , "Not able to Get user Info. Test Failed"
+        self.log.info("Get user info output: %s",user_info)
+        self.created_users.append(user_params)
+        self.log.info("END: Test create iam user specifying uid and display name. : %s",test_case_name)
        
     @pytest.mark.api_user_ops
     @pytest.mark.tags('TEST-36630')
@@ -122,21 +119,13 @@ class TestRestApiRgw:
         self.log.info("Get user info output: %s",user_info)
         self.created_users.append(user_params)
         self.log.info("END: Test create iam user specifying uid and display name and email. : %s",test_case_name)
-        """self.log.info(
-            "Step 3: Deleting a IAM user with name %s", str(user_name))
-        loop = asyncio.get_event_loop()
-        status , user_info = loop.run_until_complete(self.obj.delete_user(user_params))
-        self.log.info(
-            "Step 4: Verifying that new IAM user is Deleted successfully")
-        assert status == 200
-        self.log.info(
-            "END: Deleted created user : %s", user_name) 
-        """
 
     @pytest.mark.api_user_ops
     @pytest.mark.tags('TEST-36632')
     def test_user_create_36632(self):
         """Create new IAM user."""
+        test_case_name = cortxlogging.get_frame()
+        self.log.info("##### Test started -  %s #####", test_case_name)
         self.log.info("START: Test create iam user specifying just uid .")
         user_name = f"{self.user_name_prefix}{str(time.perf_counter_ns()).replace('.', '_')}"
         user_params = {
@@ -146,53 +135,47 @@ class TestRestApiRgw:
             "Step 1: Creating a new IAM user with name %s", str(user_name))
         loop = asyncio.get_event_loop()
         status, user_info = loop.run_until_complete(self.obj.create_user(user_params))
-        print(user_info)
-        self.log.info(
-            "Step 2: Verifying that new IAM user creation get failed when only uid is specified")
-        assert status == 400
-        self.log.info("END: Test create iam user specifying just uid .")
+        assert status == HTTPStatus.BAD_REQUEST, "Able to create user with just uid. Test Failed"
+        #self.created_users.append(user_params)
+        self.log.info("END: Test create iam user specifying just uid . : %s",test_case_name)
 
     @pytest.mark.api_user_ops
     @pytest.mark.tags('TEST-36633')
     def test_user_create_36633(self):
         """Create new IAM user."""
+        test_case_name = cortxlogging.get_frame()
+        self.log.info("##### Test started -  %s #####", test_case_name)
         self.log.info("START: Test create using uid which already exist.")
         user_name = f"{self.user_name_prefix}{str(time.perf_counter_ns()).replace('.', '_')}"
-        email=f"{user_name}@seagate.com"
         user_params = {
             'display-name': user_name,
-            'email' : email,
             'uid' : user_name
          }
         self.log.info(
             "Step 1: Creating a new IAM user with name %s", str(user_name))
         loop = asyncio.get_event_loop()
         status, user_info = loop.run_until_complete(self.obj.create_user(user_params))
-        print(user_info)
+        assert status == HTTPStatus.OK , "Not able to create user. Test Failed"
+        self.log.info("Created user details: %s",user_info)
         self.log.info(
             "Step 2: Verifying that new IAM user is created successfully")
-        assert status == 200
+        status, user_info = loop.run_until_complete(self.obj.get_user_info(user_params))
+        assert status == HTTPStatus.OK , "Not able to Get user Info. Test Failed"
+        self.log.info("Get user info output: %s",user_info)
+        self.created_users.append(user_params)
         self.log.info(
             "Step 3: Creating another IAM user with same name %s", str(user_name))
         loop = asyncio.get_event_loop()
         status, user_info = loop.run_until_complete(self.obj.create_user(user_params))
-        print(user_info)
-        assert status == 409
-        self.log.info("END: Test create using uid which already exist.")
-        self.log.info(
-            "Step 4: Deleting a IAM user with name %s", str(user_name))
-        loop = asyncio.get_event_loop()
-        status , user_info = loop.run_until_complete(self.obj.delete_user(user_params))
-        self.log.info(
-            "Step 4: Verifying that new IAM user is Deleted successfully")
-        assert status == 200
-        self.log.info(
-            "END: Deleted created user : %s", user_name)
+        assert status == HTTPStatus.CONFLICT , "Didn't get the expected error"
+        self.log.info("END: Test create using uid which already exist. : %s",test_case_name)
 
     @pytest.mark.api_user_ops
     @pytest.mark.tags('TEST-36636')
     def test_user_create_36636(self):
         """Create new IAM user."""
+        test_case_name = cortxlogging.get_frame()
+        self.log.info("##### Test started -  %s #####", test_case_name)
         self.log.info("START: Test create user using display name which already exist.")
         user_name = f"{self.user_name_prefix}{str(time.perf_counter_ns()).replace('.', '_')}"
         user_name2 = f"{self.user_name_prefix}{str(time.perf_counter_ns()).replace('.', '_')}"
@@ -208,36 +191,25 @@ class TestRestApiRgw:
             "Step 1: Creating a new IAM user with name %s", str(user_name))
         loop = asyncio.get_event_loop()
         status, user_info = loop.run_until_complete(self.obj.create_user(user_params))
-        print(user_info)
+        assert status == HTTPStatus.OK , "Not able to create user. Test Failed"
+        self.log.info("Created user details: %s",user_info)
         self.log.info(
             "Step 2: Verifying that new IAM user is created successfully")
-        assert status == 200
+        status, user_info = loop.run_until_complete(self.obj.get_user_info(user_params))
+        assert status == HTTPStatus.OK , "Not able to Get user Info. Test Failed"
+        self.log.info("Get user info output: %s",user_info)
+        self.created_users.append(user_params)
         self.log.info(
             "Step 3: Creating another IAM user with same display name %s", str(user_name))
         loop = asyncio.get_event_loop()
         status, user_info = loop.run_until_complete(self.obj.create_user(user_params2))
-        print(user_info)
+        assert status == HTTPStatus.OK , "Not able to create user. Test Failed"
+        self.log.info("Created user details: %s",user_info)
         self.log.info(
-            "Step 4: Verifying that new IAM user is created successfully")
-        assert status == 200
-        self.log.info("END: Test create using uid which already exist.")
-        self.log.info(
-            "Step 5: Deleting a IAM user with name %s", str(user_name))
-        loop = asyncio.get_event_loop()
-        status , user_info = loop.run_until_complete(self.obj.delete_user(user_params))
-        self.log.info(
-            "Step 6: Verifying that new IAM user is Deleted successfully")
-        assert status == 200
-        self.log.info(
-            "Deleted created user : %s", user_name)
-        self.log.info(
-            "Step 7: Deleting a IAM user with name %s", str(user_name2))
-        loop = asyncio.get_event_loop()
-        status , user_info = loop.run_until_complete(self.obj.delete_user(user_params2))
-        self.log.info(
-            "Step 8: Verifying that new IAM user is Deleted successfully")
-        assert status == 200
-        self.log.info(
-            "END: Deleted created user : %s", user_name2)
-
+            "Step 4: Verifying that new IAM user is created successfully with same display name")
+        status, user_info = loop.run_until_complete(self.obj.get_user_info(user_params2))
+        assert status == HTTPStatus.OK , "Not able to Get user Info. Test Failed"
+        self.log.info("Get user info output: %s",user_info)
+        self.created_users.append(user_params2)
+        self.log.info("END: Test create using uid which already exist. : %s",test_case_name)
 
