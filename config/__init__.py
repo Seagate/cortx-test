eb055a21652b180d914940b3956903e767e1929f
Rajesh Pratap Singh Chouhan <rajesh.chouhan7@gmail.com>
2022-04-28 14:25:43 +0530

    fixed codacy
    Signed-off-by: Rajesh Pratap Singh Chouhan <rajesh.chouhan7@gmail.com>

--- config/__init__.py
+++ config/__init__.py
@@ -55,7 +55,7 @@ pytest_args = sys.argv
 proc_name = os.path.split(pytest_args[0])[-1]
 target_filter = re.compile(".*--target")
 pytest_args = split_args(pytest_args)  # sanitize
-csm_checks = False
+CSM_CHECKS = False
 if proc_name == 'pytest' and '--local' in pytest_args and '--target' in pytest_args:
     # This condition will execute when args ore in format ['--target','<target name'>]
     if pytest_args[pytest_args.index("--local") + 1]:
@@ -91,8 +91,8 @@ if target and proc_name in ["testrunner.py", "testrunner", "pytest"]:
 
     _csm_checks = ('-csm' if '-csm' in pytest_args else (
         '--csm_checks' if '--csm_checks' in pytest_args else None))
-    csm_checks = pytest_args[
-        pytest_args.index(_csm_checks) + 1] if _csm_checks else 'False'
+    CSM_CHECKS = pytest_args[
+        pytest_args.index(_csm_checks) + 1] if _csm_checks else False
 
     _validate_certs = ('-c' if '-c' in pytest_args else (
         '----validate_certs' if '----validate_certs' in pytest_args else None))
@@ -146,7 +146,7 @@ else:
         fpath=CSM_CONFIG, config_key="Restcall", target=target, target_key="csm")
 
 CSM_CFG = configmanager.get_config_wrapper(fpath=CSM_CONFIG)
-if parse(csm_checks):
+if parse(CSM_CHECKS):
     CSM_REST_CFG["msg_check"] = "enable"
     CSM_CFG["Restcall"]["msg_check"] = "enable"
 RAS_VAL = configmanager.get_config_wrapper(
