fbd3cd77b90e1ff6f58784cbd7c233e621d68fcb
kulrk <rohan.1.kulkarni@seagate.com>
2022-05-02 17:28:00 +0530

    fix for support bundle call
    Signed-off-by: kulrk <rohan.1.kulkarni@seagate.com>

--- tests/iostability/test_iostability_happy_path.py
+++ tests/iostability/test_iostability_happy_path.py
@@ -57,7 +57,7 @@ class TestIOWorkload:
         cls.test_completed = False
         cls.iolib = IOStabilityLib()
 
-    def teardown_class(self):
+    def teardown_method(self):
         """Teardown class method."""
         if not self.test_completed:
             self.log.info("Test Failure observed, collecting support bundle")
