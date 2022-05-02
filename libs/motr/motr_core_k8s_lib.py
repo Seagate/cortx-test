9caa8f562f99b86dab2cd91b9f171850071a6fdc
Zoheb Khan <zoheb.khan@seagate.com>
2022-05-02 06:11:34 -0600

    Codacy warning fix
    Signed-off-by: Zoheb Khan <zoheb.khan@seagate.com>

--- libs/motr/motr_core_k8s_lib.py
+++ libs/motr/motr_core_k8s_lib.py
@@ -24,13 +24,14 @@ Python library contains methods which provides the services endpoints.
 import json
 import logging
 from random import SystemRandom
+
 from config import CMN_CFG
-from commons import commands as common_cmd
-from commons import constants as common_const
-from commons.utils import config_utils, system_utils, assert_utils
+from commons.utils import assert_utils, config_utils, system_utils
 from libs.ha.ha_common_libs_k8s import HAK8s
 from commons.helpers.pods_helper import LogicalNode
 from libs.motr import TEMP_PATH, BSIZE_LAYOUT_MAP, FILE_BLOCK_COUNT
+from commons import commands as common_cmd
+from commons import constants as common_const
 
 log = logging.getLogger(__name__)
 
