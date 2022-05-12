9d34158965cab6587044d8a73169d476b5f054c5
ankita-93 <ankita.thummar@seagate.com>
2022-05-12 15:38:58 +0530

    Codacy fix
    Signed-off-by: ankita-93 <ankita.thummar@seagate.com>

--- libs/ha/ha_common_libs_k8s.py
+++ libs/ha/ha_common_libs_k8s.py
@@ -745,7 +745,7 @@ class HAK8s:
             LOGGER.info("Response: %s", res)
             mpu_id = res[1]["UploadId"]
             LOGGER.info("Multipart Upload initiated with mpu_id %s", mpu_id)
-        except (BaseException, CTException) as error:
+        except CTException as error:
             LOGGER.error("Failed mpu due to error %s. Exiting from background process.", error)
             sys.exit(1)
 
