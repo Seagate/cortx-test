d1fcf11976d27ec02a55a3796875ff828380a6e1
pragamj <86817153+pragamj@users.noreply.github.com>
2022-05-16 15:56:51 +0530

    Hotfix ha timeout (#1537)
    * reverted the workaround
    
    Signed-off-by: pragam <pragam.jain@seagate.com>
    
    * Addressed the review comment
    
    Signed-off-by: pragam <pragam.jain@seagate.com>

--- libs/prov/prov_k8s_cortx_deploy.py
+++ libs/prov/prov_k8s_cortx_deploy.py
@@ -32,7 +32,7 @@ import string
 import time
 from threading import Thread
 from typing import List
-
+from string import Template
 import requests.exceptions
 import yaml
 
@@ -280,11 +280,8 @@ class ProvDeployK8sCortxLib:
         return : True/False and resp
         """
         LOGGER.info("Deploy Cortx cloud")
-        export_cmd = common_cmd.LINUX_EXPORT.format(self.deploy_cfg["deploy_ha_timeout_key"],
-                                                    str(self.deploy_cfg["deploy_ha_timeout_val"])
-                                                    + "s")
-        cmd = export_cmd + " && " + common_cmd.DEPLOY_CLUSTER_CMD.format(
-            remote_code_path, self.deploy_cfg['log_file'])
+        cmd = Template(common_cmd.DEPLOY_CLUSTER_CMD).substitute(path=remote_code_path,
+                                                                 log=self.deploy_cfg['log_file'])
         try:
             resp = node_obj.execute_cmd(cmd, read_lines=True, recv_ready=True,
                                         timeout=self.deploy_cfg['timeout']['deploy'])
