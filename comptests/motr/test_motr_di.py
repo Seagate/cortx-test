d31c108d5744a3c5038ef6d4b6be959ea7e404dd
seagate-sarang-sawant <70256535+seagate-sarang-sawant@users.noreply.github.com>
2022-05-16 11:39:00 +0530

    CORTX-31129 Removed not required part in test_motr_di module.
    Signed-off-by: seagate-sarang-sawant <70256535+seagate-sarang-sawant@users.noreply.github.com>

--- comptests/motr/test_motr_di.py
+++ comptests/motr/test_motr_di.py
@@ -82,63 +82,6 @@ class TestCorruptDataDetection:
             con.disconnect()
         del self.motr_obj
 
-    def update_m0crate_config(self, config_file, node):
-        """
-        This will modify the m0crate workload config yaml with the node details
-        param: confile_file: Path of m0crate workload config yaml
-        param: node: Cortx node on which m0crate utility to be executed
-        """
-        m0cfg = config_utils.read_yaml(config_file)[1]
-        node_enpts = self.motr_obj.get_cortx_node_endpoints(node)
-        # modify m0cfg and write back to file
-        m0cfg['MOTR_CONFIG']['MOTR_HA_ADDR'] = node_enpts['hax_ep']
-        m0cfg['MOTR_CONFIG']['PROF'] = self.motr_obj.profile_fid
-        m0cfg['MOTR_CONFIG']['PROCESS_FID'] = node_enpts[common_const.MOTR_CLIENT][0]['fid']
-        m0cfg['MOTR_CONFIG']['MOTR_LOCAL_ADDR'] = node_enpts[common_const.MOTR_CLIENT][0]['ep']
-        b_size = m0cfg['WORKLOAD_SPEC'][0]['WORKLOAD']['BLOCK_SIZE']
-        source_file = m0cfg['WORKLOAD_SPEC'][0]['WORKLOAD']['SOURCE_FILE']
-        file_size = source_file.split('/')[-1]
-        count = self.motr_obj.byte_conversion(file_size) // self.motr_obj.byte_conversion(b_size)
-        self.motr_obj.dd_cmd(b_size.upper(), str(count), source_file, node)
-        config_utils.write_yaml(config_file, m0cfg, backup=False, sort_keys=False)
-
-    @pytest.mark.tags("TEST-14925")
-    @pytest.mark.motr_sanity
-    def test_m0crate_utility(self, param_loop):
-        """
-        This is to run the m0crate utility tests.
-        param: param_loop: Fixture which provides one set of values required to run the utility
-        """
-        source_file = TEMP_PATH + 'source_file'
-        remote_file = TEMP_PATH + self.M0CRATE_WORKLOAD_YML.split("/")[-1]
-        m0cfg = config_utils.read_yaml(self.M0CRATE_WORKLOAD_YML)[1]
-        node = self.system_random.choice(self.motr_obj.cortx_node_list)
-        node_enpts = self.motr_obj.get_cortx_node_endpoints(node)
-        for key, value in param_loop.items():
-            if value.isdigit():
-                value = int(value)
-            if key in m0cfg['MOTR_CONFIG'].keys():
-                m0cfg['MOTR_CONFIG'][key] = value
-            elif key in m0cfg['WORKLOAD_SPEC'][0]['WORKLOAD'].keys():
-                m0cfg['WORKLOAD_SPEC'][0]['WORKLOAD'][key] = value
-            else:
-                if key == 'TEST_ID':
-                    logger.info("Executing the test: %s", value)
-                elif key == 'SOURCE_FILE_SIZE':
-                    file_size = value
-        m0cfg['MOTR_CONFIG']['MOTR_HA_ADDR'] = node_enpts['hax_ep']
-        m0cfg['MOTR_CONFIG']['PROF'] = self.motr_obj.profile_fid
-        m0cfg['MOTR_CONFIG']['PROCESS_FID'] = node_enpts[common_const.MOTR_CLIENT][0]['fid']
-        m0cfg['MOTR_CONFIG']['MOTR_LOCAL_ADDR'] = node_enpts[common_const.MOTR_CLIENT][0]['ep']
-        m0cfg['WORKLOAD_SPEC'][0]['WORKLOAD']['SOURCE_FILE'] = source_file
-        logger.info(m0cfg['MOTR_CONFIG'])
-        logger.info(m0cfg['WORKLOAD_SPEC'][0]['WORKLOAD'])
-        b_size = m0cfg['WORKLOAD_SPEC'][0]['WORKLOAD']['BLOCK_SIZE']
-        count = self.motr_obj.byte_conversion(file_size) // self.motr_obj.byte_conversion(b_size)
-        self.motr_obj.dd_cmd(b_size.upper(), str(count), source_file, node)
-        config_utils.write_yaml(self.M0CRATE_WORKLOAD_YML, m0cfg, backup=False, sort_keys=False)
-        self.motr_obj.m0crate_run(self.M0CRATE_WORKLOAD_YML, remote_file, node)
-
     def m0cp_corrupt_m0cat(self):
         logger.info("STARTED: Verify multiple m0cp/m0cat operation")
         infile = TEMP_PATH + 'input'
@@ -181,4 +124,3 @@ class TestCorruptDataDetection:
         Corrupt data block using m0cp and reading from object with m0cat should error.
         """
         self.m0cp_corrupt_m0cat()
-
