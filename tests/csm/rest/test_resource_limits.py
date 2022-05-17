15f670b0289b5978f7c62ba35b0ef2771fc088c2
Alexander Voronov <alexander.voronov@seagate.com>
2022-05-17 02:32:54 -0600

    CORTX-30331: add CSM REST active users quota test
    Signed-off-by: Alexander Voronov <alexander.voronov@seagate.com>

---
+++ tests/csm/rest/test_resource_limits.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2022 Seagate Technology LLC and/or its Affiliates
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+#
+"""Test CSM resource limits using REST API."""
+
+import logging
+import time
+from http import HTTPStatus
+
+import pytest
+
+from commons import configmanager
+from commons import cortxlogging
+from commons.constants import Rest as const  # noqa: N813
+
+from libs.csm.csm_interface import csm_api_factory
+
+
+class TestResourceLimits():
+    """REST API Test cases for CSM resource limits."""
+
+    @classmethod
+    def setup_class(cls):
+        """Set up operations for test suite set-up."""
+        cls.log = logging.getLogger(__name__)
+        cls.log.info("Initializing test setups ......")
+        cls.csm_conf = configmanager.get_config_wrapper(
+            fpath="config/csm/test_rest_resource_limits.yaml")
+        cls.created_users = []
+        cls.csm_obj = csm_api_factory("rest")
+        cls.log.info("Initiating Rest Client ...")
+
+    def teardown_method(self):
+        """Teardown for deleting temp csm users."""
+        self.log.info("[STARTED] ######### Teardown #########")
+        self.log.info("Deleting all csm users except predefined ones...")
+        delete_failed = []
+        delete_success = []
+        if self.created_users:
+            time.sleep(3)  # EOS-27030
+        for usr in self.created_users:
+            self.log.info("Sending request to delete csm user %s", usr)
+            try:
+                response = self.csm_obj.delete_csm_user(usr)
+                if response.status_code != HTTPStatus.OK:
+                    delete_failed.append(usr)
+                else:
+                    delete_success.append(usr)
+            except BaseException as any_error:  # pylint: disable=W0703
+                msg = f"Ignoring {any_error} while deleting user: {usr}"
+                self.log.warning(msg)
+        for usr in delete_success:
+            self.created_users.remove(usr)
+        self.log.info("csm delete success list %s", delete_success)
+        self.log.info("csm delete failed list %s", delete_failed)
+        assert len(delete_failed) == 0, "Delete failed for users"
+        self.log.info("[COMPLETED] ######### Teardown #########")
+
+    @pytest.mark.csmrest
+    @pytest.mark.tags("TEST-xxxxx")
+    def test_xxxxx(self):
+        """Test active users quota."""
+        test_case_name = cortxlogging.get_frame()
+        self.log.info("##### Test started - %s #####", test_case_name)
+
+        active_users_quota = self.csm_conf["test_xxxxx"]["active_users_quota"]
+        tmp_users_password = self.csm_conf["test_xxxxx"]["tmp_users_password"]
+
+        msg = f"Step 1: Create and login {active_users_quota - 1} CSM users"
+        self.log.info(msg)
+        for _ in range(active_users_quota - 1):
+            response = self.csm_obj.create_csm_user(
+                user_type="valid", user_role="manage",
+                user_password=tmp_users_password)
+            assert response.status_code == const.SUCCESS_STATUS_FOR_POST
+            username = response.json()["username"]
+            user_id = response.json()["id"]
+            self.created_users.append(user_id)
+            response = self.csm_obj.rest_login(
+                login_as={"username": username, "password": tmp_users_password})
+            assert response.status_code == const.SUCCESS_STATUS
+
+        msg = "Step 2: Create and login CSM user above active users quota"
+        self.log.info(msg)
+        response = self.csm_obj.create_csm_user(
+            user_type="valid", user_role="manage",
+            user_password=tmp_users_password)
+        assert response.status_code == const.SUCCESS_STATUS_FOR_POST
+        username = response.json()["username"]
+        user_id = response.json()["id"]
+        self.created_users.append(user_id)
+        response = self.csm_obj.rest_login(
+            login_as={"username": username, "password": tmp_users_password})
+        assert response.status_code == const.UNAUTHORIZED
+
+        self.log.info("################Test Passed##################")
