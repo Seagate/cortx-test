a3fb94cd29ab5c9e72fb30e87262ca660aa87534
aayushisharma88 <aayushi.sharma@seagate.com>
2022-05-17 21:33:11 +0530

    Cicd pipeline Changes (#1397)
    * Added CICD-Changes
    
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>
    
    * Added Changes
    
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>
    
    * Identation Error Changes
    
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>
    
    * Added command
    
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>
    
    * Added rgw
    
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>
    
    * Added requirements
    
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>
    
    * Added req
    
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>
    
    * Updated file
    
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>
    
    * Added services
    
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>
    
    * Remove file
    
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>
    
    * Added command
    
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>
    
    * Added command
    
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>
    
    * Added constant
    
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>
    
    * Added sanity
    
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>
    
    * Added set namespace command
    
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>
    
    * Codacy Resolved
    
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>
    
    * Removing Codacy
    
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>
    
    * Codacy Resolved
    
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>
    
    * Changed to main branch
    
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>
    Signed-off-by: Pranav Sahasrabudhe <Pranav.p.Sahasrabudhe@seagate.com>

--- comptests/prov/test_prov_k8s_cortx.py
+++ comptests/prov/test_prov_k8s_cortx.py
@@ -28,7 +28,9 @@ from commons import constants as common_const
 from commons import commands
 from commons.helpers.pods_helper import LogicalNode
 from commons.utils import assert_utils
-from config import CMN_CFG, PROV_CFG
+from config import CMN_CFG
+from config import PROV_CFG
+from config import PROV_TEST_CFG
 from libs.prov.prov_k8s_cortx_deploy import ProvDeployK8sCortxLib
 from libs.ha.ha_common_libs_k8s import HAK8s
 
@@ -39,7 +41,7 @@ LOGGER = logging.getLogger(__name__)
 SECRETS_FILES_LIST = ["s3_auth_admin_secret", "openldap_admin_secret", "kafka_admin_secret",
                       "csm_mgmt_admin_secret", "csm_auth_admin_secret", "consul_admin_secret",
                       "common_admin_secret"]
-PVC_LIST = ["auth", "cluster.conf", "hare", "motr", "s3", "solution", "utils", "log"]
+PVC_LIST = ["cluster.conf", "hare", "log", "motr", "rgw_s3", "solution"]
 
 
 class TestProvK8Cortx:
@@ -49,6 +51,7 @@ class TestProvK8Cortx:
         """Setup class"""
         LOGGER.info("STARTED: Setup Module operations")
         cls.deploy_cfg = PROV_CFG["k8s_cortx_deploy"]
+        cls.prov_deploy_cfg = PROV_TEST_CFG["k8s_prov_cortx_deploy"]
         cls.deploy_lc_obj = ProvDeployK8sCortxLib()
         cls.ha_obj = HAK8s()
         cls.dir_path = common_const.K8S_SCRIPTS_PATH
@@ -63,6 +66,8 @@ class TestProvK8Cortx:
             if CMN_CFG["nodes"][node]["node_type"].lower() == "master":
                 cls.master_node_obj = node_obj
                 cls.master_node_list.append(node_obj)
+                cls.master_node_obj.execute_cmd(cmd=commands.SET_NAMESPACE.format("default"),
+                read_lines=True)
             else:
                 cls.worker_node_list.append(node_obj)
         LOGGER.info("Done: Setup operations finished.")
@@ -320,7 +325,8 @@ class TestProvK8Cortx:
         assert_utils.assert_true(resp1[0], resp1[1])
         LOGGER.info("Executing cortx cluster shutdown command.")
         LOGGER.info("Step 2: Check whether cluster shutdown command ran successfully.")
-        resp = self.ha_obj.cortx_stop_cluster(self.master_node_list[0])
+        resp = self.ha_obj.cortx_stop_cluster(self.master_node_list[0],
+                                              dir_path=self.prov_deploy_cfg["git_remote_path"])
         assert_utils.assert_true(resp[0], resp[1])
         LOGGER.info("Step 3: Check whether data and control pods are not present")
         resp2 = self.ha_obj.check_pod_status(self.master_node_list[0])
@@ -340,10 +346,12 @@ class TestProvK8Cortx:
         assert_utils.assert_false(is_same)
         LOGGER.info("Step 4: Check the cluster status and start the cluster "
                     "in case its still down.")
-        resp = self.ha_obj.check_cluster_status(self.master_node_list[0])
+        resp = self.ha_obj.check_cluster_status(self.master_node_list[0],
+                                                dir_path=self.prov_deploy_cfg["git_remote_path"])
         if not resp[0]:
             LOGGER.info("Cluster not in good state, trying to restart it.")
-            resp = self.ha_obj.cortx_start_cluster(self.master_node_list[0])
+            resp = self.ha_obj.cortx_start_cluster(self.master_node_list[0],
+                                                   dir_path=self.prov_deploy_cfg["git_remote_path"])
             assert_utils.assert_true(resp[0], resp[1])
         LOGGER.info("Cluster is up and running.")
         LOGGER.info("Step 5: Cluster is back online.")
@@ -360,16 +368,19 @@ class TestProvK8Cortx:
         """
         LOGGER.info("Test Started.")
         LOGGER.info("Step 1: Check whether cluster shutdown command ran successfully.")
-        resp = self.ha_obj.cortx_stop_cluster(self.master_node_list[0])
+        resp = self.ha_obj.cortx_stop_cluster(self.master_node_list[0],
+                                              dir_path=self.prov_deploy_cfg["git_remote_path"])
         assert_utils.assert_true(resp[0], resp[1])
         LOGGER.info("Step 2: Check the cluster status and start the cluster "
                     "in case its still down.")
-        resp = self.ha_obj.check_cluster_status(self.master_node_list[0])
+        resp = self.ha_obj.check_cluster_status(self.master_node_list[0],
+                                                dir_path=self.prov_deploy_cfg["git_remote_path"])
         if not resp[0]:
             LOGGER.info("Cluster not in good state, trying to restart it.")
         LOGGER.info("Executing cortx cluster restart command.")
         LOGGER.info("Step 3: Check whether cluster restart command ran successfully.")
-        resp = self.ha_obj.cortx_start_cluster(self.master_node_list[0])
+        resp = self.ha_obj.cortx_start_cluster(self.master_node_list[0],
+                                               dir_path=self.prov_deploy_cfg["git_remote_path"])
         assert_utils.assert_true(resp[0], resp[1])
         LOGGER.info("Cluster is up and running.")
         LOGGER.info("Step 4: Checking whether all CORTX Data pods have been restarted.")
