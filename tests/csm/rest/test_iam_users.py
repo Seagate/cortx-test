e988dc17c0a3cf83d58977db20e0b78c8a28629b
Divya Kachhwaha <divya.kachhwaha@seagate.com>
2022-05-24 21:58:48 -0600

    commit
    Signed-off-by: Divya Kachhwaha <divya.kachhwaha@seagate.com>

--- tests/csm/rest/test_iam_users.py
+++ tests/csm/rest/test_iam_users.py
@@ -2167,7 +2167,6 @@ class TestIamUserRGW():
         self.log.info("##### Test completed -  %s #####", test_case_name)
 
 
-    @pytest.mark.skip("Bug CORTX-30999")
     @pytest.mark.lc
     @pytest.mark.csmrest
     @pytest.mark.cluster_user_ops
