66d50c5e009b255484f5cc0a11bb9ddea10b046b
ApurwaMohite <55824145+ApurwaMohite@users.noreply.github.com>
2022-06-01 16:20:24 +0530

    minor changes
    Signed-off-by: ApurwaMohite <55824145+ApurwaMohite@users.noreply.github.com>

--- tests/dtm/test_single_process_restart.py
+++ tests/dtm/test_single_process_restart.py
@@ -27,7 +27,6 @@ import os
 import secrets
 import threading
 import time
-import secrets
 from multiprocessing import Queue
 from time import perf_counter_ns
 
@@ -37,7 +36,6 @@ from commons import configmanager
 from commons import constants as const
 from commons.helpers.health_helper import Health
 from commons.helpers.pods_helper import LogicalNode
-from commons.params import TEST_DATA_FOLDER
 from commons.params import LATEST_LOG_FOLDER
 from commons.params import TEST_DATA_FOLDER
 from commons.utils import assert_utils
@@ -52,8 +50,6 @@ from conftest import LOG_DIR
 from libs.dtm.dtm_recovery import DTMRecoveryTestLib
 from libs.ha.ha_common_libs_k8s import HAK8s
 from libs.s3.s3_rest_cli_interface_lib import S3AccountOperations
-from libs.s3.s3_test_lib import S3TestLib
-from scripts.s3_bench import s3bench
 from libs.s3.s3_test_lib import S3TestLib
 from scripts.s3_bench import s3bench
 
