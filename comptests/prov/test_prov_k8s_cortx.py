f0ea8dbcc6bc1fc28e5e300c414c4603780c7991
Aayushi Sharma <aayushi.sharma@seagate.com>
2022-06-06 05:41:22 -0600

    Added Namespace from contstants
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>

--- comptests/prov/test_prov_k8s_cortx.py
+++ comptests/prov/test_prov_k8s_cortx.py
@@ -66,7 +66,7 @@ class TestProvK8Cortx:
             if CMN_CFG["nodes"][node]["node_type"].lower() == "master":
                 cls.master_node_obj = node_obj
                 cls.master_node_list.append(node_obj)
-                cls.master_node_obj.execute_cmd(cmd=commands.SET_NAMESPACE.format("cortx"),
+                cls.master_node_obj.execute_cmd(cmd=commands.SET_NAMESPACE.format(common_const.NAMESPACE),
                 read_lines=True)
             else:
                 cls.worker_node_list.append(node_obj)
