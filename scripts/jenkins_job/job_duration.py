26b8aa21dd150476cb5963fa4bc29b116336a02c
akshaym99 <akshay.s.mankar@seagate.com>
2022-06-20 11:23:10 +0530

    total & stage duration
    Signed-off-by: akshaym99 <akshay.s.mankar@seagate.com>

--- scripts/jenkins_job/job_duration.py
+++ scripts/jenkins_job/job_duration.py
@@ -7,6 +7,13 @@ stages = ['SANITY_TEST_EXECUTION', 'REGRESSION_TEST_EXECUTION', 'IO_PATH_TEST_EX
 STAGE_DURATION_CSV = 'stages_duration.csv'
 
 
+def convert_duration(duration_millis):
+    minutes, ss = divmod(duration_millis / 1000, 60)
+    hh, mm = divmod(minutes, 60)
+    duration = f'{hh:0>2.0f}:{mm:0>2.0f}:{ss:0>2.0f}'
+    return duration
+
+
 def main():
     """ main function """
     parser = argparse.ArgumentParser(description="Stage Duration")
@@ -20,14 +27,19 @@ def main():
     print("stages are: ", stage)
     with open(os.path.join(os.getcwd(), STAGE_DURATION_CSV), 'w', newline='', encoding="utf8") as stage_csv:
         writer = csv.writer(stage_csv)
+        total_duration = 0
         for i in stage:
             k = i['name']
             if k in stages:
                 v = i['durationMillis']
-                minutes, s = divmod(v / 1000, 60)
-                hr, m = divmod(minutes, 60)
-                duration = f'{hr:0>2.0f}:{m:0>2.0f}:{s:0>2.0f}'
+                total_duration += v
+                # minutes, s = divmod(v / 1000, 60)
+                # hr, m = divmod(minutes, 60)
+                # duration = f'{hr:0>2.0f}:{m:0>2.0f}:{s:0>2.0f}'
+                duration = convert_duration(v)
                 writer.writerow([k, duration])
+        total_duration = convert_duration(total_duration)
+        writer.writerow(['Total', total_duration])
 
 
 if __name__ == "__main__":
