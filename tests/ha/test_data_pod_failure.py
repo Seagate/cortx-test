db41024b1813c2b4bb9d797ef1065b1196312605
ankita-93 <ankita.thummar@seagate.com>
2022-07-04 09:48:58 +0530

    CORTX: HA test skip marker fix for data pod shutdown scenarion
    Signed-off-by: ankita-93 <ankita.thummar@seagate.com>

--- tests/ha/test_data_pod_failure.py
+++ tests/ha/test_data_pod_failure.py
@@ -1718,6 +1718,7 @@ class TestDataPodFailure:
     @pytest.mark.ha
     @pytest.mark.lc
     @pytest.mark.skip(reason="Functionality not Available")
+    @pytest.mark.skip(reason="VM issue in after Restart(LRL-3413). Need to be tested on HW")
     @pytest.mark.tags("TEST-32460")
     @CTFailOn(error_handler)
     def test_ha_pod_failover(self):
@@ -2278,6 +2279,7 @@ class TestDataPodFailure:
     @pytest.mark.ha
     @pytest.mark.lc
     @pytest.mark.tags("TEST-32458")
+    @pytest.mark.skip(reason="VM issue in after Restart(LRL-3413). Need to be tested on HW")
     @CTFailOn(error_handler)
     def test_pod_fail_node_down(self):
         """
@@ -2712,7 +2714,6 @@ class TestDataPodFailure:
     @pytest.mark.ha
     @pytest.mark.lc
     @pytest.mark.tags("TEST-35297")
-    @pytest.mark.skip(reason="VM issue in after Restart(LRL-3413). Need to be tested on HW")
     @CTFailOn(error_handler)
     def test_chunk_upload_during_pod_down(self):
         """
