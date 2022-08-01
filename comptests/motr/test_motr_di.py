57754349eeeea113e50eeee0bb571662137b0add
pranav-sahasrabudhe <pranav.p.sahasrabudhe@seagate.com>
2022-08-01 17:06:07 +0530

    WIP-RBF-parity-corrupt-41742
    Signed-off-by: pranav-sahasrabudhe <pranav.p.sahasrabudhe@seagate.com>

--- comptests/motr/test_motr_di.py
+++ comptests/motr/test_motr_di.py
@@ -248,8 +248,8 @@ class TestCorruptDataDetection:
         outfile = TEMP_PATH + "output"
         err_inj_script_path = const.MOTR_DI_ERR_INJ_LOCAL_PATH
         remote_script_path = const.CONTAINER_PATH,
-        motr_container_name = const.MOTR_CONTAINER_PREFIX
-        motr_container_name = motr_container_name + "-001"
+        motr_container_name = str(const.MOTR_CONTAINER_PREFIX) + "-001"
+        # motr_container_name = str(motr_container_name) + "-001"
         # node_pod_dict = self.motr_obj.get_node_pod_dict()
         node_data_pod_dict = self.motr_obj.get_node_data_pod_dict()
         logger.debug(f"node_data_pod_dict = {node_data_pod_dict}")
