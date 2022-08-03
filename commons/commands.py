e63ed26576b1c62f9916805fd74a15bc892ca037
Divya Kachhwaha <divya.kachhwaha@seagate.com>
2022-08-03 15:02:52 +0530

    Jmeter lib for parallel CSM user creation and deletion (#1790)
    * commit
    
    Signed-off-by: Divya Kachhwaha <divya.kachhwaha@seagate.com>
    
    * commit
    
    Signed-off-by: Divya Kachhwaha <divya.kachhwaha@seagate.com>
    
    * commit
    
    Signed-off-by: Divya Kachhwaha <divya.kachhwaha@seagate.com>
    
    * commit
    
    Signed-off-by: Divya Kachhwaha <divya.kachhwaha@seagate.com>
    
    * commit
    
    Signed-off-by: Divya Kachhwaha <divya.kachhwaha@seagate.com>
    
    * commit
    
    Signed-off-by: Divya Kachhwaha <divya.kachhwaha@seagate.com>
    
    * commit
    
    Signed-off-by: Divya Kachhwaha <divya.kachhwaha@seagate.com>

--- commons/commands.py
+++ commons/commands.py
@@ -532,7 +532,8 @@ K8S_CONSUL_UPDATE_CMD = 'kubectl exec -it {} -c {} -- {}'
 K8S_APPLY_YAML_CONFIG = 'kubectl apply -f {}'
 GET_STATS = "consul kv get -recurse stats"
 GET_BYTECOUNT = "consul kv get -recurse bytecount"
-
+GET_REQUEST_USAGE = "consul kv get -recurse csm/config/usage"
+GET_MAX_USERS = "consul kv get -recurse csm/config/CSM_USERS"
 # Kubectl command prefix
 KUBECTL_CMD = "kubectl {} {} -n {} {}"
 KUBECTL_GET_DEPLOYMENT = "kubectl get deployment"
