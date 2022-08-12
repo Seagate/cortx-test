38f812661e9b513b01ba3d470967d80698a1b22d
Divya Kachhwaha <divya.kachhwaha@seagate.com>
2022-08-12 05:20:36 -0600

    commit
    Signed-off-by: Divya Kachhwaha <divya.kachhwaha@seagate.com>

--- libs/csm/extensions/csm_prov_ext.py
+++ libs/csm/extensions/csm_prov_ext.py
@@ -103,7 +103,7 @@ class ProvExt():
             self.log.info("[Start] Sleep %s", self.update_seconds)
             time.sleep(self.update_seconds)
             self.log.info("[End] Sleep %s", self.update_seconds)
-            
+         
             self.log.info("Cleanup: Check cluster status")
             resp = self.ha_obj.poll_cluster_status(self.master)
             assert resp[0], resp[1]
